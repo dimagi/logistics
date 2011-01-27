77150bdbd306ee7d66c68ad98d02393c6dd62dd0
Anton de Winter <adewinter@dimagi.com>
2011-01-27 17:49:22 -0500

    fixed hack

--- ex-submodules/rapidsms/lib/rapidsms/router.py
+++ ex-submodules/rapidsms/lib/rapidsms/router.py
@@ -51,10 +51,10 @@ class Router(object, LoggerMixin):
         the list of apps to be notified of incoming messages. Return the
         app instance.
         """
-        #Terrible hack that's necessary to use gunicorn.
-        if 'gunicorn' in module_name: return None
-
-        cls = AppBase.find(module_name)
+        try:
+            cls = AppBase.find(module_name)
+        except AttributeError:
+            self.warning('Unable to create RapidSMS app from module: %s' % module_name)
         if cls is None: return None
 
         app = cls(self)
