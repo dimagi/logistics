16423d8b7372a483f972c75f42ef05caa0797834
Rowena <rluk@dimagi.com>
2011-02-09 03:19:20 -0500

    stockonhand unit tests

---
+++ ex-submodules/rapidsms-logistics/logistics/tests/stockonhand.py
@@ -0,0 +1,43 @@
+from rapidsms.tests.scripted import TestScript
+from rapidsms.contrib.messagelog.models import Message
+import logistics.apps.logistics.app as logistics_app
+from logistics.apps.logistics.handlers.stockonhand import ProductStockReport, ServiceDeliveryPoint
+
+class TestDeliveryGroup (TestScript):
+    apps = ([logistics_app.App])
+
+    def setUp(self):
+        TestScript.setUp(self)
+
+
+    def testProductStockReport(self):
+        sdp = ServiceDeliveryPoint()
+        m = Message()
+        p = ProductStockReport(sdp, m)
+        p.add_product_stock('jd',10, save=False)
+        p.add_product_stock('mc',30, save=False)
+        p.add_product_stock('aq',0, save=False)
+        p.add_product_stock('al',0, save=False)
+        self.assertEquals(p.all(), "jd 10, aq 0, al 0, mc 30")
+        self.assertEquals(p.stockouts(), "aq al")
+
+    def testStockOnHand(self):
+        a = """
+           16176023315 > register stella dedh
+           16176023315 < Thank you for registering at Dangme East District Hospital, dedh, stella
+           16176023315 > soh jd 10
+           16176023315 < Thank you stella for reporting your stock on hand for Dangme East District Hospital.  Still missing mc.
+           16176023315 > soh jd 10 mc 20
+           16176023315 < Thank you, you reported you have jd 10, mc 20. If incorrect, please resend.
+           """
+        self.runScript(a)
+
+    def testStockout(self):
+        a = """
+           16176023315 > register cynthia dedh
+           16176023315 < Thank you for registering at Dangme East District Hospital, dedh, cynthia
+           16176023315 > soh jd 0 mc 0
+           16176023315 < Thank you, you reported you have jd 0, mc 0. If incorrect, please resend.
+           """
+        self.runScript(a)
+
