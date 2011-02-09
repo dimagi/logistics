7918edce908b6d488e618080ed2a47e4be793b4e
Rowena <rluk@dimagi.com>
2011-02-09 04:06:45 -0500

    add sms registration

---
+++ logistics/apps/registration/handlers/registration.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# vim: ai ts=4 sts=4 et sw=4
+
+
+from rapidsms.contrib.handlers.handlers.keyword import KeywordHandler
+from rapidsms.models import Contact
+from rapidsms.conf import settings
+from django.utils.translation import ugettext as _
+from logistics.apps.logistics.models import LogisticsContact, ServiceDeliveryPoint
+
+class LanguageHandler(KeywordHandler):
+    """
+    Allow remote users to set their preferred language, by updating the
+    ``language`` field of the Contact associated with their connection.
+    """
+
+    keyword = "reg|register"
+
+    def help(self):
+        self.respond(_("To register, send register <name> <msd code>. Example: register john dedh"))
+    
+    def handle(self, text):
+        words = text.split()
+        if len(words) != 2:
+            self.respond(_("Sorry, I didn't understand. To register, send register <name> <msd code>. Example: register john dedh'"))
+            return
+        name, msd_code = words
+        try:
+            sdp = ServiceDeliveryPoint.objects.get(msd_code__contains=msd_code)
+        except ServiceDeliveryPoint.DoesNotExist:
+            self.respond(_("Sorry, can't find the location with MSD CODE %(msd_code)s"), msd_code=msd_code )
+            return
+        contact = LogisticsContact.objects.create(name=name, service_delivery_point=sdp)
+        self.msg.connection.contact = contact
+        self.msg.connection.save()
+        kwargs = {'sdp_name': sdp.name,
+                  'msd_code': msd_code,
+                  'contact_name': contact.name}
+        self.respond(_("Thank you for registering at %(sdp_name)s, %(msd_code)s, %(contact_name)s"), **kwargs)
