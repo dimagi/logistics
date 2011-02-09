2fe2e6519fc02a9d511f7db0b2e4713194616114
Rowena <rluk@dimagi.com>
2011-02-09 03:55:21 -0500

    add receipt unit test

---
+++ logistics/apps/logistics/tests/receipt.py
@@ -0,0 +1,22 @@
+from rapidsms.tests.scripted import TestScript
+from rapidsms.contrib.messagelog.models import Message
+import logistics.apps.logistics.app as logistics_app
+from logistics.apps.logistics.handlers.stockonhand import ProductStockReport, ServiceDeliveryPoint
+
+class TestReceipts (TestScript):
+    apps = ([logistics_app.App])
+
+    def setUp(self):
+        TestScript.setUp(self)
+
+    def testReceipt(self):
+        a = """
+           16176023315 > register stella dedh
+           16176023315 < Thank you for registering at Dangme East District Hospital, dedh, stella
+           16176023315 > rec jd 10
+           16176023315 < Thank you, you reported receipts for jd. If incorrect, please resend.
+           16176023315 > rec jd 10 mc 20
+           16176023315 < Thank you, you reported receipts for jd mc. If incorrect, please resend.
+           """
+        self.runScript(a)
+
