7f26ea659ceeb2afcd2b143a1b3260a011bf61ee
Rowena <rluk@dimagi.com>
2011-02-11 02:09:00 -0500

    fix unit tests

--- logistics/apps/logistics/tests/stockonhand.py
+++ logistics/apps/logistics/tests/stockonhand.py
@@ -14,21 +14,21 @@ class TestStockOnHand (TestScript):
         sdp = ServiceDeliveryPoint()
         m = Message()
         p = ProductStockReport(sdp, m, STOCK_ON_HAND_REPORT_TYPE)
-        p.add_product_stock('jd',10, save=False)
+        p.add_product_stock('lf',10, save=False)
         p.add_product_stock('mc',30, save=False)
         p.add_product_stock('aq',0, save=False)
         p.add_product_stock('al',0, save=False)
-        self.assertEquals(p.all(), "jd 10, aq 0, al 0, mc 30")
+        self.assertEquals(p.all(), "lf 10, aq 0, al 0, mc 30")
         self.assertEquals(p.stockouts(), "aq al")
 
     def testStockOnHand(self):
         a = """
            16176023315 > register stella dedh
            16176023315 < Thank you for registering at Dangme East District Hospital, dedh, stella
-           16176023315 > soh jd 10
+           16176023315 > soh lf 10
            16176023315 < Thank you stella for reporting your stock on hand for Dangme East District Hospital.  Still missing mc.
-           16176023315 > soh jd 10 mc 20
-           16176023315 < Thank you, you reported you have jd 10, mc 20. If incorrect, please resend.
+           16176023315 > soh lf 10 mc 20
+           16176023315 < Thank you, you reported you have lf 10, mc 20. If incorrect, please resend.
            """
         self.runScript(a)
 
@@ -36,8 +36,8 @@ class TestStockOnHand (TestScript):
         a = """
            16176023315 > register cynthia dedh
            16176023315 < Thank you for registering at Dangme East District Hospital, dedh, cynthia
-           16176023315 > soh jd 0 mc 0
-           16176023315 < The following items are stocked out: jd mc. Please place an order now.
+           16176023315 > soh lf 0 mc 0
+           16176023315 < The following items are stocked out: lf mc. Please place an order now.
            """
         self.runScript(a)
 
@@ -45,8 +45,8 @@ class TestStockOnHand (TestScript):
         a = """
            16176023315 > register cynthia dedh
            16176023315 < Thank you for registering at Dangme East District Hospital, dedh, cynthia
-           16176023315 > soh jd 9 mc 9
-           16176023315 < The following items are in low supply: jd mc. Please place an order now.
+           16176023315 > soh lf 9 mc 9
+           16176023315 < The following items are in low supply: lf mc. Please place an order now.
            """
         self.runScript(a)
 
@@ -54,8 +54,8 @@ class TestStockOnHand (TestScript):
         a = """
            16176023315 > register cynthia dedh
            16176023315 < Thank you for registering at Dangme East District Hospital, dedh, cynthia
-           16176023315 > soh jd 10 20 mc 20
-           16176023315 < Thank you, you reported you have jd 10, mc 20. You received jd 20. If incorrect, please resend.
+           16176023315 > soh lf 10 20 mc 20
+           16176023315 < Thank you, you reported you have lf 10, mc 20. You received lf 20. If incorrect, please resend.
            """
         self.runScript(a)
 
@@ -63,8 +63,8 @@ class TestStockOnHand (TestScript):
         a = """
            16176023315 > register cynthia dedh
            16176023315 < Thank you for registering at Dangme East District Hospital, dedh, cynthia
-           16176023315 > soh jd 10-20.mc 20
-           16176023315 < Thank you, you reported you have jd 10, mc 20. You received jd 20. If incorrect, please resend.
+           16176023315 > soh lf 10-20.mc 20
+           16176023315 < Thank you, you reported you have lf 10, mc 20. You received lf 20. If incorrect, please resend.
            """
         self.runScript(a)
 
