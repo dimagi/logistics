280b8dd5ec8c7d17eba98820406ba3d205d95890
Rowena <rluk@dimagi.com>
2011-02-26 16:11:19 -0500

    first pass at aggregate stock report. also pull out accordion nav into a partial.

--- ex-submodules/rapidsms-logistics/logistics/urls.py
+++ ex-submodules/rapidsms-logistics/logistics/urls.py
@@ -3,9 +3,12 @@
 
 
 from django.conf.urls.defaults import *
+from logistics.apps.logistics.models import get_geography
 
 urlpatterns = patterns('',
     url(r'^input_stock$', 'logistics.apps.logistics.views.input_stock', name="input_stock"),
+    url(r'^(?P<location_code>\w+)/aggregate/?$', 'logistics.apps.logistics.views.aggregate', name="aggregate"),
+    url(r'^ghana/aggregate/?$', 'logistics.apps.logistics.views.aggregate', kwargs={'location_code':get_geography().code}, name="aggregate_top"),
     url(r'^(?P<facility_code>\w+)/stockonhand/?$', 'logistics.apps.logistics.views.stockonhand', name="stockonhand"),
     url(r'^reporting$', 'logistics.apps.logistics.views.reporting', name="reporting"),
 )
