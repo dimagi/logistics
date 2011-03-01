d5cad3f0c2adc6447a4886b12069d431fdb3ef7d
Rowena <rluk@dimagi.com>
2011-03-01 18:11:10 -0500

    update rapidsms submodule ref

--- ex-submodules/rapidsms-logistics/logistics/handlers/help.py
+++ ex-submodules/rapidsms-logistics/logistics/handlers/help.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
+
 from rapidsms.contrib.handlers.handlers.keyword import KeywordHandler
 from django.utils.translation import ugettext as _
 
