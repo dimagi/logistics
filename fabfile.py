296d07e617f524d3b98142fa82c340b841c7ea89
Rowena <rluk@dimagi.com>
2011-03-05 12:42:46 -0500

    get fabfile deploy working

--- fabfile.py
+++ fabfile.py
@@ -14,54 +14,58 @@ CONFIGURATION
 """""""""""""""
 def test():
     env.config = 'test'
-    env.fab_user = 'rowena'
-    env.hosts = ['localhost']
+    env.deploy_dir = os.path.dirname(__file__)
+    env.code_dir = os.path.join(env.deploy_dir, 'logistics')
+    env.hosts = ['rowena@localhost']
 
 def staging():
     env.config = 'staging'
-    env.fab_user = 'ewsghana'
-    env.hosts = ['ewsghana.dyndns.org']
+    env.deploy_dir = '/home/ewsghana'
+    env.code_dir = os.path.join(env.deploy_dir, 'logistics')
+    env.hosts = ['ewsghana@ewsghana.dyndns.org']
 
 def production():
     env.config = 'production'
-    env.fab_user = 'ewsghana'
-    env.hosts = ['ewsghana.dyndns.org']
+    env.deploy_dir = '/home/ewsghana'
+    env.code_dir = os.path.join(env.deploy_dir, 'logistics')
+    env.hosts = ['ewsghana@ewsghana.dyndns.org']
 
 """""""""""""""""
 CAN'T TOUCH THIS
 """""""""""""""""
-def _setup_paths():
-    env.cwd = os.path.dirname(__file__)
-    env.code_root = os.path.join(env.cwd, 'logistics')
-
 def django_tests():
-    """Run django tests"""
+    """run django tests"""
     with cd('logistics'):
-        local('./manage.py test', capture=False)
+        local('./manage.py test --noinput', capture=False)
 
 def update_requirements():
-    """ update external dependencies on remote host """
-    _setup_paths()
-    local('pip install --requirement %s' % os.path.join(env.cwd, "pip-requires.txt"))
+    """ update external dependencies """
+    with cd(env.code_dir):
+        run('ls')
+        run('pip install --requirement %s' % os.path.join(env.code_dir, "pip-requires.txt"))
 
 def bootstrap():
-    """ Run this after you've checked out the code """
-    update_requirements()
-    run('git submodule init')
-    run('git submodule update')
-    with cd('logistics'):
-        run('./manage.py syncdb')
-        run('./setup.py')
+    """ run this after you've checked out the code """
+    with cd(env.code_dir):
+        run('git submodule init')
+        run('git submodule update')
+        with cd('logistics'):
+            run('./manage.py syncdb --noinput')
+            run('./setup.py')
 
 def deploy():
-    require('fab_user', provided_by=('test', 'staging', 'production'))
+    """ deploy code to some remote environment """
+    require('config', provided_by=('test', 'staging', 'production'))
     if env.config == 'production':
         if not console.confirm('Are you sure you want to deploy production?',
                                default=False):
             utils.abort('Production deployment aborted.')
+    if not console.confirm('Are you sure you want to wipe out the "logistics" folder?',
+                           default=False):
+        utils.abort('Deployment aborted.')
+    run('rm -rf logistics')
     run('git clone git://github.com/dimagi/logistics.git')
-    with cd('logistics'):
-        bootstrap()
+    bootstrap()
 
 def test_and_deploy():
     django_tests()
