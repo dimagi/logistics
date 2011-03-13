ab71a6858f9c5bb3d82b40acb68353598338b4eb
Rowena <rluk@dimagi.com>
2011-03-13 18:57:47 -0400

    create a 'district' view that uses report template for both aggregate and by-product reports

---
+++ ex-submodules/rapidsms-logistics/logistics/view_decorators.py
@@ -0,0 +1,33 @@
+from django.shortcuts import get_object_or_404
+from logistics.apps.logistics.models import Product, ProductType, Location, get_geography
+
+def filter_context(func):
+    """
+    add commodities and commoditytypes to context
+    """
+    def _new_func(*args, **kwargs):
+        context = {}
+        context['commodities'] = Product.objects.all().order_by('name')
+        context['commoditytypes'] = ProductType.objects.all().order_by('name')
+        if 'context' in kwargs:
+            kwargs['context'].update(context)
+        else:
+            kwargs['context'] = context
+        return func(*args, **kwargs)
+    return _new_func
+
+def geography_context(func):
+    """
+    add geography to context
+    """
+    def _new_func(*args, **kwargs):
+        context = {}
+        context['geography'] = get_geography()
+        if 'location_code' in kwargs:
+            context['location'] = get_object_or_404(Location, code=kwargs['location_code'])
+        if 'context' in kwargs:
+            kwargs['context'].update(context)
+        else:
+            kwargs['context'] = context
+        return func(*args, **kwargs)
+    return _new_func
