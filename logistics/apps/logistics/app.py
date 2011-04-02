7720e9454a381f1baef907176d2472b8ff962849
Cory Zue <czue@dimagi.com>
2011-04-02 17:26:03 +0200

    Revert "simplify registration app for demo"
    This reverts commit afdeada12048deb4029baecd47a048922bb63f7f.
    
    Tests pass again, woot!
    
    Conflicts:
    
    	logistics/apps/logistics/app.py

--- logistics/apps/logistics/app.py
+++ logistics/apps/logistics/app.py
@@ -14,12 +14,10 @@ from rapidsms.contrib.scheduler.models import EventSchedule, set_weekly_event
 from logistics.apps.logistics.models import Product, ProductReportsHelper, \
     STOCK_ON_HAND_REPORT_TYPE, GET_HELP_MESSAGE
 from logistics.apps.logistics.errors import UnknownCommodityCodeError
+from logistics.apps.logistics.models import REGISTER_MESSAGE
 
 ERR_MSG = _("Please send your stock on hand in the format 'soh <product> <amount> <product> <amount>'")
 SOH_KEYWORD = 'soh'
-REGISTRATION_REQUIRED_MSG = ("You must registered on the Early Warning System "
-                             "before you can submit a stock report. Please "
-                             "contact your district administrator.")
 
 class App(AppBase):
     bootstrapped = False
@@ -59,7 +57,7 @@ class App(AppBase):
         if not self._should_handle(message):
             return False
         if not hasattr(message,'logistics_contact'):
-            message.respond(REGISTRATION_REQUIRED_MSG)
+            message.respond(REGISTER_MESSAGE)
             return True
         sdp = message.logistics_contact.facility
         if sdp is None:
