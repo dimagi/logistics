039d4e19f386656f1e828479d9fad5d7b60bfd00
Rowena <rluk@dimagi.com>
2011-04-12 21:54:30 +0000

    fix facility registration form so that admins can select which commodities are actively stocked or not

--- logistics/apps/logistics/forms.py
+++ logistics/apps/logistics/forms.py
@@ -4,12 +4,47 @@
 from django import forms
 from django.conf import settings
 from django.utils.translation import ugettext as _
-from .models import Facility, Product
+from .models import Facility, Product, ProductStock
 
 class FacilityForm(forms.ModelForm):
+    commodities = forms.ModelMultipleChoiceField(Product.objects.all().order_by('name'), required=False)
+    
     class Meta:
         model = Facility
-
+    
+    def __init__(self, *args, **kwargs):
+        kwargs['initial'] = {'commodities': [0,1,2,3]}
+        if 'instance' in kwargs:
+            if 'initial' not in kwargs:
+                kwargs['initial'] = {}
+            pss = ProductStock.objects.filter(facility=kwargs['instance'], 
+                                              is_active=True)
+            kwargs['initial']['commodities'] = [p.product.pk for p in pss]
+        super(FacilityForm, self).__init__(*args, **kwargs)
+                
+    def save(self, *args, **kwargs):
+        facility = super(FacilityForm, self).save(*args, **kwargs)
+        if self.cleaned_data['commodities']:
+            for commodity in Product.objects.all():
+                if commodity in self.cleaned_data['commodities']:
+                    ps, created = ProductStock.objects.get_or_create(facility=facility, 
+                                                                     product=commodity)
+                    ps.is_active = True
+                    ps.save()
+                else:
+                    try:
+                        ps = ProductStock.objects.get(facility=facility, 
+                                                      product=commodity)
+                    except ProductStock.DoesNotExist:
+                        # great
+                        pass
+                    else:
+                        # if we have stock info, we keep it around just in case
+                        # but we mark it as inactive
+                        ps.is_active = False
+                        ps.save()
+        return facility
+    
 class CommodityForm(forms.ModelForm):
     class Meta:
         model = Product
