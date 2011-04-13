38cac0c43bd91edb6c3fab724d89ec8971c69f1c
Cory Zue <czue@dimagi.com>
2011-04-13 15:46:21 -0700

    fix blow up when no attached profile on login

--- logistics/manage.py
+++ logistics/manage.py
@@ -13,10 +13,6 @@ sys.path.append(os.path.join(filedir,'..','rapidsms','lib'))
 sys.path.append(os.path.join(filedir,'..','rapidsms','lib','rapidsms'))
 sys.path.append(os.path.join(filedir,'..','rapidsms','lib','rapidsms','contrib'))
 sys.path.append(os.path.join(filedir,'..','submodules','django-cpserver'))
-sys.path.append(os.path.join(filedir,'..','submodules','dimagi-utils'))
-sys.path.append(os.path.join(filedir,'..','submodules','django-tablib'))
-sys.path.append(os.path.join(filedir,'..','submodules','tablib'))
-sys.path.append(os.path.join(filedir,'..','submodules','auditcare'))
 
 if __name__ == "__main__":
     execute_manager(settings)
