23e63bac04f39c46cf54533b8eca669dbc70ae69
Rowena <rluk@dimagi.com>
2011-04-14 21:56:01 +0000

    make banner title of rapidsms page configurable

--- ex-submodules/rapidsms/lib/rapidsms/context_processors.py
+++ ex-submodules/rapidsms/lib/rapidsms/context_processors.py
@@ -4,10 +4,12 @@ def logo(request):
     try:
         logo_right_url = settings.LOGO_RIGHT_URL
         logo_left_url = settings.LOGO_LEFT_URL
+        site_title = settings.SITE_TITLE
     except AttributeError:
-        warnings.warn("No LOGO_LEFT_URL and/or LOGO_RIGHT_URL specified in settings! rapidsms.context_processors.logo")
+        warnings.warn("No LOGO_LEFT_URL and/or LOGO_RIGHT_URL and/or SITE_TITLE specified in settings! rapidsms.context_processors.logo")
         logo_right_url = ""
         logo_left_url = ""
 
     return {'logo_right_url' : logo_right_url ,
-            'logo_left_url' : logo_left_url }
+            'logo_left_url' : logo_left_url, 
+            'site_title' : site_title }
