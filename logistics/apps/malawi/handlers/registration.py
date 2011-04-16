2cb067e14efeca825036d9daf9c7938e138f6fa9
Christian Ternus <cternus@dimagi.com>
2011-04-15 20:42:38 -0400

    New 'hsareg' command allows ID specification and creates associated SupplyPoint.

---
+++ logistics/apps/malawi/handlers/registration.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# vim: ai ts=4 sts=4 et sw=4
+
+from django.utils.translation import ugettext as _
+from rapidsms.conf import settings
+from rapidsms.contrib.handlers.handlers.keyword import KeywordHandler
+from rapidsms.models import Contact
+from logistics.apps.logistics.models import ContactRole, Facility, SupplyPoint, REGISTER_MESSAGE, SupplyPointType
+
+HELP_MESSAGE = "Sorry, I didn't understand. To register, send register <name> <id> <parent facility>. Example: register john 115 dwdh'"
+class HSARegistrationHandler(KeywordHandler):
+    """
+    Allow remote users to set their preferred language, by updating the
+    ``language`` field of the Contact associated with their connection.
+    """
+
+    keyword = "hsareg|hsaregister"
+
+    def help(self):
+        self.respond(_(HELP_MESSAGE))
+    
+    def handle(self, text):
+        words = text.split()
+        if len(words) < 3 or len(words) > 4:
+            self.respond(_(HELP_MESSAGE))
+            return
+        name = words[0]
+        id =   words[1]
+        code = words[2]
+        try:
+            fac = Facility.objects.get(code__contains=code)
+        except Facility.DoesNotExist:
+            self.respond(_("Sorry, can't find the location with FACILITY CODE %(code)s"), code=code )
+            return
+
+        role = None
+        if len(words) == 4:
+            role_code = words[3]
+            try:
+                role = ContactRole.objects.get(code=role_code)
+            except ContactRole.DoesNotExist:
+                self.respond("Sorry, I don't understand the role %(role)s", role=role_code)
+                return
+
+        sp = SupplyPoint.objects.create(name=name, code=id, type=SupplyPointType.objects.get(pk="hsa"), location=fac.location, supplied_by=fac)
+
+        if role:
+            contact = Contact.objects.create(name=name, supply_point=sp, role=role)
+        else:
+            contact = Contact.objects.create(name=name, supply_point=sp)
+        self.msg.connection.contact = contact
+        self.msg.connection.save()
+        kwargs = {'sdp_name': fac.name,
+                  'code': code,
+                  'contact_name': contact.name}
+        self.respond(_("Congratulations %(contact_name)s, you have successfully been registered for the Early Warning System. Your facility is %(sdp_name)s"), **kwargs)
---
+++ logistics/apps/malawi/tests.py
@@ -0,0 +1,32 @@
+__author__ = 'ternus'
+import unittest
+from django import forms
+from rapidsms.conf import settings
+from rapidsms.tests.scripted import TestScript
+from logistics.apps.registration.forms import IntlSMSContactForm
+from logistics.apps.logistics.models import Location, Facility, SupplyPointType
+from logistics.apps.malawi.handlers.registration import REGISTER_MESSAGE, HELP_MESSAGE
+
+class TestHSARegister(TestScript):
+    fixtures = ["ghana_initial_data.json"]
+    def setUp(self):
+        TestScript.setUp(self)
+        location = Location.objects.get(code='de')
+        facilitytype = SupplyPointType.objects.get(code='hc')
+        rms = Facility.objects.get(code='garms')
+        Facility.objects.get_or_create(code='dedh', name='Dangme East District Hospital',
+                                       location=location, active=True,
+                                       type=facilitytype, supplied_by=rms)
+
+    def testRegister(self):
+        a = """
+              8005551212 > hsareg
+              8005551212 < %(help_message)s
+              8005551212 > hsareg stella
+              8005551212 < %(help_message)s
+              8005551212 > hsareg stella 115 doesntexist
+              8005551212 < Sorry, can't find the location with FACILITY CODE doesntexist
+              8005551212 > hsareg stella 115 dedh
+              8005551212 < Congratulations stella, you have successfully been registered for the Early Warning System. Your facility is Dangme East District Hospital
+            """ % {'register_message':REGISTER_MESSAGE, 'help_message':HELP_MESSAGE}
+        self.runScript(a)
