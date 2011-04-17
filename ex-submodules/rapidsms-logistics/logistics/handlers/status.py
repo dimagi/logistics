b75555fbbab163c1cd1e73b9e621fdd082d6dec2
Rowena <rluk@dimagi.com>
2011-04-17 11:33:52 +0000

    small tweak for better status reporting for supervisors

--- ex-submodules/rapidsms-logistics/logistics/handlers/status.py
+++ ex-submodules/rapidsms-logistics/logistics/handlers/status.py
@@ -4,7 +4,8 @@
 from rapidsms.contrib.handlers.handlers.keyword import KeywordHandler
 from django.utils.translation import ugettext_noop as _
 from logistics.apps.logistics.models import REGISTER_MESSAGE
-from logistics.apps.logistics.models import ProductReport, ProductStock
+from logistics.apps.logistics.models import ProductReport, ProductStock, \
+    REPORTEE_RESPONSIBILITY
 
 class Stop(KeywordHandler):
     """
@@ -20,9 +21,18 @@ class Stop(KeywordHandler):
         if self.msg.contact is None:
             self.respond(REGISTER_MESSAGE)
             return
-        reports = ProductReport.objects.filter(message__contact=self.msg.contact)
-        if not reports:
-            self.respond("You have not submitted any stock reports yet.")
+        if REPORTEE_RESPONSIBILITY in self.msg.contact.role.responsibilities.values_list('code', flat=True):
+            # if a super, show last report from your facility
+            reports = ProductReport.objects.filter(facility=self.msg.contact.facility)
+            if not reports:
+                self.respond("Your facility has not submitted any stock reports yet.")
+            resp = "Last Report: %s"
+        else:
+            # else if a reporter, show your last report
+            reports = ProductReport.objects.filter(message__contact=self.msg.contact)
+            if not reports:
+                self.respond("You have not submitted any stock reports yet.")
+            resp = "You sent: %s"
         last_report = reports.order_by("-report_date")[0]
-        self.respond(("You sent: %s" % last_report.message.text)[:160])
+        self.respond((resp % last_report.message.text)[:160])
 
