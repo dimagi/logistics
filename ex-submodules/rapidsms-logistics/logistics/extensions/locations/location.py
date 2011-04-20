d8d2a91d23fc9eb81c0fbd856ba1faa4b499d475
Cory Zue <czue@dimagi.com>
2011-04-20 17:23:05 -0400

    add default activation status setting, fix aggregate views

--- ex-submodules/rapidsms-logistics/logistics/extensions/locations/location.py
+++ ex-submodules/rapidsms-logistics/logistics/extensions/locations/location.py
@@ -37,36 +37,36 @@ class Location(models.Model):
     """ The following methods express AGGREGATE counts, of all subsumed facilities"""
     def stockout_count(self, product=None, producttype=None):
         from logistics.apps.logistics.models import stockout_count
-        return stockout_count(self.all_facilities, product, producttype)
+        return stockout_count(self.all_facilities(), product, producttype)
 
     def emergency_stock_count(self, product=None, producttype=None):
         """ This indicates all stock below reorder levels,
             including all stock below emergency supply levels
         """
         from logistics.apps.logistics.models import emergency_stock_count
-        return emergency_stock_count(self.all_facilities, product, producttype)
+        return emergency_stock_count(self.all_facilities(), product, producttype)
 
     def low_stock_count(self, product=None, producttype=None):
         """ This indicates all stock below reorder levels,
             including all stock below emergency supply levels
         """
         from logistics.apps.logistics.models import low_stock_count
-        return low_stock_count(self.all_facilities, product, producttype)
+        return low_stock_count(self.all_facilities(), product, producttype)
 
     def good_supply_count(self, product=None, producttype=None):
         """ This indicates all stock below reorder levels,
             including all stock below emergency supply levels
         """
         from logistics.apps.logistics.models import good_supply_count
-        return good_supply_count(self.all_facilities, product, producttype)
+        return good_supply_count(self.all_facilities(), product, producttype)
 
     def overstocked_count(self, product=None, producttype=None):
         from logistics.apps.logistics.models import overstocked_count
-        return overstocked_count(self.all_facilities, product, producttype)
+        return overstocked_count(self.all_facilities(), product, producttype)
 
     def consumption(self, product=None, producttype=None):
         from logistics.apps.logistics.models import consumption
-        return consumption(self.all_facilities, product, producttype)
+        return consumption(self.all_facilities(), product, producttype)
 
     def deprecate(self, new_code=None):
         """
