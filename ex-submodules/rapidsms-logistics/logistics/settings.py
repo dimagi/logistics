0e15307cdf60a7710cf88d84dbb58f3de96776ef
Cory Zue <czue@dimagi.com>
2011-04-21 11:16:39 -0400

    update to malawi-specific calculation settings

--- ex-submodules/rapidsms-logistics/logistics/settings.py
+++ ex-submodules/rapidsms-logistics/logistics/settings.py
@@ -1,6 +1,7 @@
 # Logistics App
 # These settings define how many months constitute emergency, low supply,
 # and oversupply stock for the logistics app
+LOGISTICS_DEFAULT_PRODUCT_ACTIVATION_STATUS = False
 LOGISTICS_EMERGENCY_LEVEL_IN_MONTHS = 0.5
 LOGISTICS_REORDER_LEVEL_IN_MONTHS = 1.5
 LOGISTICS_MAXIMUM_LEVEL_IN_MONTHS = 3
