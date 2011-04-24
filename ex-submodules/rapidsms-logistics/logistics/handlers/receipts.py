4265c920d0c065cc2c8844acaec2b706c36155d0
Cory Zue <czue@dimagi.com>
2011-04-24 11:30:34 -0400

    proper linkage of receipts with pending requests. updated tests.

--- ex-submodules/rapidsms-logistics/logistics/handlers/receipts.py
+++ ex-submodules/rapidsms-logistics/logistics/handlers/receipts.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python
 # vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
-import datetime
 
-from re import sub
 from datetime import datetime
-from dateutil.relativedelta import relativedelta
-from django.db.models import Q
 from django.utils.translation import ugettext as _
 from rapidsms.contrib.handlers.handlers.keyword import KeywordHandler
-from rapidsms.messages import OutgoingMessage
-from logistics.apps.logistics.models import ProductReportsHelper, RECEIPT_REPORT_TYPE
+from logistics.apps.logistics.models import ProductReportsHelper, RECEIPT_REPORT_TYPE,\
+    StockRequest
 from logistics.apps.logistics.models import REGISTER_MESSAGE
 
 class ReceiptHandler(KeywordHandler):
@@ -30,4 +26,15 @@ class ReceiptHandler(KeywordHandler):
         stock_report = ProductReportsHelper(facility, RECEIPT_REPORT_TYPE, self.msg.logger_msg)
         stock_report.parse(text)
         stock_report.save()
+        # Close pending requests. This logic only applies if you are using the 
+        # StockRequest workflow, but should not break anything if you are not
+        pending_reqs = StockRequest.pending_requests().filter\
+            (supply_point=self.msg.logistics_contact.supply_point,
+             product__sms_code__in=stock_report.product_stock.keys())
+        now = datetime.utcnow()
+        for req in pending_reqs:
+            req.receive(self.msg.logistics_contact, 
+                        stock_report.product_stock[req.product.sms_code],
+                        now)
+                
         self.respond(_('Thank you, you reported receipts for %(stocks)s.'), stocks=" ".join(stock_report.reported_products()).strip())
