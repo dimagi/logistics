ffc753d59252e1f85eb9ba29abf45cb9d32a379f
Christian Ternus <cternus@dimagi.com>
2011-05-03 17:42:23 -0400

    Use icons to indicate product stock status.

--- ex-submodules/rapidsms-logistics/logistics/templatetags/logistics_extras.py
+++ ex-submodules/rapidsms-logistics/logistics/templatetags/logistics_extras.py
@@ -2,5 +2,5 @@ from django import template
 register = template.Library()
 
 @register.inclusion_tag("logistics/templatetags/highlight_months_available.html")
-def highlight_months(stockonhand):
-    return { "stockonhand": stockonhand }
+def highlight_months(stockonhand, media_url):
+    return { "stockonhand": stockonhand, "MEDIA_URL":media_url }
