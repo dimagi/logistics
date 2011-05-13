9970030a0a2fabfd0037f3903198a9700bf8b736
Cory Zue <czue@dimagi.com>
2011-05-13 12:47:03 -0400

    alphabetizing is fun!

--- logistics/apps/malawi/tests/__init__.py
+++ logistics/apps/malawi/tests/__init__.py
@@ -1,4 +1,6 @@
 import unittest
+from logistics.apps.malawi.tests.boot import *
+from logistics.apps.malawi.tests.createuser import *
 from logistics.apps.malawi.tests.nag import *
 from logistics.apps.malawi.tests.product import *
 from logistics.apps.malawi.tests.register import *
@@ -6,5 +8,3 @@ from logistics.apps.malawi.tests.report import *
 from logistics.apps.malawi.tests.roles import *
 from logistics.apps.malawi.tests.stockonhand import *
 from logistics.apps.malawi.tests.transfer import *
-from logistics.apps.malawi.tests.createuser import *
-from logistics.apps.malawi.tests.boot import *
