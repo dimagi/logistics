4491cd54472352f94aa544a1dbba1520fcad2102
Cory Zue <czue@dimagi.com>
2011-05-18 18:55:34 -0400

    refactor charts utility out into it's own module, use transaction model

---
+++ ex-submodules/rapidsms-logistics/logistics/charts.py
@@ -0,0 +1,28 @@
+import gviz_api
+from logistics.apps.logistics.models import ProductReportType, Product
+from logistics.apps.logistics.const import Reports
+
+
+def stocklevel_plot(transactions):
+    products = set(transactions.values_list("product__sms_code", flat=True))
+    cols = {"date": ("datetime", "Date")}
+    for p in products:
+        cols[Product.objects.get(sms_code=p).name] = ('number', p)#, {'type': 'string', 'label': "title_"+s.sms_code}]
+    table = gviz_api.DataTable(cols)
+
+    data_rows = {}
+    for t in transactions:
+        if not t.date in data_rows: data_rows[t.date] = {}
+        data_rows[t.date][t.product.name] = t.ending_balance
+        
+    rows = []
+    for d in data_rows.keys():
+        q = {"date":d}
+        q.update(data_rows[d])
+        rows += [q]
+    table.LoadData(rows)
+    chart_data = table.ToJSCode("chart_data", columns_order=["date"] + [x for x in cols.keys() if x != "date"],
+                                order_by="date")
+    
+    return chart_data
+        
