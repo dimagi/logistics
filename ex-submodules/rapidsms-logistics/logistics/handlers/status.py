d5b51f21c4e5741bedf578402d2915d433f4ff58
Rowena <rluk@dimagi.com>
2011-05-22 18:13:27 -0400

    small fix so that status doesn't throw hidden exceptions

--- ex-submodules/rapidsms-logistics/logistics/handlers/status.py
+++ ex-submodules/rapidsms-logistics/logistics/handlers/status.py
@@ -26,12 +26,14 @@ class Stop(KeywordHandler):
             reports = ProductReport.objects.filter(facility=self.msg.contact.facility)
             if not reports:
                 self.respond("Your facility has not submitted any stock reports yet.")
+                return
             resp = "Last Report: '%(report)s' on %(date)s"
         else:
             # else if a reporter, show your last report
             reports = ProductReport.objects.filter(message__contact=self.msg.contact)
             if not reports:
                 self.respond("You have not submitted any stock reports yet.")
+                return
             resp = "You sent '%(report)s' on %(date)s "
         last_report = reports.order_by("-report_date")[0]
         self.respond(resp % {'report': last_report.message.text, 
