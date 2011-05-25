53d0448886f3f874df4075c5796cdf1c6fd0a3b0
Cory Zue <czue@dimagi.com>
2011-05-25 16:06:50 -0400

    add breadcrumbs

---
+++ logistics/apps/malawi/templatetags/malawi_tags.py
@@ -0,0 +1,30 @@
+from django import template
+from logistics.apps.logistics.util import config
+from django.core.urlresolvers import reverse
+
+register = template.Library()
+
+@register.simple_tag
+def place_url(location):
+    if location.type.slug == config.LocationCodes.HSA:
+        return reverse("malawi_hsa", args=[location.code])
+    elif location.type.slug == config.LocationCodes.FACILITY:
+        return reverse("malawi_facility", args=[location.code])
+    elif location.type.slug == config.LocationCodes.DISTRICT:
+        return "%s?place=%s" % (reverse("malawi_facilities"), location.code)
+    else:
+        return reverse("malawi_dashboard")
+
+
+@register.simple_tag
+def breadcrumbs(location):
+    mycrumbs = '<a href="%(link)s">%(display)s</a>' % \
+                {"link": place_url(location),
+                 "display": location.name}
+    if location.parent is not None:
+        # > 
+        return '%(parentcrumbs)s &raquo; %(mycrumbs)s' % \
+            {"parentcrumbs": breadcrumbs(location.parent),
+             "mycrumbs": mycrumbs}
+    else:
+        return mycrumbs
