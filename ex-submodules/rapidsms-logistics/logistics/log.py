540812542bdd9ee1e8b70781c6f5477e0669f90e
Rowena <rluk@dimagi.com>
2011-06-06 14:38:31 +0800

    whoops, fix previous. forgot that many django deployments don't use manage.py

---
+++ ex-submodules/rapidsms-logistics/logistics/log.py
@@ -0,0 +1,15 @@
+#from __future__ import absolute_import
+#import logging
+#from dimagi.utils.gmailloghandler import TLSSMTPHandler
+#
+#root = logging.getLogger()
+#root.setLevel(logging.ERROR)
+#handler = TLSSMTPHandler(
+#    ('smtp.gmail.com', 587),
+#    'ewsghana.notifications@gmail.com',
+#    'rluk@dimagi.com',
+#    'Logistics Error',
+#    ('ewsghana.notifications@gmail.com', 'password')
+#)
+#handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(message)s'))
+#root.addHandler(handler)
