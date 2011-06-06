22ed5269698ac9b236a4a6e69e42f28e01630a4e
Rowena <rluk@dimagi.com>
2011-06-06 18:31:56 +0800

    first pass at a breadcrumb nav

--- ex-submodules/rapidsms-logistics/logistics/templatetags/geonav_tags.py
+++ ex-submodules/rapidsms-logistics/logistics/templatetags/geonav_tags.py
@@ -7,3 +7,6 @@ def render_nav(geography, location):
     return render_to_string("logistics/partials/accordion_partial.html", {"geography": geography,
                                                                           "location": location})
     
+@register.simple_tag
+def render_bread_nav(location):
+    return render_to_string("logistics/partials/breadcrumb_partial.html", {"location": location})
