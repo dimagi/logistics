73ee1059b7caf66d0ce21aea963711e2ba1fa299
Cory Zue <czue@dimagi.com>
2011-06-06 19:02:21 -0400

    add hover text to late m&e report

--- ex-submodules/rapidsms-logistics/logistics/templatetags/logistics_extras.py
+++ ex-submodules/rapidsms-logistics/logistics/templatetags/logistics_extras.py
@@ -1,6 +1,12 @@
 from django import template
+from logistics.apps.logistics.templatetags.math_tags import percent
 register = template.Library()
 
 @register.inclusion_tag("logistics/templatetags/highlight_months_available.html")
 def highlight_months(stockonhand, media_url):
     return { "stockonhand": stockonhand, "MEDIA_URL":media_url }
+
+@register.simple_tag
+def percent_cell(a, b):
+    val = percent(a, b)
+    return '<td title="%(a)s of %(b)s">%(val)s</td>' % {"a": a, "b": b, "val": val}
