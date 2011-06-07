579c1e35c47382ce2ae7b1e36eed06555a1bc843
Cory Zue <czue@dimagi.com>
2011-06-07 09:21:00 -0400

    better docs and comments in the monitoring report code

--- logistics/apps/malawi/reports.py
+++ logistics/apps/malawi/reports.py
@@ -3,6 +3,12 @@ from django.core.urlresolvers import reverse
 class ReportDefinition(object):
     """
     A configuration object to hold a report definition.
+    
+    This object expects you to have a corresponding function in 
+    reportcalcs.py that contains metadata associated with the report.
+    
+    The docstring of the function is used as the report description.
+    The function itself is used to render the report data.
     """
     
     def __init__(self, slug):
@@ -20,10 +26,15 @@ class ReportDefinition(object):
         """
         The name of the view for the report. 
         """
+        # TODO: genericize
         return "malawi_monitoring_report"
     
     @property
     def template(self):
+        """
+        The place where this report goes to look for a template.
+        """
+        # TODO: don't hard code the folder reference
         return "malawi/partials/monitoring_reports/%s.html" % self.slug
     
     @property
@@ -36,15 +47,25 @@ class ReportDefinition(object):
         Expects that you have a function defined in reportcalcs to match
         the slug
         """
+        # TODO: genericize
         import logistics.apps.malawi.reportcalcs as reportcalcs
         return getattr(reportcalcs, self.slug)
     
 class ReportInstance(object):
+    """
+    An instance of a report. Right now consists only of a pairing of 
+    the report definition with a datespan representing the range to
+    run the report on.
+    """
+    
     def __init__(self, definition, datespan):
         self.definition = definition
         self.datespan = datespan
     
     def get_report_body(self):
+        """
+        The body of the report, as html suitable for putting in a template
+        """
         return self.definition.calcfunction(self)
     
 REPORT_SLUGS = ["em_late_reporting",
