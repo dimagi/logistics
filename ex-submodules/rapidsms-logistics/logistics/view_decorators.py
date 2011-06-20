ec80208e4e65ab43fb409d62cf14f1f8851d1544
Rowena <rluk@dimagi.com>
2011-06-20 15:27:13 -0400

    add location filtering to facilities list view

--- ex-submodules/rapidsms-logistics/logistics/view_decorators.py
+++ ex-submodules/rapidsms-logistics/logistics/view_decorators.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404
+from rapidsms.conf import settings
 from logistics.apps.logistics.models import Product, ProductType, Location, get_geography
 
 def filter_context(func):
@@ -44,6 +45,24 @@ def geography_context(func):
     def _new_func(*args, **kwargs):
         context = {}
         context['geography'] = get_geography()
+        if 'context' in kwargs:
+            kwargs['context'].update(context)
+        else:
+            kwargs['context'] = context
+        return func(*args, **kwargs)
+    return _new_func
+
+def location_context(func):
+    """
+    add geography to context
+    """
+    def _new_func(*args, **kwargs):
+        context = {}
+        location_code = settings.COUNTRY
+        if 'location_code' in kwargs:
+            location_code = kwargs['location_code']
+        location = get_object_or_404(Location, code=location_code)
+        context['location'] = location
         if 'context' in kwargs:
             kwargs['context'].update(context)
         else:
