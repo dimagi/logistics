40dbfdef2dce2cebe2a80230c2f3d5eba10b6854
Rowena <rluk@dimagi.com>
2011-07-11 17:19:43 +0300

    context_processor to inject base_template in template view

--- ex-submodules/rapidsms-logistics/logistics/context_processors.py
+++ ex-submodules/rapidsms-logistics/logistics/context_processors.py
@@ -6,3 +6,21 @@ def custom_settings(request):
 
 def google_analytics(request):
     return {"GOOGLE_ANALYTICS_ID": getattr(settings, 'GOOGLE_ANALYTICS_ID', None)}
+
+def base_template(request):
+    """ 
+    For compatibility with the logistics app, which allows users
+    to specify a custom 'base template' dynamically
+    """ 
+    try:
+        base_template = settings.BASE_TEMPLATE
+    except AttributeError:
+        base_template = "layout.html"
+
+    try:
+        base_template_split_2 = settings.BASE_TEMPLATE_SPLIT_2
+    except AttributeError:
+        base_template_split_2 = "layout-split-2.html"
+
+    return {'base_template': base_template,
+            'base_template_split_2': base_template_split_2 }
