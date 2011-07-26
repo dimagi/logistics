abb26edc05d63880970e58a64bd4215514a85717
Christian Ternus <cternus@dimagi.com>
2011-07-26 09:38:39 -0400

    Disable four of the eight alerts, per request.

--- logistics/deployments/malawi/settings_base.py
+++ logistics/deployments/malawi/settings_base.py
@@ -145,10 +145,10 @@ BASE_TEMPLATE_SPLIT_2="malawi/base-split-2.html"
 LOGISTICS_CONFIG = 'static.malawi.config'
 
 LOGISTICS_ALERT_GENERATORS = [
-    'logistics.apps.malawi.alerts.hsas_no_supervision',
-    'logistics.apps.malawi.alerts.hsas_no_products',
-    'logistics.apps.malawi.alerts.late_reporting_receipt',
-    'logistics.apps.malawi.alerts.non_reporting_hsas',
+    #'logistics.apps.malawi.alerts.hsas_no_supervision',
+    #'logistics.apps.malawi.alerts.hsas_no_products',
+    #'logistics.apps.malawi.alerts.late_reporting_receipt',
+    #'logistics.apps.malawi.alerts.non_reporting_hsas',
     'logistics.apps.malawi.alerts.health_center_stockout',
     'logistics.apps.malawi.alerts.hsa_below_emergency_quantity',
     'logistics.apps.malawi.alerts.health_center_unable_resupply_stockout',
