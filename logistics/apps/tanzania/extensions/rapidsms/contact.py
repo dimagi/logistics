b15db791db1bdfce0f99afc1ad9c1d059e4c9495
Christian Ternus <cternus@dimagi.com>
2011-08-03 19:25:44 -0400

    Migration for field User in tanzania Contact extension.

--- logistics/apps/tanzania/extensions/rapidsms/contact.py
+++ logistics/apps/tanzania/extensions/rapidsms/contact.py
@@ -4,8 +4,8 @@ from django.db import models
 
 class TanzaniaContactExtension(models.Model):
 
-    # user = models.OneToOneField(User)
-    
+    user = models.OneToOneField(User, null=True, blank=True)
+
     class Meta:
         abstract = True
 
