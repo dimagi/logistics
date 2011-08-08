e398ae4442dccf7eae0d40b78977ff296158b90b
Ryan Hartford <hartfordr@gmail.com>
2011-08-08 14:43:22 -0700

    added test for stop command

---
+++ logistics/apps/tanzania/tests/stop.py
@@ -0,0 +1,13 @@
+from logistics.apps.tanzania.tests.base import TanzaniaTestScriptBase
+from logistics.apps.tanzania.tests.util import register_user
+
+class TestStop(TanzaniaTestScriptBase):
+    
+    def testStop(self):
+        contact = register_user(self, "778", "someone")
+        script = """
+          778 > stop
+          778 < Umesitisha kukumbushwa kwenye hii namba. tafadhali tuma 'msaada' kupata maelekezo jinsi ya kujiunga tena
+        """
+        self.runScript(script)
+        self.assertFalse(contact.is_active)
