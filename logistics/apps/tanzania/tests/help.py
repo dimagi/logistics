c7d6668d874c86c51ea1c8a27ff2df007c1d93a4
Ryan Hartford <hartfordr@gmail.com>
2011-08-08 16:25:39 -0700

    updated the help handler test to include registered and unregistered states

--- logistics/apps/tanzania/tests/help.py
+++ logistics/apps/tanzania/tests/help.py
@@ -1,4 +1,7 @@
 from logistics.apps.tanzania.tests.base import TanzaniaTestScriptBase
+from logistics.apps.tanzania.tests.util import register_user
+from logistics.apps.logistics.util import config
+from django.utils.translation import ugettext as _
 
 class TestHelp(TanzaniaTestScriptBase):
     
@@ -7,6 +10,14 @@ class TestHelp(TanzaniaTestScriptBase):
 #        Swahili
         script = """
           743 > help
-          743 < Haujasajiliwa,Tafadhali jisajili kwanza kabla ya kupata huduma,Kusajili andika 'sajili<nafasi><jina lako><nafasi><msd code>'. Mfano 'sajili Peter Juma d34002'
-        """
+          743 < %(help_unregistered)s
+        """ % {'help_unregistered': _(config.Messages.HELP_UNREGISTERED)}
         self.runScript(script)
+        
+        contact = register_user(self, "778", "someone")
+        script = """
+          778 > help
+          778 < %(help_registered)s
+        """ % {'help_registered': _(config.Messages.HELP_REGISTERED)}
+        self.runScript(script)
+        
