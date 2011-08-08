928efa4e6359099d49a00157beb68fe504ab9ce3
Cory Zue <czue@dimagi.com>
2011-08-08 17:19:08 -0400

    clean malawi handler imports

--- logistics/apps/malawi/handlers/abstract/register.py
+++ logistics/apps/malawi/handlers/abstract/register.py
@@ -3,7 +3,6 @@ from logistics.apps.logistics.models import SupplyPoint
 from rapidsms.contrib.handlers.handlers.keyword import KeywordHandler
 from logistics.apps.malawi.handlers.abstract.base import RecordResponseHandler
 from logistics.apps.logistics.util import config
-from config import Messages
 
 class RegistrationBaseHandler(RecordResponseHandler):
     supply_point = None
@@ -12,14 +11,14 @@ class RegistrationBaseHandler(RecordResponseHandler):
     
     def handle_preconditions(self, text):
         """
-        Check some precondidtions, based on shared assumptions of these handlers.
+        Check some preconditions, based on shared assumptions of these handlers.
         Return true if there is a precondition that wasn't met. If all preconditions
         are met, the variables for facility and name will be set.
         
         This method will manage some replies as well.
         """
         if hasattr(self.msg,'logistics_contact') and self.msg.logistics_contact.is_active:
-            self.respond(Messages.ALREADY_REGISTERED)
+            self.respond(config.Messages.ALREADY_REGISTERED)
         
         words = text.split()
         if len(words) < 3:
@@ -31,7 +30,7 @@ class RegistrationBaseHandler(RecordResponseHandler):
             try:
                 self.supply_point = SupplyPoint.objects.get(code__iexact=code)
             except SupplyPoint.DoesNotExist:
-                self.respond(_(Messages.UNKNOWN_LOCATION), code=code )
+                self.respond(_(config.Messages.UNKNOWN_LOCATION), code=code )
 
         return self.responded
         
