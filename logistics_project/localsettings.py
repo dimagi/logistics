caa56a782debd6db9e560df23f3c9c823975b237
Cory Zue <czue@dimagi.com>
2011-08-09 16:42:32 -0400

    delete localsettings

--- logistics_project/localsettings.py
+++
@@ -1,6 +0,0 @@
-# you should configure your database here before doing any real work.
-# see: http://docs.djangoproject.com/en/dev/ref/settings/#databases
-
-# import your deployment specific settings here.
-from logistics_project.deployments.ghana.settings_base import *
-
