6eba986547cb6a46b115cd1578db047e830c2c29
Cory Zue <czue@dimagi.com>
2011-08-11 18:15:10 -0400

    check in a semi-functional migration script

--- logistics_project/apps/tanzania/management/commands/tz_migrate.py
+++ logistics_project/apps/tanzania/management/commands/tz_migrate.py
@@ -5,6 +5,14 @@ import os
 import csv
 from logistics.models import ContactRole, SupplyPoint
 from logistics_project.apps.tanzania.loader import init_static_data
+from couchdb.client import Row
+from dimagi.utils.parsing import string_to_datetime
+from rapidsms.router import router
+from django.core.management import call_command
+from rapidsms.contrib.ajax.exceptions import RouterNotResponding, RouterError
+import sys
+from rapidsms.contrib.messagelog.models import Message
+from logistics_project.apps.migration import utils
 
 def get_supply_point(name, code, type):
     """
@@ -31,10 +39,10 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         init_static_data()
         
-        def cleanup(self):
+        def cleanup():
             Contact.objects.all().delete()
             Connection.objects.all().delete()
-        
+            Message.objects.all().delete()
     
         def load_contacts(file):
             print "loading contacts from %s" % file
@@ -56,25 +64,80 @@ class Command(BaseCommand):
                                                    supply_point=sp, is_approved=True)
                         idmap[id] = c
                     c = idmap[id]
-                    
                     if backend and phone:
                         be = Backend.objects.get_or_create(name=backend)[0]
                         Connection.objects.create(backend=be, identity=phone,
                                                   contact=c)
+                    if phone and backend=="push_backend":
+                        # for real users, also create a migration backend for them 
+                        be = Backend.objects.get_or_create(name="migration")[0]
+                        Connection.objects.get_or_create(backend=be, identity=phone,
+                                                         contact=c)
                         
             print "Migrated %s Contacts and %s Phone numbers" % \
                     (Contact.objects.count(), Connection.objects.count())
         
+        def load_messages(message_file):
+            print "loading messages from %s" % message_file
+            print "started"
+            with open(message_file) as f:
+                reader = csv.reader(f, delimiter=',', quotechar='"')
+                count = 0
+                max = 100
+                for row in reader:
+                    pk1, pk2, pk3, dir, timestamp, text, phone = row
+                    if dir == "I":
+                        print "%s: %s (%s)" % (phone, text, timestamp)
+                        count = count + 1
+                        try:
+                            utils.send_test_message(identity=phone,
+                                                    text=text,
+                                                    timestamp=timestamp)
+                        except RouterError, e:
+                            print e.code
+                            print e.content_type
+                            print e.response
+                            raise
+                            
+
+                    if count >= max:
+                        break
+            pass
+        
+        def check_router():
+            # quasi-stolen from the httptester
+            try:
+                utils.check_status()
+                return True
+            except RouterNotResponding:
+                print "The router is not available! Remember to start the rapidsms " \
+                  "router before running the migration. FAIL."
+            except RouterError:
+                print "The router appears to be running but returned an error. " \
+                      "Is the migration app in your installed_apps list? and " \
+                      "have you enabled the migration backend?" 
+                return False
+                
+        print "checking router.."
+        if not check_router():
+            sys.exit()
+        print "router running."
+            
         if True or are_you_sure("really run the migration? (yes or no): "):
+            cleanup()
             print "Starting migration!"
-            file = os.path.join(os.path.dirname\
+            datapath = os.path.join(os.path.dirname\
                                 (os.path.dirname\
                                  (os.path.dirname\
                                   (os.path.dirname\
                                    (os.path.dirname\
                                     (os.path.abspath(os.path.dirname(__file__))))))),
-                                "static", "tanzania", "migration", "contacts.csv")
-            load_contacts(file)
+                                "static", "tanzania", "migration")
+            contact_file = os.path.join(datapath, "contacts.csv")
+            load_contacts(contact_file)
+            
+            messages_file = os.path.join(datapath, "messages.csv")
+            load_messages(messages_file)
             
         else:
             print "Migration canceled."
