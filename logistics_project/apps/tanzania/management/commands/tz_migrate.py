2d3fd5b8ed946025c166979ee6e7a9ad99ca8f5a
Christian Ternus <cternus@dimagi.com>
2011-08-15 11:57:42 -0400

    Ignore invalid Unicode messages in migration.

--- logistics_project/apps/tanzania/management/commands/tz_migrate.py
+++ logistics_project/apps/tanzania/management/commands/tz_migrate.py
@@ -14,6 +14,21 @@ import sys
 from rapidsms.contrib.messagelog.models import Message
 from logistics_project.apps.migration import utils
 
+def unicode_csv_reader(unicode_csv_data, dialect=csv.excel, **kwargs):
+    # csv.py doesn't do Unicode; encode temporarily as UTF-8:
+    csv_reader = csv.reader(utf_8_encoder(unicode_csv_data),
+                            dialect=dialect, **kwargs)
+    for row in csv_reader:
+        # decode UTF-8 back to Unicode, cell by cell:
+        yield [unicode(cell, 'utf-8') for cell in row]
+def utf_8_encoder(unicode_csv_data):
+    for line in unicode_csv_data:
+        try:
+            yield line.encode('utf-8')
+        except UnicodeDecodeError:
+            print line
+            continue
+
 def get_supply_point(name, code, type):
     """
     Get a supply point. Intentionally fail if not found.
@@ -80,8 +95,8 @@ class Command(BaseCommand):
         def load_messages(message_file):
             print "loading messages from %s" % message_file
             print "started"
-            with open(message_file) as f:
-                reader = csv.reader(f, delimiter=',', quotechar='"')
+            with open(message_file, 'r') as f:
+                reader = unicode_csv_reader(f, delimiter=',', quotechar='"')
                 count = 0
                 max = 9999999999
                 for row in reader:
