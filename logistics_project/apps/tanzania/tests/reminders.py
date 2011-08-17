91c84ac86d571a4923b76ee14ebdb36b61473765
Cory Zue <czue@dimagi.com>
2011-08-17 18:01:49 -0400

    add test for reminders-by-day

--- logistics_project/apps/tanzania/tests/reminders.py
+++ logistics_project/apps/tanzania/tests/reminders.py
@@ -1,10 +1,46 @@
 from logistics_project.apps.tanzania.tests.base import TanzaniaTestScriptBase
 from dimagi.utils.dates import get_business_day_of_month,\
-    get_business_day_of_month_after, get_business_day_of_month_before
+    get_business_day_of_month_after, get_business_day_of_month_before,\
+    get_day_of_month
 from datetime import date
+from django.test.testcases import TestCase
 
-class TestBusinessDays(TanzaniaTestScriptBase):
+class TestBusinessDays(TestCase):
         
+    def testDays(self):
+        # there's nothing fancy here
+        self.assertEqual(date(2011, 8, 1), get_day_of_month(2011, 8, 1))
+        self.assertEqual(date(2011, 8, 2), get_day_of_month(2011, 8, 2))
+        self.assertEqual(date(2011, 8, 3), get_day_of_month(2011, 8, 3))
+        self.assertEqual(date(2011, 8, 4), get_day_of_month(2011, 8, 4))
+        self.assertEqual(date(2011, 8, 5), get_day_of_month(2011, 8, 5))
+        self.assertEqual(date(2011, 8, 6), get_day_of_month(2011, 8, 6))
+        
+        # negative
+        self.assertEqual(date(2011, 8, 31), get_day_of_month(2011, 8, -1))
+        self.assertEqual(date(2011, 8, 28), get_day_of_month(2011, 8, -4))
+        
+        # random
+        self.assertEqual(date(2011, 9, 1), get_day_of_month(2011, 9, 1))
+        self.assertEqual(date(2011, 10, 1), get_day_of_month(2011, 10, 1))
+        self.assertEqual(date(2012, 2, 5), get_day_of_month(2012, 2, 5))
+        
+        # leap year
+        self.assertEqual(date(2012, 2, 29), get_day_of_month(2012, 2, -1))
+        
+        
+        # fail
+        try:
+            get_day_of_month(2011, 2, 30)
+            self.fail("previous call should have failed")
+        except ValueError: pass
+        
+        try:
+            get_day_of_month(2011, 2, -30)
+            self.fail("previous call should have failed")
+        except ValueError: pass
+        
+    
     def testBusinessDays(self):
         # normal
         self.assertEqual(date(2011, 8, 1), get_business_day_of_month(2011, 8, 1))
