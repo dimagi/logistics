41bfb5bcd22b70229bdc47817715609b17a70f6e
Christian Ternus <cternus@dimagi.com>
2011-08-19 17:52:12 -0400

    fabfile for ilsstaging

--- fabfile.py
+++ fabfile.py
@@ -73,6 +73,30 @@ def malawi_old():
     env.hosts = ['sc4ccm@50.56.116.170']
     _malawi_shared()
 
+def _tz_shared():
+    env.pathhack = True # sketchily, this must come before any join calls
+    env.config = 'tz'
+    env.code_dir = _join(env.deploy_dir, 'logistics')
+    env.code_cleanup = False
+    env.db_cleanup = False
+    env.stop_start = True
+    env.branch = "tz-dev"
+    def malawi_extras():
+#        run("python manage.py malawi_init")
+#        run("python manage.py loaddata ../deploy/malawi/initial_data.json")
+	sudo("/etc/init.d/memcached restart")
+    env.extras = malawi_extras
+
+def tz_staging():
+    """
+    TZ configuration (staging)
+    """
+    env.deploy_dir = '/home/dimagivm/src'
+    env.db_name = "logistics"
+    env.hosts = ['dimagivm@ilsstaging.dimagi.com']
+    _tz_shared()
+
+
 def staging():
     env.config = 'staging'
     env.deploy_dir = '/home/ewsghana'
@@ -99,12 +123,12 @@ def update_requirements():
         run('ls')
         run('pip install --requirement %s' % _join(env.code_dir, "pip-requires.txt"))
 
-def bootstrap():
+def bootstrap(subdir='logistics'):
     """ run this after you've checked out the code """
     with cd(env.code_dir):
         run('git submodule init')
         run('git submodule update')
-        with cd('logistics'):
+        with cd(subdir):
             run('./manage.py syncdb --noinput')
             run('./manage.py migrate --noinput')
             # this doesn't seem to exist
@@ -138,7 +162,7 @@ def deploy():
         sudo('dropdb %(dbname)s' % {"dbname": env.db_name}, user="postgres")
         sudo('createdb %(dbname)s' % {"dbname": env.db_name}, user="postgres")
         
-    bootstrap()
+    bootstrap(subdir='logistics_project' if env.config.startswith('tz') else 'logistics')
     if env.stop_start:
         sudo("/etc/init.d/apache2 start")
         sudo("supervisorctl start all")
