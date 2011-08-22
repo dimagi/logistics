41dd66f123c7fa63661517c621b8fe5655ca467a
Cory Zue <czue@dimagi.com>
2011-08-22 17:21:07 -0400

    add cutoffs for randr reminders. tests

--- logistics_project/apps/tanzania/tests/reminders.py
+++ logistics_project/apps/tanzania/tests/reminders.py
@@ -1,4 +1,5 @@
-from logistics_project.apps.tanzania.reminders import stockonhand , delivery
+from logistics_project.apps.tanzania.reminders import stockonhand , delivery,\
+    randr
 from logistics_project.apps.tanzania.tests.util import register_user
 from logistics_project.apps.tanzania.tests.base import TanzaniaTestScriptBase
 from logistics_project.apps.tanzania.config import SupplyPointCodes
@@ -78,6 +79,12 @@ class TestDeliveryReminder(TanzaniaTestScriptBase):
         now = datetime.utcnow()
         self.assertEqual(1, len(list(delivery.get_facility_people(now))))
         
+        # reports for a different type shouldn't update status
+        script = """
+            778 > nimetuma
+        """
+        self.assertEqual(1, len(list(delivery.get_facility_people(now))))
+        
         script = """
             778 > nimepokea
         """
@@ -86,4 +93,51 @@ class TestDeliveryReminder(TanzaniaTestScriptBase):
         self.assertEqual(1, len(list(delivery.get_facility_people(datetime.utcnow()))))
         
         
+class TestRandRReminder(TanzaniaTestScriptBase):
+    
+    def setUp(self):
+        super(TestRandRReminder, self).setUp()
+        Contact.objects.all().delete()
+        ProductReport.objects.all().delete()
+        self.contact = register_user(self, "778", "someone")
+        sp = self.contact.supply_point
+        sp.groups = (SupplyPointGroup.objects.get\
+                     (code=DeliveryGroups.current_submitting_group()),)
+        sp.save()
+        
+    def testGroupExclusion(self):
+        people = list(randr.get_facility_people(datetime.utcnow()))
+        self.assertEqual(1, len(people))
+        for person in people:
+            self.assertEqual(self.contact, person)
+        
+        sp = self.contact.supply_point
+        sp.groups = (SupplyPointGroup.objects.get\
+                     (code=DeliveryGroups.current_delivering_group()),)
+        sp.save()
+        self.assertEqual(0, len(list(randr.get_facility_people(datetime.utcnow()))))
+        
+        sp = self.contact.supply_point
+        sp.groups = (SupplyPointGroup.objects.get\
+                     (code=DeliveryGroups.current_processing_group()),)
+        sp.save()
+        self.assertEqual(0, len(list(randr.get_facility_people(datetime.utcnow()))))
+        
+    def testReportExclusion(self):
+        now = datetime.utcnow()
+        self.assertEqual(1, len(list(randr.get_facility_people(now))))
+        
+        # reports for a different type shouldn't update status
+        script = """
+            778 > nimepokea
+        """
+        self.runScript(script)
+        self.assertEqual(1, len(list(randr.get_facility_people(now))))
+        
+        script = """
+            778 > nimetuma
+        """
+        self.runScript(script)
+        self.assertEqual(0, len(list(randr.get_facility_people(now))))
+        self.assertEqual(1, len(list(randr.get_facility_people(datetime.utcnow()))))
     
