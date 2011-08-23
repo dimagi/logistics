c4270d16c7e974bdde9dcb9935d6ffc1ee188bf1
Cory Zue <czue@dimagi.com>
2011-08-23 15:36:33 -0400

    properly update status for soh reminders

--- logistics_project/apps/tanzania/reminders/stockonhand.py
+++ logistics_project/apps/tanzania/reminders/stockonhand.py
@@ -6,6 +6,8 @@ from logistics_project.apps.tanzania.config import SupplyPointCodes
 from rapidsms.models import Contact
 from logistics.const import Reports
 from dimagi.utils.dates import get_business_day_of_month_before
+from logistics_project.apps.tanzania.models import SupplyPointStatusValues,\
+    SupplyPointStatusTypes, SupplyPointStatus
 
 def get_people(cutoff):
     # these go out every month to every active person at all facilities
@@ -30,23 +32,34 @@ def last_yearmonth():
     last_month = datetime(now.year, now.month, 1) - timedelta(days=1)
     return (last_month.year, last_month.month)
     
+def update_statuses(contacts):
+    now = datetime.utcnow()
+    for sp in set(c.supply_point for c in contacts):
+        SupplyPointStatus.objects.create(supply_point=sp,
+                                         status_type=SupplyPointStatusTypes.SOH_FACILITY,
+                                         status_value=SupplyPointStatusValues.REMINDER_SENT,
+                                         status_date=now)
+            
 @businessday(-1)
 def first():
     """Last business day of the month 2:00 PM"""
-    send_reminders(get_people(get_cutoff(*this_yearmonth())), 
-                   config.Messages.REMINDER_STOCKONHAND)
+    people = get_people(get_cutoff(*this_yearmonth()))
+    send_reminders(people, config.Messages.REMINDER_STOCKONHAND) 
+    update_statuses(people)        
     
 @businessday(1)
 def second():
     """1st business day of the next month 9:00am"""
-    send_reminders(get_people(get_cutoff(*last_yearmonth())), 
-                   config.Messages.REMINDER_STOCKONHAND)
+    people = get_people(get_cutoff(*last_yearmonth()))
+    send_reminders(people, config.Messages.REMINDER_STOCKONHAND)
+    update_statuses(people)
     
 @businessday(5)
 def third():
     """5th business day of the next month 8:15am"""
-    send_reminders(get_people(get_cutoff(*last_yearmonth())), 
-                   config.Messages.REMINDER_STOCKONHAND)
+    people = get_people(get_cutoff(*last_yearmonth()))
+    send_reminders(people, config.Messages.REMINDER_STOCKONHAND)
+    update_statuses(people)
     
 
     
