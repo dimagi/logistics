dc7fe39f8df9d4eaa9a909cad441acfffae9d36a
Christian Ternus <cternus@dimagi.com>
2011-08-24 17:50:29 -0400

    MonthPager now handles other GET parameters properly.

--- ex-submodules/rapidsms-logistics/logistics/templatetags/logistics_tags.py
+++ ex-submodules/rapidsms-logistics/logistics/templatetags/logistics_tags.py
@@ -9,3 +9,17 @@ def contact(contact):
     context['link'] = reverse('registration_edit', args=(contact.pk, ))
     context['name'] = contact.name
     return "<a href=\"%(link)s\">%(name)s (%(phone)s)</a></br>" % context
+
+@register.simple_tag
+def mp_prev_month_link(request, mp):
+    qd = request.GET.copy()
+    qd['month'] = mp.prev_month.month
+    qd['year'] = mp.prev_month.year
+    return "%s?%s" % (request.path, qd.urlencode())
+
+@register.simple_tag
+def mp_next_month_link(request, mp):
+    qd = request.GET.copy()
+    qd['month'] = mp.next_month.month
+    qd['year'] = mp.next_month.year
+    return "%s?%s" % (request.path, qd.urlencode())
