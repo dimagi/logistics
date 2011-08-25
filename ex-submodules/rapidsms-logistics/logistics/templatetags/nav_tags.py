1efabb5a836f99557a39238213a8f66a09ccfb7e
Cory Zue <czue@dimagi.com>
2011-08-25 10:43:13 -0400

    use to_function in dimagi-utils

--- ex-submodules/rapidsms-logistics/logistics/templatetags/nav_tags.py
+++ ex-submodules/rapidsms-logistics/logistics/templatetags/nav_tags.py
@@ -2,28 +2,21 @@ from django import template
 from django.conf import settings
 from logistics.util import config
 import logging
+from dimagi.utils.modules import to_function
 
 register = template.Library()
 
 def _url_generator():
     if hasattr(settings, "LOGISTICS_URL_GENERATOR_FUNCTION"):
-        try:
-            # TODO: make this less brittle if imports or args don't line up
-            module, callback = settings.LOGISTICS_URL_GENERATOR_FUNCTION.rsplit(".", 1)
-            module = __import__(module, globals(), locals(), [callback])
-            callback = getattr(module, callback)
-            return callback
-        except ImportError:
-            logging.error("problem importing url function %s" % settings.LOGISTICS_URL_GENERATOR_FUNCTION)
-    
-        
+        return to_function(settings.LOGISTICS_URL_GENERATOR_FUNCTION)
+
 @register.simple_tag
 def breadcrumbs(location, url_generator=None):
     if location is None: return ""
     if url_generator is None: 
         url_generator = _url_generator()
     
-    if url_generator is not None:
+    if url_generator is not None and url_generator(location):
         mycrumbs = '<a href="%(link)s">%(display)s</a>' % \
                     {"link": url_generator(location),
                      "display": location.name}
