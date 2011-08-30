31a02c98e5fa23ecc8804dba67bb2244924cc240
Cory Zue <czue@dimagi.com>
2011-08-30 09:44:58 -0400

    don't show alerts when there are no matching facilities

--- logistics_project/apps/tanzania/alerts.py
+++ logistics_project/apps/tanzania/alerts.py
@@ -8,93 +8,106 @@ from logistics.models import ProductStock
 from logistics.decorators import place_in_request, return_if_place_not_set
 from django.utils.translation import ugettext as _
 
-# TODO Don't show these alerts if 0 facilities fall under that category.
-
-class RandRNotSubmitted(Alert):
+class SupplyPointStatusAlert(Alert):
+    
     def __init__(self, sps):
         self.sps = sps
         self.bd = SupplyPointStatusBreakdown(facilities=sps)
-        super(RandRNotSubmitted, self).__init__(self._get_text, reverse('ordering'))
+    
+    def _get_template(self):
+        raise NotImplementedError("Subclasses should override this!")
+    
+    def get_count(self):
+        raise NotImplementedError("Subclasses should override this!")
+    
+    @property
+    def text(self):
+        return _(self._get_template()) % {'count': self.get_count()}
 
-    def _get_text(self):
-        return _('%(count)d facilities have reported not submitting their R&R form as of today.') % {'count': len(self.bd.not_submitted)}
+    @property
+    def url(self):
+        return reverse('ordering')
+    
+class RandRNotSubmitted(SupplyPointStatusAlert):
+    
+    def _get_template(self):
+        return '%(count)d facilities have reported not submitting their R&R form as of today.'
+    
+    def get_count(self):
+        return len(self.bd.not_submitted)
 
+def _alert_list(cls, facilities):
+    if facilities:
+        alert = cls(facilities)
+        if alert.get_count():
+            return [alert]
+    
+        
 @place_in_request()
 @return_if_place_not_set()
 def randr_not_submitted(request):
     facilities = request.location.all_child_facilities()
-    if not facilities:
-        return None
-    return [RandRNotSubmitted(facilities)]
-
-class RandRNotResponded(Alert):
-    def __init__(self, sps):
-        self.sps = sps
-        self.bd = SupplyPointStatusBreakdown(facilities=sps)
-        super(RandRNotResponded, self).__init__(self._get_text(), reverse('ordering'))
-
-    def _get_text(self):
-        return _('%(count)d facilities did not respond to the SMS asking if they had submitted their R&R form.') % {'count': len(self.bd.submit_not_responding)}
+    return _alert_list(RandRNotSubmitted, facilities)
+    
+class RandRNotResponded(SupplyPointStatusAlert):
+    
+    def _get_template(self):
+        return '%(count)d facilities did not respond to the SMS asking if they had submitted their R&R form.'
+    
+    def get_count(self):
+        return len(self.bd.submit_not_responding)
 
 @place_in_request()
 @return_if_place_not_set()
 def randr_not_responded(request):
     facilities = request.location.all_child_facilities()
-    if not facilities:
-        return None
-    return [RandRNotResponded(facilities)]
-
-class DeliveryNotReceived(Alert):
-    def __init__(self, sps):
-        self.sps = sps
-        self.bd = SupplyPointStatusBreakdown(facilities=sps)
-        super(DeliveryNotReceived, self).__init__(self._get_text(), reverse('ordering'))
-
-    def _get_text(self):
-        return _('%(count)d facilities have reported not receiving their deliveries as of today.') % {'count':len(self.bd.delivery_not_received)}
+    return _alert_list(RandRNotResponded, facilities)
+    
+class DeliveryNotReceived(SupplyPointStatusAlert):
+    
+    def _get_template(self):
+        return '%(count)d facilities have reported not receiving their deliveries as of today.'
+    
+    def get_count(self):
+        return len(self.bd.delivery_not_received)
 
 @place_in_request()
 @return_if_place_not_set()
 def delivery_not_received(request):
     facilities = request.location.all_child_facilities()
-    if not facilities:
-        return None
-    return [DeliveryNotReceived(facilities)]
-
-class DeliveryNotResponding(Alert):
-    def __init__(self, sps):
-        self.sps = sps
-        self.bd = SupplyPointStatusBreakdown(facilities=sps)
-        super(DeliveryNotResponding, self).__init__(self._get_text(), reverse('ordering'))
-
-    def _get_text(self):
-        return _('%(count)d facilities did not respond to the SMS asking if they had received their delivery.') % {'count': len(self.bd.delivery_not_responding)}
+    return _alert_list(DeliveryNotReceived, facilities)
+    
+class DeliveryNotResponding(SupplyPointStatusAlert):
+    
+    def _get_template(self):
+        return '%(count)d facilities did not respond to the SMS asking if they had received their delivery.'
+    
+    def get_count(self):
+        return len(self.bd.delivery_not_responding)
 
 @place_in_request()
 @return_if_place_not_set()
 def delivery_not_responding(request):
     facilities = request.location.all_child_facilities()
-    if not facilities:
-        return None
-    return [DeliveryNotResponding(facilities)]
-
-class SOHNotResponding(Alert):
-    def __init__(self, sps):
-        self.sps = sps
-        self.bd = SupplyPointStatusBreakdown(facilities=sps)
-        super(SOHNotResponding, self).__init__(self._get_text(), reverse('facilities_index'))
-
-    def _get_text(self):
-        return _('%(count)d facilities have not reported their stock levels for last month.') % {'count': len(self.bd.soh_not_responding)}
-
+    return _alert_list(DeliveryNotResponding, facilities)
+    
+class SOHNotResponding(SupplyPointStatusAlert):
+    def _get_template(self):
+        '%(count)d facilities have not reported their stock levels for last month.'
+    
+    def get_count(self):    
+        return len(self.bd.soh_not_responding)
+    
+    @property
+    def url(self):
+        return reverse('facilities_index')
+        
 @place_in_request()
 @return_if_place_not_set()
 def soh_not_responding(request):
     facilities = request.location.all_child_facilities()
-    if not facilities:
-        return None
-    return [SOHNotResponding(facilities)]
-
+    return _alert_list(SOHNotResponding, facilities)
+    
 class ProductStockout(Alert):
     def __init__(self, sp, product):
         self.sp = sp
