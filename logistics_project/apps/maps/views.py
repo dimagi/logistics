39741c3043bef7abb7368eef83eec4c842382845
Cory Zue <czue@dimagi.com>
2011-08-31 16:14:27 -0400

    attempt to show all facilities in maps views. how slow could it be?

--- logistics_project/apps/maps/views.py
+++ logistics_project/apps/maps/views.py
@@ -12,7 +12,7 @@ def dashboard(request, location_code=None):
     # whole country
     # supply_points = SupplyPoint.objects.exclude(location__point=None)
     location = get_object_or_404(Location, code=location_code)
-    supply_points = location.child_facilities().exclude(location__point=None)
+    supply_points = location.all_facilities().exclude(location__point=None)
     return render_to_response("maps/dashboard.html",
                               {"supply_points": supply_points,
                                "default_latitude":  settings.MAP_DEFAULT_LATITUDE,
