6f1bfd730d4b0fa9c7e37ebca14b3427ec98ba29
Cory Zue <czue@dimagi.com>
2011-09-07 17:38:40 -0400

    raise scheduler exceptions up the stack so they can be escalated

--- ex-submodules/django-scheduler/scheduler/tasks.py
+++ ex-submodules/django-scheduler/scheduler/tasks.py
@@ -12,6 +12,7 @@ def scheduler_heartbeat():
     Check all scheduled tasks and run whatever is necessary.
     """
     # this should get called every minute by celery
+    failures = []
     currtime = datetime.utcnow()
     relevant = get_relevant_schedules(currtime)
     for schedule in relevant:
@@ -19,6 +20,10 @@ def scheduler_heartbeat():
             # call the callback function
             # possibly passing in args and kwargs
             schedule.run(currtime)
-        except Exception:
+        except Exception, e:
             # Don't prevent exceptions from killing the rest
-            logging.exception("Problem in the scheduler heartbeat for %s" % schedule)            
+            logging.exception("Problem in the scheduler heartbeat for %s" % schedule)
+            failures.append(e)
+    if failures:            
+        raise Exception("Failed to process all celery tasks, errors are: \n%s" % 
+                        "\n".join(["(%(type)s: %(val)s" % {"type": type(e), "val": str(e)} for e in failures])
