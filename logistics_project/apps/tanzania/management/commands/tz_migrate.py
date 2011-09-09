38b4309d3952f205b4896ff2e76c24a424c7e5c9
Cory Zue <czue@dimagi.com>
2011-09-09 16:27:15 -0400

    add push migration files

--- logistics_project/apps/tanzania/management/commands/tz_migrate.py
+++ logistics_project/apps/tanzania/management/commands/tz_migrate.py
@@ -14,21 +14,8 @@ from dimagi.utils.django.management import are_you_sure
 import os
 import sys
 from dimagi.utils.parsing import string_to_datetime
-
-def unicode_csv_reader(unicode_csv_data, dialect=csv.excel, **kwargs):
-    # csv.py doesn't do Unicode; encode temporarily as UTF-8:
-    csv_reader = csv.reader(utf_8_encoder(unicode_csv_data),
-                            dialect=dialect, **kwargs)
-    for row in csv_reader:
-        # decode UTF-8 back to Unicode, cell by cell:
-        yield [unicode(cell, 'utf-8') for cell in row]
-def utf_8_encoder(unicode_csv_data):
-    for line in unicode_csv_data:
-        try:
-            yield line.encode('utf-8')
-        except UnicodeDecodeError:
-            print line
-            continue
+from logistics_project.apps.tanzania.migration import check_router,\
+    unicode_csv_reader
 
 def get_supply_point(name, code, type):
     """
@@ -182,20 +169,6 @@ class Command(BaseCommand):
                         break
             print "processed %s incoming and %s outgoing messages" % (inbound_count, outbound_count)
             
-        def check_router():
-            # quasi-stolen from the httptester
-            try:
-                utils.check_status()
-                return True
-            except RouterNotResponding:
-                print "The router is not available! Remember to start the rapidsms " \
-                  "router before running the migration. FAIL."
-            except RouterError:
-                print "The router appears to be running but returned an error. " \
-                      "Is the migration app in your installed_apps list? and " \
-                      "have you enabled the migration backend?" 
-                return False
-        
         def clean_migration_backends():
             migration_connections = Connection.objects.filter(backend__name="migration")
             push_backend = Backend.objects.get_or_create(name="push_backend")[0]
