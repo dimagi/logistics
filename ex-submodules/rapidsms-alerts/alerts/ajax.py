02b8c2fc45987007d803b446c11486d1bd8d4658
Drew Roos <droos@dimagi.com>
2011-09-19 16:13:00 -0400

    catch attempts to add comments w/o logged-in session

--- ex-submodules/rapidsms-alerts/alerts/ajax.py
+++ ex-submodules/rapidsms-alerts/alerts/ajax.py
@@ -2,14 +2,18 @@ from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound
 from django.shortcuts import render_to_response, get_object_or_404, redirect
 from models import Notification, NotificationComment
 import json
+from django.core.exceptions import SuspiciousOperation
 
 def add_comment(request):
     alert_id = request.POST.get('alert_id')
     text = request.POST.get('text')
+    user = request.user
+    if not user.is_authenticated():
+        raise SuspiciousOperation('attempt to add comment w/o authenticated user')
 
     comment = NotificationComment(
         notification=Notification.objects.get(id=alert_id),
-        user=request.user,
+        user=user,
         text=text
     )
     comment.save()
