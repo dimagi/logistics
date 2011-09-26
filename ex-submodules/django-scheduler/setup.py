65a93ce4b404f688de4754c7c86b0cc947cf6791
Cory Zue <czue@dimagi.com>
2011-09-26 11:20:00 -0400

    add setup.py

---
+++ ex-submodules/django-scheduler/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+setup(
+    name="scheduler",
+    version="0.0.1",
+    license="BSD",
+
+    install_requires = [
+        "django",
+        "django-celery",
+        "dimagi-utils",
+    ],
+
+    packages = find_packages(exclude=['*.pyc']),
+    include_package_data=True,
+
+    author="Rowena Luk, Cory Zue",
+    author_email="information@dimagi.com",
+
+    description="Database backed scheduler for Django",
+    url="http://github.com/dimagi/django-scheduler"
+)
