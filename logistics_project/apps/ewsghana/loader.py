db9de46d1790f0c2cb059493eb2b315a8848bed0
Rowena <rluk@dimagi.com>
2011-10-13 14:56:34 -0700

    pull in changes from logistics/ghana-master

--- logistics_project/apps/ewsghana/loader.py
+++ logistics_project/apps/ewsghana/loader.py
@@ -245,7 +245,7 @@ def init_reminders():
         EventSchedule.objects.get(callback="logistics.schedule.reminder_to_submit_RRIRV")
     except EventSchedule.DoesNotExist:
         # 2:15 pm on the 28th
-        set_monthly_event("logistics.schedule.reminder_to_submit_RRIRV",28,14,15)
+        set_monthly_event("logistics.schedule.reminder_to_submit_RRIRV",7,14,15)
         #EventSchedule.objects.create(callback="logistics.schedule.reminder_to_submit_RRIRV", 
         #                             minutes='*')
         
