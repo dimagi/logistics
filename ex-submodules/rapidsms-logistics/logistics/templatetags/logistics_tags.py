51b3dd0ef71e3f076daa76412ede1b1d60271a28
Rowena <rluk@dimagi.com>
2011-10-31 15:49:01 -0700

    add templatetag and middleware to enable just-in-time rendering of logged-in username

--- ex-submodules/rapidsms-logistics/logistics/templatetags/logistics_tags.py
+++ ex-submodules/rapidsms-logistics/logistics/templatetags/logistics_tags.py
@@ -29,3 +29,29 @@ def url_get_replace(request, a, b):
     qd = request.GET.copy()
     qd[a] = b
     return "%s?%s" % (request.path, qd.urlencode())
+
+def raw(parser, token):
+    # ro: thanks to EveryBlock for this code snippet
+    # Whatever is between {% raw %} and {% endraw %} will skip the cache
+    # so that e.g. 'usernames' can be refreshed while reports remain cached
+    text = []
+    parse_until = 'endraw'
+    tag_mapping = {
+        template.TOKEN_TEXT: ('', ''),
+        template.TOKEN_VAR: ('{{', '}}'),
+        template.TOKEN_BLOCK: ('{%', '%}'),
+        template.TOKEN_COMMENT: ('{#', '#}'),
+    }
+    # By the time this template tag is called, the template system has already
+    # lexed the template into tokens. Here, we loop over the tokens until
+    # {% endraw %} and parse them to TextNodes. We have to add the start and
+    # end bits (e.g. "{{" for variables) because those have already been
+    # stripped off in a previous part of the template-parsing process.
+    while parser.tokens:
+        token = parser.next_token()
+        if token.token_type == template.TOKEN_BLOCK and token.contents == parse_until:
+            return template.TextNode(u''.join(text))
+        start, end = tag_mapping[token.token_type]
+        text.append(u'%s%s%s' % (start, token.contents, end))
+    parser.unclosed_block_tag(parse_until)
+raw = register.tag(raw)
