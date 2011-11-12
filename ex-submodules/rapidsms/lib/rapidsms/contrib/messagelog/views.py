ea45aee1bb03e86326f3e579295c17e7542f8e25
Cory Zue <czue@dimagi.com>
2011-11-12 14:31:11 -0500

    support searching by phone number

--- ex-submodules/rapidsms/lib/rapidsms/contrib/messagelog/views.py
+++ ex-submodules/rapidsms/lib/rapidsms/contrib/messagelog/views.py
@@ -9,6 +9,7 @@ from .tables import MessageTable
 from .models import Message
 from taggit.models import Tag, TaggedItem
 import re
+from django.db.models import Q
 
 def message_log(req, template="messagelog/index.html"):
     messages = Message.objects.all()
@@ -29,7 +30,8 @@ def message_log(req, template="messagelog/index.html"):
 
     if 'search' in req.GET and req.GET['search'] != '':
         search = req.GET['search']
-        messages = messages.filter(text__iregex=re.escape(search))
+        messages = messages.filter(Q(text__iregex=re.escape(search)) |\
+                                   Q(connection__identity__iregex=re.escape(search)))
     
     # Extract and sort all tag names
     for tag in TaggedItem.tags_for(Message):
