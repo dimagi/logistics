68764cfae613fdcb2ebc7e865790a27a80a4eeac
Christian Ternus <ternus@mit.edu>
2011-12-05 22:33:25 -0500

    Fix default formatting in daterange.js.

--- ex-submodules/rapidsms-logistics/logistics/static/javascripts/daterange.js
+++ ex-submodules/rapidsms-logistics/logistics/static/javascripts/daterange.js
@@ -4,12 +4,11 @@ $(function() {
         changeMonth: true,
         maxDate: "+0d",
         numberOfMonths: 2,
+        dateFormat: "yy-mm-dd",
         onSelect: function( selectedDate ) {
             var option = this.id == "from" ? "minDate" : "maxDate",
                 instance = $( this ).data( "datepicker" ),
-                date = $.datepicker.parseDate(
-                    instance.settings.dateFormat ||
-                    $.datepicker._defaults.dateFormat,
+                date = $.datepicker.parseDate("yy-mm-dd",
                     selectedDate, instance.settings );
             dates.not( this ).datepicker( "option", option, date );
         }
