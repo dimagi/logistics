df9d36cd49aad387991206d9ce018a0b46450ead
Rowena <rluk@dimagi.com>
2012-01-10 16:04:48 -0500

    Revert "add more autoconsumption unit tests"
    This reverts commit 0b2293fbd1eba2db6320c61f3d20317d8c81e0a0.

--- ex-submodules/rapidsms-logistics/logistics/tests/consumption.py
+++ ex-submodules/rapidsms-logistics/logistics/tests/consumption.py
@@ -22,13 +22,12 @@ class TestConsumption (TestScript):
         self.ps = self._report(30, 30, Reports.SOH) 
         self.ps = self._report(20, 20, Reports.SOH) 
         self.ps = self._report(10, 10, Reports.SOH) 
-        self.assertEquals(1, round(self.ps.daily_consumption))
+        self.assertEquals(1, self.ps.daily_consumption)
         self.ps.use_auto_consumption = False
-        self.assertEquals(5, round(self.ps.monthly_consumption))
+        self.assertEquals(5, self.ps.monthly_consumption)
         self.ps.use_auto_consumption = True
-        self.assertEquals(1, round(self.ps.daily_consumption))
-        self.assertEquals(30, round(self.ps.monthly_consumption))
-    
+        self.assertEquals(30, self.ps.monthly_consumption)
+
     def testConsumption(self):
         self.sp.report_stock(self.pr, 200) 
 
@@ -56,8 +55,8 @@ class TestConsumption (TestScript):
 
         # 10 days is enough.
         self.ps = ProductStock.objects.get(supply_point=self.sp, product=self.pr)
-        self.assertEquals(10, round(self.ps.daily_consumption)) # 200 in stock 10 days ago, 150 in stock 5 days ago
-        self.assertEquals(300, round(self.ps.monthly_consumption))
+        self.assertEquals(10, self.ps.daily_consumption) # 200 in stock 10 days ago, 150 in stock 5 days ago
+        self.assertEquals(300, self.ps.monthly_consumption)
 
         sta = StockTransaction.objects.filter(supply_point=self.sp,product=self.pr)
         for st in sta:
@@ -92,6 +91,46 @@ class TestConsumption (TestScript):
         self.ps = ProductStock.objects.get(supply_point=self.sp, product=self.pr)
         self.assertEquals(7.5, round(self.ps.daily_consumption, 1))
 
+    def testConsumptionWithMultipleReports(self):
+        self.ps = ProductStock.objects.get(supply_point=self.sp, 
+                                           product=self.pr)
+        self.ps.use_auto_consumption = True
+        self.ps.save()
+                    
+        self.ps = self._report(40, 60, Reports.SOH) # 60 days ago we had 40 in stock. 
+        self.assertEquals(None, self.ps.daily_consumption)
+
+        self.ps = self._report(30, 50, Reports.REC) # 50 days ago, we received 30
+        self.assertEquals(None, self.ps.daily_consumption) # consumption unchanged
+
+        self.ps = self._report(20, 50, Reports.SOH) # 50 days ago, we had 20 in stock (so we've consumed 50)
+        self.assertEquals(5, self.ps.daily_consumption) # 50/10 days
+
+        self.ps = self._report(10, 40, Reports.SOH) # 40 days ago we had 10 in stock (so we've consume 60 over 20 days)
+        self.assertEquals(3, self.ps.daily_consumption)
+
+    def testFloatingPointAccuracy(self):
+        self.ps = self._report(10, 51, Reports.REC) 
+        self.assertEquals(None, self.ps.daily_consumption) # consumption unchanged
+
+        self.ps = self._report(50, 50, Reports.SOH) 
+        self.assertEquals(None, self.ps.daily_consumption)
+
+        self.ps = self._report(0, 41, Reports.REC)
+        self.assertEquals(None, self.ps.daily_consumption) # consumption unchanged
+
+        self.ps = self._report(30, 40, Reports.SOH) 
+        self.assertEquals(2, round(self.ps.daily_consumption)) # 20/10 days
+        self.assertEquals(54, round(self.ps.monthly_consumption))
+
+        self.ps = self._report(10, 31, Reports.REC) 
+        self.assertEquals(2, round(self.ps.daily_consumption)) # consumption unchanged
+        self.assertEquals(54, round(self.ps.monthly_consumption))
+
+        self.ps = self._report(20, 30, Reports.SOH)
+        self.assertEquals(2, round(self.ps.daily_consumption)) # 40/20
+        self.assertEquals(57, round(self.ps.monthly_consumption))
+
     def testAutoVsManualConsumption(self):
         # test all combinations of use_auto_consumption, 
         # manual_consumption, and auto_consumption
@@ -121,154 +160,21 @@ class TestConsumption (TestScript):
         self.ps.unset_auto_consumption()
         self.assertEquals(8, self.ps.monthly_consumption)
 
-    def testFloatingPointAccuracy(self):
-        self.ps = self._report(50, 50, Reports.SOH) 
-        self.ps = self._report(49, 49, Reports.SOH) 
-        self.ps = self._report(48, 48, Reports.SOH) 
-        self.ps = self._report(40, 40, Reports.SOH) 
-        self.assertEquals(1.0, round(self.ps.daily_consumption)) 
-        self.ps = self._report(38, 39, Reports.SOH) 
-        self.ps = self._report(36, 38, Reports.SOH) 
-        self.assertEquals(1.17, round(self.ps.daily_consumption,2)) 
-        
-    def testConsumptionWithMultipleReports(self):
-        self.ps = ProductStock.objects.get(supply_point=self.sp, 
-                                           product=self.pr)
-        self.ps.use_auto_consumption = True
-        self.ps.save()
-        self.ps = self._report(80, 80, Reports.SOH)
-        self.ps = self._report(70, 70, Reports.SOH)
-        self.ps = self._report(65, 65, Reports.SOH)
-        self.ps = self._report(60, 60, Reports.SOH)
-        self.assertEquals(1, round(self.ps.daily_consumption))
-        self.ps = self._report(20, 55, Reports.REC) # 50 days ago, we received 30
-        self.assertEquals(1, self.ps.daily_consumption) # consumption unchanged
-        self.ps = self._report(40, 50, Reports.SOH) # 50 days ago, we had 20 in stock (so we've consumed 50)
-        self.assertEquals(2, self.ps.daily_consumption) # 50/10 days
-
-    def testIgnorePeriodsOfStockout(self):
-        self.ps = ProductStock.objects.get(supply_point=self.sp, 
-                                           product=self.pr)
-        self.ps.use_auto_consumption = True
-        self.ps.save()
-                    
-        self.ps = self._report(100, 100, Reports.SOH) 
-        self.ps = self._report(90, 90, Reports.SOH) 
-        self.ps = self._report(80, 80, Reports.SOH) 
-        self.assertEquals(1, self.ps.daily_consumption) 
-        self.ps = self._report(0, 70, Reports.SOH) 
-        self.ps = self._report(0, 60, Reports.SOH) 
-        self.assertEquals(1, self.ps.daily_consumption) # consumption unchanged after stockouts
-        self.ps = self._report(70, 70, Reports.SOH) 
-        self.ps = self._report(20, 60, Reports.SOH) 
-        self.assertEquals(2, round(self.ps.daily_consumption)) # consumption changed
-
-    def testConsumptionCalculationIntervals(self):
-        self.ps = ProductStock.objects.get(supply_point=self.sp, 
-                                           product=self.pr)
-        self.ps.use_auto_consumption = True
-        self.ps.save()
-                    
-        self.ps = self._report(100, 100, Reports.SOH)
-        self.ps = self._report(5, 91, Reports.REC)
-        self.ps = self._report(95, 90, Reports.SOH)
-        self.ps = self._report(5, 81, Reports.REC)
-        self.assertEquals(None, self.ps.daily_consumption) # not enough data
-        self.ps = self._report(90, 80, Reports.SOH)
-        self.assertEquals(1, self.ps.daily_consumption)
-        self.ps = self._report(5, 71, Reports.REC)
-        self.ps = self._report(85, 70, Reports.SOH)
-        self.assertEquals(1, self.ps.daily_consumption)
-        self.ps = self._report(5, 61, Reports.REC)
-        self.ps = self._report(80, 60, Reports.SOH)
-        self.assertEquals(1, self.ps.daily_consumption)
-    
-    def testMoreComplicatedStockoutExample(self):
-        self.ps = ProductStock.objects.get(supply_point=self.sp, 
-                                           product=self.pr)
-        self.ps.use_auto_consumption = True
-        self.ps.save()
-                    
-        self.ps = self._report(10, 100, Reports.SOH) # 100 days ago we had 10 in stock. 
-        self.assertEquals(None, self.ps.daily_consumption) # not enough data
-        self.ps = self._report(5, 95, Reports.REC) 
-        self.assertEquals(None, self.ps.daily_consumption) # not enough data
-        self.ps = self._report(10, 95, Reports.REC) 
-        self.assertEquals(None, self.ps.daily_consumption) # not enough data
-        self.ps = self._report(5, 90, Reports.REC) 
-        self.assertEquals(None, self.ps.daily_consumption) # not enough data
-
-
-        # 90 days ago, we have 10 in stock 
-        self.ps = self._report(10, 90, Reports.SOH) 
-        self.assertEquals(2, self.ps.daily_consumption) # 10 stock/10 days
-        
-        # 80 days ago we had 0 in stock
-        self.ps = self._report(0, 80, Reports.SOH) 
-        self.assertEquals(2, self.ps.daily_consumption)
-        # no matter how much we receive, as long as soh remains 0, nothing changes
-        self.ps = self._report(50, 80, Reports.REC) 
-        self.assertEquals(2, self.ps.daily_consumption)
-        self.ps = self._report(100, 70, Reports.REC) 
-        self.assertEquals(2, self.ps.daily_consumption)
-        self.ps = self._report(30000, 60, Reports.REC) 
-        self.assertEquals(2, self.ps.daily_consumption)
-        self.ps = self._report(0, 50, Reports.SOH) 
-        self.assertEquals(2, self.ps.daily_consumption)
-        
-        # only once the stockout is resolved do consumption update again
-        self.ps = self._report(1, 50, Reports.SOH) 
-        self.assertEquals(2, self.ps.daily_consumption)
-        self.ps = self._report(191, 40, Reports.REC) 
-        self.assertEquals(2, self.ps.daily_consumption)
-        self.ps = self._report(1, 40, Reports.SOH) 
-        self.assertEquals(10, self.ps.daily_consumption) # 200 / 20 days
-                
-    def testNonReporting(self):
-        """ For now, we don't do anything special about non-reporting"""
-        self.ps = ProductStock.objects.get(supply_point=self.sp, 
-                                           product=self.pr)
-        self.ps.use_auto_consumption = True
-        self.ps.save()
-                    
-        self.ps = self._report(100, 100, Reports.SOH) # 100 days ago we had 10 in stock. 
-        self.assertEquals(None, self.ps.daily_consumption) # not enough data
-        self.ps = self._report(10, 90, Reports.REC) # 90 days ago, we received 10
-        self.assertEquals(None, self.ps.daily_consumption) # not enough data
-        # 90 days ago, we have 10 in stock 
-        self.ps = self._report(100, 90, Reports.SOH) 
-        self.assertEquals(1, self.ps.daily_consumption) # 10 stock/10 days
-        
-        # even though we received no reports between day 90 and day 50
-        # the consumption gets updated all the same
-        self.ps = self._report(490, 50, Reports.REC) 
-        self.ps = self._report(100, 60, Reports.SOH) 
-        self.assertEquals(10, self.ps.daily_consumption)
-    
     def _report(self, amount, days_ago, report_type):
         if report_type == Reports.SOH:
             report = self.sp.report_stock(self.pr, amount)
         else:
             report = self.sp.report_receipt(self.pr, amount)
-        # must call post_save manually since signals don't work 
-        # properly in the test environment
-        #report.post_save()
-        txs = StockTransaction.objects.filter(product_report=report).order_by('-pk')
         try:
-            trans = txs[0]
-        except IndexError:
-            # no big deal; it wasn't a transaction-generating report
+            trans = StockTransaction.objects.get(product_report=report)
+        except StockTransaction.DoesNotExist:
             pass
         else:
             trans.date = trans.date - timedelta(days=days_ago)
             trans.save()
-        # NB: it is important that we draw this from the database since the automatic
-        # stock calculation works by updating the productstock.auto_monthly_consumption field
         self.ps = ProductStock.objects.get(supply_point=self.sp, product=self.pr)
-        # again, this should be called by a signal, 
-        # but in the unit test we do it manually
-        #self.ps.update_auto_consumption()
         return self.ps
+
 
     def tearDown(self):
         Location.objects.all().delete()
