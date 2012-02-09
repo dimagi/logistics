ab5942e49d22b1499f543d07a97a49abaa1d88a4
Rowena <rluk@dimagi.com>
2012-02-09 16:31:37 -0200

    add a global settings to specify how deployments manage the required set of commodities which facilities are responsible for reporting (options: USER on the basis of users associated, FACILITY on the basis of active productstocks, and PRODUCT on the basis of active products)

--- ex-submodules/rapidsms-logistics/logistics/settings.py
+++ ex-submodules/rapidsms-logistics/logistics/settings.py
@@ -20,3 +20,9 @@ LOGISTICS_USE_LOCATION_SESSIONS = False # keep persistent locations across reque
 LOGISTICS_NAVIGATION_MODE = "url" # "url" or "param", depending how your site navigation works
 LOGISTICS_USE_SPOT_CACHING = False # use spot caches in various places we've found performance hits
 LOGISTICS_SPOT_CACHE_TIMEOUT = 60 * 60 # spot cache timeout, in seconds, defaults to an hour
+class StockedBy:
+    # this is the set of allowable values for STOCKED_BY
+    USER='user' # sp's are responsible for reporting commodities registered to specific users
+    FACILITY='facility' # sp's are respnsible for reporting commodities registered to specific facilities
+    PRODUCT='product' # sp's are responsible for reporting commodities marked as 'is_active'
+LOGISTICS_STOCKED_BY = StockedBy.USER
