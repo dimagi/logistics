c1344df9a0acd898c41b1a99722a9dc8f26267f0
Cory Zue <czue@dimagi.com>
2012-07-26 16:02:36 -0400

    fleshing out reporting models, with annotated comments of what they'll do

--- logistics_project/apps/malawi/warehouse_models.py
+++ logistics_project/apps/malawi/warehouse_models.py
@@ -9,6 +9,10 @@ class ProductAvailabilityData(ReportingModel):
     as well as anything that needs to compute percent with / without 
     stock, oversupplied, undersupplied, etc.
     """
+    # Sidebar: % with stockout
+    # Dashboard: % with stockout, current stock status
+    # Resupply Qts: % with stockout
+    # Stock status: all
     product = models.ForeignKey(Product)
     total = models.PositiveIntegerField(default=0)
     with_stock = models.PositiveIntegerField(default=0)
@@ -17,3 +21,52 @@ class ProductAvailabilityData(ReportingModel):
     without_stock = models.PositiveIntegerField(default=0)
     without_data = models.PositiveIntegerField(default=0)
 
+class ReportingRate(ReportingModel):
+    """
+    Records information used to calculate the reporting rates
+    """
+    # Dashboard: Reporting Rates
+    # Reporting Rates: all
+    total = models.PositiveIntegerField(default=0)
+    on_time = models.PositiveIntegerField(default=0)
+    late = models.PositiveIntegerField(default=0)
+
+class TimeTracker(ReportingModel):
+    """
+    For keeping track of a time between two events. Currently used for 
+    lead times. We keep the number of data points around so that we can
+    include multiple values into an average.
+    """
+    # Lead times: all 
+    type = models.CharField(max_length=10) # e.g. ord-ready
+    total = models.PositiveIntegerField(default=0) # number of contributions to this
+    time_in_seconds = models.PositiveIntegerField(default=0)
+    
+class OrderRequest(ReportingModel):
+    """
+    Each time an order is made, used to count both regular and emergency
+    orders for a particular month.
+    """
+    # Emergency Orders: all
+    product = models.ForeignKey(Product)
+    total = models.PositiveIntegerField(default=0)
+    emergency = models.PositiveIntegerField(default=0)
+    
+    
+class OrderFulfillment(ReportingModel):
+    """
+    Each time an order is fulfilled, add up the amount requested and
+    the amount received so we can determine order fill rates.
+    """
+    # Order Fill Rates: all 
+    product = models.ForeignKey(Product)
+    total = models.PositiveIntegerField(default=0)
+    quantity_requested = models.PositiveIntegerField(default=0)
+    quantity_received = models.PositiveIntegerField(default=0)
+
+# Other:
+# User Profiles (no changes needed)
+# HSA (no changes needed)
+# Consumption Profiles (likely changes needed, to be clarified)
+# Resupply Qts: anything needed? TBD
+# Alerts: TBD
