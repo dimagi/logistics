ef409e21f9d02b2ffbc5c48c610c594808802048
Cory Zue <czue@dimagi.com>
2012-08-02 15:11:08 +0200

    finish stock status report.
    - updated line charts template to be more customizable
    - add product type selector
    - might break other line charts

--- logistics_project/apps/malawi/warehouse/report_views/stock_status.py
+++ logistics_project/apps/malawi/warehouse/report_views/stock_status.py
@@ -1,20 +1,32 @@
 from random import random
+from datetime import datetime
+from collections import defaultdict
+
 from logistics_project.apps.malawi.util import get_country_sp, fmt_pct,\
-    get_district_supply_points
+    get_district_supply_points, pct
 from logistics_project.apps.malawi.warehouse.models import ProductAvailabilityData,\
     ProductAvailabilityDataSummary
 
-from logistics.models import Product, SupplyPoint
+from logistics.models import Product, SupplyPoint, ProductType
 
 from logistics_project.apps.malawi.warehouse import warehouse_view
+from dimagi.utils.dates import months_between
+import json
 
 class View(warehouse_view.MalawiWarehouseView):
 
     def get_context(self, request):
-        ret_obj = {}
+        ret_obj = {
+            'product_types': ProductType.objects.all()
+        }
+        selected_type = ProductType.objects.get(code=request.GET["product-type"]) \
+            if "product-type" in request.GET else None
+        
+        ret_obj['selected_type'] = selected_type
         
         sp = SupplyPoint.objects.get(location=request.location) \
             if request.location else get_country_sp()
+        
         # Correct
         # date = current_report_period()
         date = request.datespan.enddate # testing
@@ -29,7 +41,6 @@ class View(warehouse_view.MalawiWarehouseView):
         p_pad_tuples = [(p, ProductAvailabilityData.objects.get\
                                 (supply_point=sp, date=date, product=p)) \
                         for p in Product.objects.all().order_by('sms_code')]
-        
         product_data = [[p.sms_code] + \
                         [fmt_pct(getattr(pad, "managed_and_%s" % k), pad.managed) \
                          for k in ordered_slugs] \
@@ -56,22 +67,32 @@ class View(warehouse_view.MalawiWarehouseView):
             "data": district_data,
         }
         
-        table2 = {
-            "title": "HSA Current Stock Status by District",
-            "header": ["District"] + headings,
-            "data": [['cc', 33, 45, 52, 31], ['dt', 21, 29, 45, 13], ['sr', 43, 42, 42, 61]],
-        }
-        line_chart = {
+        # product line chart 
+        
+        products = Product.objects.filter(type=selected_type) if selected_type else \
+            Product.objects.all()
+        data = defaultdict(lambda: defaultdict(lambda: 0)) 
+        dates = [datetime(year, month, 1) for year, month in \
+                    months_between(request.datespan.startdate, 
+                                   request.datespan.enddate)]
+        for p in products:
+            for dt in dates:
+                pad = ProductAvailabilityData.objects.get\
+                    (supply_point=sp, product=p, date=dt)
+                data[p][dt] = pct(pad.managed_and_without_stock, pad.managed)
+        
+        graph_data = [{'data': [[i + 1, data[p][dt]] for i, dt in enumerate(dates)],
+                       'label': p.sms_code, 'lines': {"show": True}, 
+                       "bars": {"show": False}} \
+                       for p in products]
+        ret_obj['graphdata'] = {
+            "div": "product-stockouts-chart",
+            "legenddiv": "product-stockouts-chart-legend",
+            "legendcols": 10,
+            "max_value": 100,
             "height": "350px",
             "width": "100%", # "300px",
-            "series": [],
+            "xlabels": [[i + 1, '%s' % dt.strftime("%b")] for i, dt in enumerate(dates)],
+            "data": json.dumps(graph_data),
         }
-        for j in ['LA 1x6', 'LA 2x6']:
-            temp = []
-            for i in range(0,5):
-                temp.append([random(),random()])
-            line_chart["series"].append({"title": j, "data": sorted(temp)})
-
-        ret_obj['table2'] = table2
-        ret_obj['line'] = line_chart
         return ret_obj
