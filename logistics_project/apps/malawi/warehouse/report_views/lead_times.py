7ac02016cc34ae596b6f0d8c72bab386355e3534
Cory Zue <czue@dimagi.com>
2012-08-03 11:17:12 +0200

    implment lead times report

--- logistics_project/apps/malawi/warehouse/report_views/lead_times.py
+++ logistics_project/apps/malawi/warehouse/report_views/lead_times.py
@@ -1,57 +1,97 @@
-def timechart(labels):
-    summary = {
-        "number": 3,
-        "xlabels": [],
-        "legenddiv": "legend-div",
-        "div": "chart-div",
-        "max_value": 3,
-        # "width": "730px",
-        "width": "100%",
-        "height": "200px",
-        "data": [],
-        "xaxistitle": "month",
-        "yaxistitle": "rate"
-    }
-    count = 0
-    summary['xlabels'] = month_labels(datetime.now() - timedelta(days=61), datetime.now())
-    summary['data'] = barseries(labels, len(summary['xlabels']))
-    return summary
+from collections import defaultdict
+from datetime import datetime
 
-
-def barseries(labels, num_points):
-    return [{"label": l, "data": bardata(num_points)} for l in labels]
-    
-def bardata(num_points):
-    return [[i + 1, random()] for i in range(num_points)]
-
-from random import random
-
-#^^^^^^^^^^^junk
-
-from datetime import datetime, timedelta
+from logistics.models import SupplyPoint
 
 from dimagi.utils.dates import months_between
 
-from logistics_project.apps.malawi.warehouse.report_utils import month_labels
 from logistics_project.apps.malawi.warehouse import warehouse_view
+from logistics_project.apps.malawi.util import get_country_sp,\
+    facility_supply_points_below
+from static.malawi.config import TimeTrackerTypes
+from logistics_project.apps.malawi.warehouse.models import TimeTracker,\
+    TIME_TRACKER_TYPES
+import json
+from django.db.models import Sum
 
 
 class View(warehouse_view.MalawiWarehouseView):
 
     def get_context(self, request):
+        sp = SupplyPoint.objects.get(location=request.location) \
+            if request.location else get_country_sp()
+        
+        data = defaultdict(lambda: defaultdict(lambda: 0)) # turtles!
+        dates = [datetime(year, month, 1) for year, month in \
+                 months_between(request.datespan.startdate, 
+                                request.datespan.enddate)]
+        
+        for dt in dates:
+            for code, name in TIME_TRACKER_TYPES:
+                data[code][dt] = TimeTracker.objects.get\
+                    (supply_point=sp, date=dt, type=code).avg_time_in_days
+            
+        ret_data = [{'data': [[i + 1, data[code][dt]] for i, dt in enumerate(dates)],
+                     'label': name, 'lines': {"show": False}, "bars": {"show": True},
+                     'stack': 0} \
+                     for code, name in TIME_TRACKER_TYPES]
+        report_chart = {
+            "legenddiv": "leadtime-legend-div",
+            "div": "leadtime-chart-div",
+            "width": "100%",
+            "height": "200px",
+            "xaxistitle": "month",
+            "xlabels": [[i + 1, '%s' % dt.strftime("%b")] for i, dt in enumerate(dates)],
+            "data": json.dumps(ret_data)
+        }
+        
+        def _table_fmt(val):
+            if val is not None:
+                return "%.1f" % val
+            return "No data"
+        
+        rows = []
+        for dt in dates:
+            or_days = data[TimeTrackerTypes.ORD_READY][dt] 
+            rr_days = data[TimeTrackerTypes.READY_REC][dt] 
+            tot_days = or_days + rr_days if or_days is not None and rr_days is not None else None
+            rows.append([dt.strftime("%b")] + [_table_fmt(val) for val in \
+                                               (or_days, rr_days, tot_days)])
         month_table = {
             "id": "month-table",
             "is_datatable": False,
-            "header": ['Month', 'Ord-Ord Ready (days)', 'Ord-Ord Received(days)', 'Total Lead Time (days)'],
-            "data": [['Jan', 3, 14, 7], ['Feb', 12, 7, 4], ['Mar', 14, 6, 4]],
+            "title": "",
+            "header": ['Month', 'Ord-Ord Ready (days)', 'Ord-Ord Received (days)', 
+                       'Total Lead Time (days)'],
+            "data": rows
         }
-
+        
+        def _to_days(secs, count):
+            if count:
+                return float(secs) / float(count * 60 * 60 * 24)
+            return None
+        
+        f_data = []
+        for f in facility_supply_points_below(sp.location).order_by('name'):
+            matching = TimeTracker.objects.filter(supply_point=f,
+                                                  date__gte=request.datespan.startdate,
+                                                  date__lte=request.datespan.enddate)
+            or_tots = matching.filter(type=TimeTrackerTypes.ORD_READY).aggregate\
+                (Sum('total'), Sum('time_in_seconds'))
+            avg_or_lt = _to_days(or_tots["time_in_seconds__sum"], or_tots["total__sum"])
+            rr_tots = matching.filter(type=TimeTrackerTypes.READY_REC).aggregate\
+                (Sum('total'), Sum('time_in_seconds'))
+            avg_rr_lt = _to_days(rr_tots["time_in_seconds__sum"], rr_tots["total__sum"])
+            avg_tot_lt = avg_or_lt + avg_rr_lt if avg_or_lt is not None and avg_rr_lt is not None else None
+            f_data.append([f.name, len(dates)] + [_table_fmt(val) for val in \
+                                      (avg_or_lt, avg_rr_lt, avg_tot_lt)])
+            
         lt_table = {
             "id": "average-lead-times-facility",
             "is_datatable": False,
             "header": ['Facility', 'Period (# Months)', 'Ord-Ord Ready (days)', 'Ord-Ord Received(days)', 'Total Lead Time (days)'],
-            "data": [['BULA', 6, 31, 42, 37], ['Chesamu', 6, 212, 27, 14], ['Chikwina', 6, 143, 61, 14]],
+            "data": f_data,
         }    
-        return {"summary": timechart(['Ord-Ord Ready', 'Ord-Ord Received']),
+        return {"graphdata": report_chart,
                 "month_table": month_table,
                 "lt_table": lt_table}
