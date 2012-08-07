4cb527c2b10f378ebdc0850c8b835c69fe9f5c20
Cory Zue <czue@dimagi.com>
2012-08-07 17:01:15 +0200

    add managed_supply_points to organization model

--- logistics_project/apps/malawi/models.py
+++ logistics_project/apps/malawi/models.py
@@ -2,13 +2,15 @@ from django.db import models
 
 
 class Organization(models.Model):
-	"""
-	An organization. Used for reporting purposes. For now contacts  	
-	may belong to at most 1 organization.
-	"""
-	name = models.CharField(max_length=128)
+    """
+    An organization. Used for reporting purposes. For now contacts  	
+    may belong to at most 1 organization.
+    """
+    name = models.CharField(max_length=128)
+    managed_supply_points = models.ManyToManyField("logistics.SupplyPoint", 
+                                                   null=True, blank=True)
 
-	def __unicode__(self):
-		return self.name
+    def __unicode__(self):
+        return self.name
 
 from .warehouse.models import *
