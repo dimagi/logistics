1d7199e0cd827faae187045c065789bd644a81c3
Cory Zue <czue@dimagi.com>
2012-08-07 11:59:34 +0200

    add south migrations for malawi app and warehouse models

--- logistics_project/migrations/malawi/0002_auto__add_field_contact_organization.py
+++ logistics_project/migrations/malawi/0002_auto__add_field_contact_organization.py
@@ -7,6 +7,10 @@ from django.db import models
 
 class Migration(SchemaMigration):
 
+    depends_on = (
+        ("malawi", "0001_initial"),
+    )
+
     def forwards(self, orm):
         # Adding field 'Contact.organization'
         db.add_column('rapidsms_contact', 'organization',
