aca1b9ad7d9e8201a292d27a1ec353d21aaa110e
Cory Zue <czue@dimagi.com>
2012-08-08 11:29:27 +0200

    add managed_supply_points to web for for orgs, only allow disricts

--- logistics_project/apps/malawi/forms.py
+++ logistics_project/apps/malawi/forms.py
@@ -1,9 +1,15 @@
 from django import forms
 from logistics_project.apps.malawi.models import Organization
+from logistics.models import SupplyPoint
+from logistics.util import config
 
 class OrganizationForm(forms.ModelForm):
     name = forms.CharField()
-    
+    managed_supply_points = forms.ModelMultipleChoiceField(
+        queryset=SupplyPoint.objects.filter(active=True,
+                                   type__code=config.SupplyPointCodes.DISTRICT),
+        required=False                                               
+    )
     class Meta:
         model = Organization
         
