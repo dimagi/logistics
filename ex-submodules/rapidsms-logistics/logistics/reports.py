35b028cc174b831b60498d8794499ba32a16e80a
Cory Zue <czue@dimagi.com>
2012-08-08 11:02:45 +0200

    move WarehouseView --> reports.ReportView

--- ex-submodules/rapidsms-logistics/logistics/reports.py
+++ ex-submodules/rapidsms-logistics/logistics/reports.py
@@ -2,16 +2,15 @@ import json
 from datetime import timedelta, datetime
 from django.core.urlresolvers import reverse
 from django.db.models.expressions import F
-from django.db.models import Q, Sum
+from django.shortcuts import render_to_response
+from django.template.context import RequestContext
+from django.db.models import Q
 from django.utils.importlib import import_module
 from rapidsms.conf import settings
-from rapidsms.models import Contact
 from dimagi.utils.dates import DateSpan
-from logistics.const import Reports
 from logistics.models import ProductReport, \
     Product, ProductStock, SupplyPoint, StockRequest, HistoricalStockCache
-from .tables import ReportingTable, SOHReportingTable
-import models as logistics_models
+from .tables import SOHReportingTable
 from .const import Reports
 from .util import config
 
@@ -690,6 +689,7 @@ class HSASupplyPointRow(SupplyPointRow):
         return [self.supply_point]
     
 
+
 class FacilitySupplyPointRow(SupplyPointRow):
     
     @property
@@ -716,3 +716,78 @@ def get_reporting_and_nonreporting_facilities(deadline, location):
     on_time_facilities = facilities.filter(last_reported__gte=deadline, active=True)
     late_facilities = facilities.filter(Q(last_reported=None) | Q(last_reported__lt=deadline), active=True)
     return on_time_facilities, late_facilities
+
+
+
+class ReportView(object):
+    """
+    View class to be extended for each report page 
+        with its own custom_context method
+    
+    Include in settings:
+
+        REPORT_LIST = SortedDict([
+        ("Dashboard", "dashboard"),])
+    """
+    _context = None
+    
+    def __init__(self, slug):
+        self.slug = slug
+        self._context = None
+    
+        
+    @property
+    def template_name(self):
+        """
+        The template that should be used for this.
+        """
+        # should be overridden
+        return "%s.html" % self.slug
+     
+    def can_view(self, request):
+        """
+        Whether this report can be viewed - typically based of the user and 
+        potentially other information in the request.
+        """
+        # should be overridden if you want to restrict things
+        return True
+    
+    def get_response(self, request):
+        """
+        The HTTP Response object for this report
+        """
+        return render_to_response(self.template_name, 
+                                  self.get_context(request),
+                                  context_instance=RequestContext(request))
+
+    
+    def shared_context(self, request):
+        """
+        Add this to your subclasses shared_context method:
+
+        base_context = super(<YourWarehouseViewSubclass>, self).shared_context(request)
+        """
+        to_stub = lambda x: {"name": x, "slug": settings.REPORT_LIST[x]}
+        stub_reports = [to_stub(r) for r in settings.REPORT_LIST.keys()]
+
+        return { 
+            "report_list": stub_reports,
+            "settings": settings
+        }
+        
+    def custom_context(self, request):
+        """
+        Custom context required for a specific report
+        """
+        # should be overridden
+        return {}
+    
+    def get_context(self, request):
+        """
+        Specific data to be included in a report
+        """
+        if not self._context:
+            self._context = {"slug": self.slug}
+            self._context.update(self.shared_context(request))
+            self._context.update(self.custom_context(request))
+        return self._context
