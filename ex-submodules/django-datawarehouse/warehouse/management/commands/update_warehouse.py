524cdce479cecfa36d0176cefd9f7e6184c0e6b3
Cory Zue <czue@dimagi.com>
2012-08-10 11:08:28 +0200

    move the main warehouse function to a method in runner.py
    - change the generate() signature to take in a run record not a start
      and end date

--- ex-submodules/django-datawarehouse/warehouse/management/commands/update_warehouse.py
+++ ex-submodules/django-datawarehouse/warehouse/management/commands/update_warehouse.py
@@ -1,12 +1,7 @@
 from django.core.management.base import LabelCommand
-from datetime import datetime
-from dimagi.utils.modules import to_function
-from django.conf import settings
 from optparse import make_option
 from dimagi.utils.parsing import string_to_datetime
-from warehouse.models import ReportRun
-from django.db import transaction
-from django.db.utils import DatabaseError
+from warehouse import runner
 
 class Command(LabelCommand):
     
@@ -20,44 +15,9 @@ class Command(LabelCommand):
     
 
     def handle(self, *args, **options):
-        print "Start time: %s" % datetime.now()
-        classpath = settings.WAREHOUSE_RUNNER if hasattr(settings, "WAREHOUSE_RUNNER") \
-            else "warehouse.runner.DemoWarehouseRunner"
-        runner = to_function(classpath, failhard=True)()
         
-        last_run = ReportRun.last_success()
-        # use the passed in date, or the last run end, or the beginning of time,
-        # in the order that they're available
-        start_date = (datetime.min if not last_run else last_run.end ) \
-            if len(args) < 1 else string_to_datetime(args[0]) 
-        # use the passed in date, or right now
-        end_date = datetime.utcnow() if len(args) < 2 else string_to_datetime(args[1]) 
-        
-        print "executing warehouse from %s, %s to %s" % (classpath, start_date.date(), end_date.date())
-        if options["cleanup"]:
-            runner.cleanup(start_date, end_date)
-        
-        running = ReportRun.objects.filter(complete=False)
-        if running.count() > 0:
-            print "Warehouse already running, will do nothing..."
-            return
-    
-        # start new run
-        new_run = ReportRun.objects.create(start=start_date, end=end_date,
-                                           start_run=datetime.utcnow())
-        try: 
-            runner.generate(start_date, end_date)
-        except Exception, e:
-            # just in case something funky happened in the DB
-            if isinstance(e, DatabaseError):
-                transaction.rollback()
-            new_run.has_error = True
-            raise
-        finally:
-            # complete run
-            new_run.end_run = datetime.utcnow()
-            new_run.complete = True
-            new_run.save()
-            print "End time: %s" % datetime.now()
-
+        start_date = None if len(args) < 1 else string_to_datetime(args[0]) 
+        end_date = None if len(args) < 2 else string_to_datetime(args[1]) 
+        cleanup = options["cleanup"]
+        return runner.update_warehouse(start_date, end_date, cleanup) 
         
--- ex-submodules/django-datawarehouse/warehouse/runner.py
+++ ex-submodules/django-datawarehouse/warehouse/runner.py
@@ -1,3 +1,9 @@
+from datetime import datetime
+from dimagi.utils.modules import to_function
+from django.conf import settings
+from warehouse.models import ReportRun
+from django.db import transaction
+from django.db.utils import DatabaseError
 
 
 class WarehouseRunner(object):
@@ -12,7 +18,7 @@ class WarehouseRunner(object):
         """
         pass
     
-    def generate(self, start, end):
+    def generate(self, run_record):
         """
         Generate all warehouse data between start and end.
         """
@@ -29,9 +35,63 @@ class DemoWarehouseRunner(WarehouseRunner):
                "Override WAREHOUSE_CLASS in your settings.py file to have "
                "this actually do something.") 
     
-    def generate(self, start, end):
+    def generate(self, run_record):
         print ("Demo warehouse generate! Would create all data from %s-%s. "
                "Override WAREHOUSE_CLASS in your settings.py file to have "
-               "this actually do something.") % (start, end)
+               "this actually do something.") % (run_record.start, run_record.end)
     
+
+def get_warehouse_runner():
+    """
+    Get the configured runner, bsed on the WAREHOUSE_RUNNER setting, or
+    the default demo runner if none found. 
+    """
+    classpath = settings.WAREHOUSE_RUNNER if hasattr(settings, "WAREHOUSE_RUNNER") \
+        else "warehouse.runner.DemoWarehouseRunner"
+    return to_function(classpath, failhard=True)()
+    
+def update_warehouse(start_date=None, end_date=None, cleanup=False):
+    
+    print "Start time: %s" % datetime.now()
+    
+    last_run = ReportRun.last_success()
+    # use the passed in date, or the last run end, or the beginning of time,
+    # in the order that they're available
+    if start_date is None:
+        start_date = (datetime.min if not last_run else last_run.end )
+    # use the passed in date, or right now
+    if end_date is None:
+        end_date = datetime.utcnow() 
+    
+    runner = get_warehouse_runner()
+    print "executing warehouse from %s, %s to %s" % (runner, start_date.date(), end_date.date())
+    if cleanup:
+        runner.cleanup(start_date, end_date)
+    
+    running = ReportRun.objects.filter(complete=False)
+    if running.count() > 0:
+        print "Warehouse already running, will do nothing..."
+        return
+
+    # start new run
+    new_run = ReportRun.objects.create(start=start_date, end=end_date,
+                                       start_run=datetime.utcnow())
+    try: 
+        runner.generate(new_run)
+    except Exception, e:
+        # just in case something funky happened in the DB
+        if isinstance(e, DatabaseError):
+            try:
+                transaction.rollback()
+            except:
+                pass
+        new_run.has_error = True
+        raise
+    finally:
+        # complete run
+        new_run.end_run = datetime.utcnow()
+        new_run.complete = True
+        new_run.save()
+        print "End time: %s" % datetime.now()
+
     
