9f9493f70940f1e211fc11970ae8effbd69186c6
Rowena <rluk@dimagi.com>
2012-08-13 18:42:47 -0400

    fix broken reference to self.supply_points

--- ex-submodules/rapidsms-logistics/logistics/reports.py
+++ ex-submodules/rapidsms-logistics/logistics/reports.py
@@ -64,10 +64,7 @@ class ReportingBreakdown(object):
     def __init__(self, supply_points, datespan=None, include_late=False,
                  days_for_late=5, MNE=False, request=None):
         
-        if supply_points.filter(active=False).exists(): 
-            supply_points = self.supply_points.filter(active=True)
-        
-        self.supply_points = supply_points
+        supply_points = supply_points.filter(active=True)
         
         if not datespan:
             datespan = DateSpan.since(30)
