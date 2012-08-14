e7fef64bd360c5cc8641d8bdbacaf3e7aa9bb964
Cory Zue <czue@dimagi.com>
2012-08-14 21:25:03 +0200

    updating to the django-datawarehouse framework for warehouse stuff

---
+++ logistics_project/apps/tanzania/reporting/runner.py
@@ -0,0 +1,22 @@
+from warehouse.runner import WarehouseRunner
+from logistics_project.apps.tanzania.reporting.run_reports2 import populate_report_data,\
+    clear_out_reports
+
+
+class TanzaniaWarehouseRunner(WarehouseRunner):
+    """
+    Tanzania's implementation of the warehouse runner. 
+    """
+    
+    def cleanup(self, start, end):
+        """
+        Cleanup all warehouse data between start and end.
+        """
+        clear_out_reports(start, end)
+    
+    def generate(self, run_record):
+        """
+        Generate all warehouse data between start and end.
+        """
+        populate_report_data(run_record.start, run_record.end)
+    
