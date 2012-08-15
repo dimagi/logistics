f0fbc4667ac8b33daf5573c8fe319c39ddac83e5
Cory Zue <czue@dimagi.com>
2012-08-15 21:01:20 +0200

    add tasks.py for async warehouse updates

---
+++ ex-submodules/django-datawarehouse/warehouse/tasks.py
@@ -0,0 +1,10 @@
+from celery.decorators import task
+from warehouse.runner import update_warehouse
+
+@task
+def update_warehouse_async(start_date=None, end_date=None, cleanup=False):
+    """
+    To asynchronously update the warehouse, e.g. from a view.
+    """
+    return update_warehouse(start_date, end_date, cleanup)
+
