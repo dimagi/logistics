585bd74b3a59b7dbcebd1cf5bcd95270807f4cd5
Cory Zue <czue@dimagi.com>
2012-08-29 17:06:01 -0400

    add base reporting model class

--- ex-submodules/rapidsms-logistics/logistics/warehouse_models.py
+++ ex-submodules/rapidsms-logistics/logistics/warehouse_models.py
@@ -10,23 +10,34 @@ class HistoricalStockCache(models.Model):
     year = models.PositiveIntegerField()
     month = models.PositiveIntegerField()
     stock = models.IntegerField(null=True)
-    
 
-class ReportingModel(models.Model):
+class BaseReportingModel(models.Model):
     """
     A model to encapsulate aggregate (data warehouse) data used by a report.
     """
     supply_point = models.ForeignKey('logistics.SupplyPoint') # viewing supply point
-    date = models.DateTimeField()                   # viewing time period
     create_date = models.DateTimeField(editable=False)
     update_date = models.DateTimeField(editable=False)
-
+    
     def save(self, *args, **kwargs):
         if not self.id:
             self.create_date = datetime.utcnow()
         self.update_date = datetime.utcnow()
-        super(ReportingModel, self).save(*args, **kwargs)
+        super(BaseReportingModel, self).save(*args, **kwargs)
+        
+    class Meta:
+        abstract = True
 
+
+class ReportingModel(BaseReportingModel):
+    """
+    A model to encapsulate aggregate (data warehouse) data used by a report.
+    
+    Just a BaseReportingModel + a date.
+    """
+    date = models.DateTimeField()                   # viewing time period
+    
+    
     class Meta:
         abstract = True
 
