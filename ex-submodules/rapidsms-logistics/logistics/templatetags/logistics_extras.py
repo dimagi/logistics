853abcb1d271df7cbad64365cd217b16e824fc8b
Cory Zue <czue@dimagi.com>
2012-10-29 11:09:20 -0400

    add more templatetags from email-reports

--- ex-submodules/rapidsms-logistics/logistics/templatetags/logistics_extras.py
+++ ex-submodules/rapidsms-logistics/logistics/templatetags/logistics_extras.py
@@ -22,3 +22,15 @@ def historical_date_last_stocked(productstock, date):
 @register.filter
 def times(a,b):
     return a * b
+
+@register.filter
+def dict_lookup(dict, key):
+    '''Get an item from a dictionary.'''
+    return dict.get(key)
+    
+@register.filter
+def array_lookup(array, index):
+    '''Get an item from an array.'''
+    if index < len(array):
+        return array[index]
+    
