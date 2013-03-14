2f36f975983ad3869975c70fc690e4b99dc99182
Cory Zue <czue@dimagi.com>
2013-03-14 10:05:00 -0400

    add better admin for locations

--- logistics_project/apps/tanzania/admin.py
+++ logistics_project/apps/tanzania/admin.py
@@ -4,11 +4,24 @@
 from django.contrib import admin
 from logistics_project.apps.tanzania.models import SupplyPointStatus,\
     AdHocReport
+from rapidsms.contrib.locations.models import Location
 
 class SupplyPointStatusAdmin(admin.ModelAdmin):
     model = SupplyPointStatus
     list_display = ('status_type', 'status_value', 'supply_point', 'status_date')
     list_filter = ('status_type', 'status_value', 'supply_point', 'status_date')
 
+class TzLocationAdmin(admin.ModelAdmin):
+    model = Location
+    list_display = ("name", "code", "type", "parent", "is_active")
+    list_filter = ('type', 'is_active', 'parent_id')
+
 admin.site.register(SupplyPointStatus, SupplyPointStatusAdmin)
 admin.site.register(AdHocReport)
+
+try:
+    admin.site.unregister(Location)
+except Exception:
+    pass
+
+admin.site.register(Location, TzLocationAdmin)
