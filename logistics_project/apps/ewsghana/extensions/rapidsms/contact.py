4b5554387a9d246422df3bcd791db48b7ab0567f
Rowena <rluk@dimagi.com>
2013-04-05 12:55:41 -0400

    don't list reporting date for in-charges

--- logistics_project/apps/ewsghana/extensions/rapidsms/contact.py
+++ logistics_project/apps/ewsghana/extensions/rapidsms/contact.py
@@ -1,8 +1,15 @@
 from __future__ import absolute_import
 from django.db import models
+from logistics.util import config
 
 class Contact(models.Model):
     family_name  = models.CharField(max_length=100, blank=True, verbose_name="Family Name")
 
     class Meta:
         abstract = True
+        
+    def is_incharge(self):
+        supervise_resp = config.Responsibilities.REPORTEE_RESPONSIBILITY
+        if self.role.responsibilities.filter(code=supervise_resp):
+            return True
+        return False
