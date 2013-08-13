cd17fca27755c96379c19d35bf4187d54fff8ce9
Cory Zue <czue@dimagi.com>
2013-08-13 11:17:26 -0700

    better messagelog admin

--- logistics_project/apps/tanzania/admin.py
+++ logistics_project/apps/tanzania/admin.py
@@ -5,6 +5,8 @@ from django.contrib import admin
 from logistics_project.apps.tanzania.models import SupplyPointStatus,\
     AdHocReport
 from rapidsms.contrib.locations.models import Location
+from rapidsms.contrib.messagelog.models import Message
+
 
 class SupplyPointStatusAdmin(admin.ModelAdmin):
     model = SupplyPointStatus
@@ -25,3 +27,14 @@ except Exception:
     pass
 
 admin.site.register(Location, TzLocationAdmin)
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('text', 'direction', 'who', 'date')
+    list_filter = ('direction', 'contact', 'connection', 'date')
+
+try:
+    admin.site.unregister(Message)
+except Exception:
+    pass
+
+admin.site.register(Message, MessageAdmin)
