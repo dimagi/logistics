a30d4eaa051e08b0192eaddd2cc46e7326d68a29
Cory Zue <czue@dimagi.com>
2013-08-20 14:54:13 -0400

    test the language after it is set

--- logistics_project/apps/tanzania/tests/language.py
+++ logistics_project/apps/tanzania/tests/language.py
@@ -6,6 +6,17 @@ from django.utils import translation
 
 class TestLanguage(TanzaniaTestScriptBase):
 
+    def _verify_language(self, language, phone_number):
+        previous_language = translation.get_language()
+        translation.activate(language)
+        expected = _(config.Messages.HELP_REGISTERED)
+        translation.activate(previous_language)
+        script = """
+          %(phone)s > help
+          %(phone)s < %(help_registered)s
+        """ % {'phone': phone_number, 'help_registered': expected}
+        self.runScript(script)
+
     def testLanguageEnglish(self):
         translation.activate("en")
         contact = register_user(self, "778", "someone", "d10001")
@@ -14,15 +25,18 @@ class TestLanguage(TanzaniaTestScriptBase):
             778 < %(language_confirm)s
             """ % {'language_confirm': _(config.Messages.LANGUAGE_CONFIRM) % {"language": "English"}}
         self.runScript(script)
+        self._verify_language('en', '778')
 
     def testLanguageSwahili(self):
         translation.activate("sw")
-        contact = register_user(self, "778", "someone", "d10001")
+        contact = register_user(self, "779", "someone", "d10001")
         script = """
-            778 > lugha sw
-            778 < %(language_confirm)s
+            779 > lugha sw
+            779 < %(language_confirm)s
             """ % {'language_confirm': _(config.Messages.LANGUAGE_CONFIRM) % {"language": "Swahili"}}
         self.runScript(script)
+        self._verify_language('sw', '779')
+
 
     def testLanguageUnknown(self):
         translation.activate("sw")
