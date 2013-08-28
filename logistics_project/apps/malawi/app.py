646dc8045c36b4325241903383ae056e2881f8b7
Cory Zue <czue@dimagi.com>
2013-08-28 17:41:25 -0400

    remove unnecessary complexity

--- logistics_project/apps/malawi/app.py
+++ logistics_project/apps/malawi/app.py
@@ -1,22 +1,3 @@
-from logistics.app import App as LogisticsApp
-
-class App(LogisticsApp):
-    """
-    Once upon a time this app overrode some stuff.
-    All logic has since been moved into handlers, 
-    and this actually does nothing.
-    """
-    
-    def start (self):
-        # don't do all the scheduling stuff
-        pass
-    
-    def handle (self, message):
-        pass
-        
-    def default(self, message):
-        # the base class overrides all possible responses. 
-        # this avoids that problem.
-        pass
-    
-    
+class App(object):
+    # this is just a stub class to keep rapidsms from complaining
+    pass
