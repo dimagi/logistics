cc539256a7d43c65d0f1064a7945ddd14e318379
Cory Zue <czue@dimagi.com>
2014-02-11 15:51:24 -0600

    api tweak

--- ex-submodules/rapidsms-logistics/logistics/shortcuts.py
+++ ex-submodules/rapidsms-logistics/logistics/shortcuts.py
@@ -1,10 +1,11 @@
 from logistics.models import ProductReportsHelper, SupplyPoint
 
 def create_stock_report(report_type, supply_point, text, logger_msg=None, 
-                        timestamp=None, additional_validation=lambda report: True):
+                        timestamp=None, additional_validation=None):
     """
     Gets a stock report helper object parses it, and saves it.
     """
+    additional_validation = additional_validation or (lambda report: True)
     stock_report = ProductReportsHelper(supply_point, 
                                         report_type,  
                                         logger_msg,
