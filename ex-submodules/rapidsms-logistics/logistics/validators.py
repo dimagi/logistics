a67e083f35a97811f934b34195403dd31cb04dc4
Cory Zue <czue@dimagi.com>
2014-02-12 10:59:03 -0600

    don't crash if no matching stock to check max level for

--- ex-submodules/rapidsms-logistics/logistics/validators.py
+++ ex-submodules/rapidsms-logistics/logistics/validators.py
@@ -10,8 +10,11 @@ def check_max_levels(stock_report):
     """
     for product_code, stock in stock_report.product_stock.items():
         product = stock_report.get_product(product_code)
-        current_stock = ProductStock.objects.get(supply_point=stock_report.supply_point,
-                                                 product=product)
-        max = current_stock.maximum_level * settings.LOGISTICS_MAX_REPORT_LEVEL_FACTOR
-        if stock > max:
-            raise TooMuchStockError(product=product, amount=stock, max=max)
+        try:
+            current_stock = ProductStock.objects.get(supply_point=stock_report.supply_point,
+                                                     product=product)
+            max = current_stock.maximum_level * settings.LOGISTICS_MAX_REPORT_LEVEL_FACTOR
+            if stock > max:
+                raise TooMuchStockError(product=product, amount=stock, max=max)
+        except ProductStock.DoesNotExist:
+            pass
