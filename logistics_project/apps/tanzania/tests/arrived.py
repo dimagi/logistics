b89b977244f4be32c588c99779ade9c3848ccdff
Cory Zue <czue@dimagi.com>
2014-03-12 11:17:04 -0400

    add arrived keyword and tests

---
+++ logistics_project/apps/tanzania/tests/arrived.py
@@ -0,0 +1,36 @@
+from logistics_project.apps.tanzania.tests.base import TanzaniaTestScriptBase
+from django.utils.translation import ugettext as _
+from django.utils import translation
+from logistics.util import config
+
+class TestArrival(TanzaniaTestScriptBase):
+
+    def testArrivedHelp(self):
+        translation.activate("en")
+        script = """
+          743 > arrived
+          743 < %(arrived_help)s
+        """ % {"arrived_help": _(config.Messages.ARRIVED_HELP)}
+        self.runScript(script)
+        script = """
+          743 > arrived with too many words
+          743 < %(arrived_help)s
+        """ % {"arrived_help": _(config.Messages.ARRIVED_HELP)}
+        self.runScript(script)
+
+    def testArrivedUnknownCode(self):
+        translation.activate("en")
+        script = """
+          743 > arrived NOTACODEINTHESYSTEM
+          743 < %(arrived_default)s
+        """ % {"arrived_default": _(config.Messages.ARRIVED_DEFAULT)}
+        self.runScript(script)
+
+    def testArrivedKnownCode(self):
+        translation.activate("en")
+        script = """
+          743 > arrived D10001
+          743 < %(arrived_known)s
+        """ % {"arrived_known": _(config.Messages.ARRIVED_KNOWN) % {"facility": "VETA 1"}}
+        self.runScript(script)
+
