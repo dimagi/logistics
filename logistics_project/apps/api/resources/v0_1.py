4a604b2c4856e6ff4f791aa64807dcb3f0953f79
Lukasz Wyszomirski <lwyszomirski@soldevelo.com>
2014-07-01 13:00:47 +0200

    chnaged JSON format

--- logistics_project/apps/api/resources/v0_1.py
+++ logistics_project/apps/api/resources/v0_1.py
@@ -1,34 +1,7 @@
 from tastypie.authentication import BasicAuthentication
 from tastypie.authorization import ReadOnlyAuthorization
-from tastypie.exceptions import BadRequest
-from tastypie.resources import Resource, ModelResource
+from tastypie.resources import ModelResource
 from logistics.models import Product
-from tastypie.utils.validate_jsonp import is_valid_jsonp_callback_value
-from logistics_project.apps.api import ApiSerializer
-
-
-class CustomResource(ModelResource):
-    # copy from Resource class
-    def serialize(self, request, data, format, options={}):
-        """
-        Given a request, data and a desired format, produces a serialized
-        version suitable for transfer over the wire.
-
-        Mostly a hook, this uses the ``Serializer`` from ``Resource._meta``.
-        """
-        options = options or {'type': self.type}
-
-        if 'text/javascript' in format:
-            # get JSONP callback name. default to "callback"
-            callback = request.GET.get('callback', 'callback')
-
-            if not is_valid_jsonp_callback_value(callback):
-                raise BadRequest('JSONP callback name is invalid.')
-
-            options['callback'] = callback
-
-        return self._meta.serializer.serialize(data, format, options)
-
 
 
 class CustomResourceMeta(object):
@@ -36,12 +9,10 @@ class CustomResourceMeta(object):
     authentication = BasicAuthentication()
     default_format = 'application/json'
 
-class ProductResources(CustomResource):
-    type = "product_list"
+class ProductResources(ModelResource):
 
     class Meta(CustomResourceMeta):
         max_limit = None
-        serializer = ApiSerializer()
         queryset = Product.objects.all()
         include_resource_uri = False
         fields = ['name', 'units', 'sms_code', 'description', 'is_active']
