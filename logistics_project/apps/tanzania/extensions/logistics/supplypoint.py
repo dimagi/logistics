518ffe1089dea5246b37cb3c33d34b19c00d8fb0
Kamil Krampa <kkrampa@soldevelo.com>
2014-09-15 13:26:40 +0200

    Changed name of field

--- logistics_project/apps/tanzania/extensions/logistics/supplypoint.py
+++ logistics_project/apps/tanzania/extensions/logistics/supplypoint.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class TanzaniaSupplyPointExtension(models.Model):
     is_pilot = models.BooleanField(default=False)
-    nearests_supply_points = models.ManyToManyField('self', related_name='nearests', symmetrical=True,
+    closest_supply_points = models.ManyToManyField('self', related_name='closest', symmetrical=True,
                                                     limit_choices_to={'is_pilot': True}, null=False, blank=True)
 
     class Meta:
