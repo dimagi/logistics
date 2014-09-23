fdbda2cc7a84e4e06744d2b283d3792cc6adbc86
Łukasz Wyszomirski <lwyszomirski@soldevelo.com>
2014-09-23 10:07:22 +0200

    added --test option

--- logistics_project/apps/tanzania/management/commands/tz_update_facilities_code.py
+++ logistics_project/apps/tanzania/management/commands/tz_update_facilities_code.py
@@ -1,11 +1,19 @@
 from django.core.management.base import BaseCommand
+from optparse import make_option
+from logistics.models import SupplyPoint
 import xlrd
 import os
-from logistics.models import SupplyPoint
 
 
 class Command(BaseCommand):
     help = "Update facilities code from excel file (usage in migration from ILS to HQ)"
+    option_list = BaseCommand.option_list + (
+        make_option('--test',
+            action='store_true',
+            dest='test',
+            default=False,
+            help='Delete poll instead of closing it'),
+        )
 
     def handle(self, *args, **options):
         PATH = 'apps/tanzania/management/commands/resource'
@@ -26,8 +34,11 @@ class Command(BaseCommand):
                                 if ex_sp.code != sp.code:
                                     print "Conflict in facilities: old_code - %s, new_code -%s, supply point id to update - %s, Conflicted supply point id - %s" % (cell[1], cell[3], sp.id, ex_sp.id)
                             except SupplyPoint.DoesNotExist:
-                                sp.location.save()
-                                sp.save()
+                                if options['test']:
+                                    print "Facility - DISTRICT_NAME: %s, ILS_CODE: %s, ILS_NAME %s, ELMIS_CODE: %s, ELMIS_NAME: %s - OK" % (sheet.name, cell[1], cell[0], cell[3], cell[2])
+                                else:
+                                    sp.location.save()
+                                    sp.save()
                                 pass
                         except SupplyPoint.DoesNotExist:
                             print("Problem with update facility: name - %s, code - %s" % (cell[0], cell[1]))
