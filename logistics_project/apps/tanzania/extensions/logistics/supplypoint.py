e0bcd916ab585d259067b0142bd70e265885347f
Kamil Krampa <kkrampa@soldevelo.com>
2014-09-23 13:57:41 +0200

    Added comments, exception and code tweaks

--- logistics_project/apps/tanzania/extensions/logistics/supplypoint.py
+++ logistics_project/apps/tanzania/extensions/logistics/supplypoint.py
@@ -3,6 +3,10 @@ from datetime import date, timedelta
 from django.db import models
 
 
+class ImproperState(Exception):
+    pass
+
+
 class TanzaniaSupplyPointExtension(models.Model):
     is_pilot = models.BooleanField(default=False)
     closest_supply_points = models.ManyToManyField('self', related_name='closest', symmetrical=True,
@@ -25,11 +29,11 @@ class TanzaniaSupplyPointExtension(models.Model):
         from logistics.models import ProductReport
 
         if state == 'overstocked':
-            cond = lambda (x): x > 6
+            cond = lambda x: x > 6
         elif state == 'stockout':
-            cond = lambda (x): x < 0.35
+            cond = lambda x: x < 0.35
         else:
-            return {}
+            raise ImproperState()
 
         if self.type.name != 'FACILITY':
             return {}
@@ -41,19 +45,24 @@ class TanzaniaSupplyPointExtension(models.Model):
             try:
                 report = ProductReport.objects.filter(supply_point=self,
                                                       product=product_stock.product,
-                                                      report_type_id=1,
+                                                      report_type__code='soh',
                                                       report_date__gte=last_ten_days).latest('report_date')
+                #Ignore all products where the monthly consumption is unknown unless their current stock level is 0
                 if not product_stock.auto_monthly_consumption or product_stock.auto_monthly_consumption == 0:
                     if report.quantity == 0:
                         remaining = 0
+                        six_months_consumption = 0
                     else:
                         continue
                 else:
+                    six_months_consumption = 6 * product_stock.auto_monthly_consumption
                     remaining = report.quantity / float(product_stock.auto_monthly_consumption)
 
                 if cond(remaining):
-                    stock_level[product_stock.product.sms_code] = (report.quantity, report.quantity -
-                                                                   6 * product_stock.auto_monthly_consumption)
+                    #It computes how much stock facility have to keep for themselves.
+                    #It's using the equivalent of 6 months of stock
+                    stock_level[product_stock.product.sms_code] = (report.quantity,
+                                                                   six_months_consumption)
             except ProductReport.DoesNotExist:
                 continue
 
