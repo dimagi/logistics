fd20099e39eb6186668961551b83cbfab8b596e5
Cory Zue <czue@dimagi.com>
2015-01-09 10:13:35 -0500

    add translations markup

--- logistics_project/apps/tanzania/config.py
+++ logistics_project/apps/tanzania/config.py
@@ -1,4 +1,4 @@
-
+from django.utils.translation import ugettext_noop as _
 
 
 HSA = "hsa"
@@ -130,122 +130,122 @@ def hsa_location_type():
 class Messages(object):
     # some semblance of an attempt to start being consistent about this.
 
-    NUMBER_OF_SUPPLY_POINTS = "Facilities" # label
-    
-    # "register" keyword (tz registration)
-    REGISTER_HELP = "To register send reg <name> <msd code> or reg <name> at <district name>. Example:reg john patel d34002 or reg john patel : tandahimba"
-    REGISTER_BAD_CODE = "I didn't recognize your msd code.  To register, send register <name> <msd code>. example: register Peter Juma d34002"
-    REGISTER_UNKNOWN_CODE = "Sorry, can't find the location with MSD CODE %(msd_code)s"
-    REGISTER_UNKNOWN_DISTRICT = "Sorry, can't find the location with the name %(name)s"
-    REGISTRATION_CONFIRM = "Thank you for registering at %(sdp_name)s, %(msd_code)s, %(contact_name)s"
-    REGISTRATION_CONFIRM_DISTRICT = "Thank you for registering at %(sdp_name)s, %(contact_name)s"
+    NUMBER_OF_SUPPLY_POINTS = _("Facilities") # label
 
-    HELP_REGISTERED = 'Welcome to ILSGateway. Available commands are soh, delivered, not delivered, submitted, not submitted, language, sw, en, stop, supervision, la'
+    # "register" keyword (tz registration)
+    REGISTER_HELP = _("To register send reg <name> <msd code> or reg <name> at <district name>. Example:reg john patel d34002 or reg john patel : tandahimba")
+    REGISTER_BAD_CODE = _("I didn't recognize your msd code.  To register, send register <name> <msd code>. example: register Peter Juma d34002")
+    REGISTER_UNKNOWN_CODE = _("Sorry, can't find the location with MSD CODE %(msd_code)s")
+    REGISTER_UNKNOWN_DISTRICT = _("Sorry, can't find the location with the name %(name)s")
+    REGISTRATION_CONFIRM = _("Thank you for registering at %(sdp_name)s, %(msd_code)s, %(contact_name)s")
+    REGISTRATION_CONFIRM_DISTRICT = _("Thank you for registering at %(sdp_name)s, %(contact_name)s")
+
+    HELP_REGISTERED = _('Welcome to ILSGateway. Available commands are soh, delivered, not delivered, submitted, not submitted, language, sw, en, stop, supervision, la')
     HELP_UNREGISTERED = REGISTER_HELP
-    
+
     # language keyword
-    LANGUAGE_HELP = "To set your language, send LANGUAGE <CODE>"
-    LANGUAGE_CONTACT_REQUIRED = "You must JOIN or IDENTIFY yourself before you can set your language preference"
-    LANGUAGE_CONFIRM = "I will speak to you in %(language)s."
-    LANGUAGE_UNKNOWN = 'Sorry, I don\'t speak "%(language)s".'
-    
+    LANGUAGE_HELP = _("To set your language, send LANGUAGE <CODE>")
+    LANGUAGE_CONTACT_REQUIRED = _("You must JOIN or IDENTIFY yourself before you can set your language preference")
+    LANGUAGE_CONFIRM = _("I will speak to you in %(language)s.")
+    LANGUAGE_UNKNOWN = _('Sorry, I don\'t speak "%(language)s".')
+
     # "stop" keyword
-    STOP_CONFIRM = "You have requested to stop reminders to this number.  Send 'help' to this number for instructions on how to reactivate."
-    
+    STOP_CONFIRM = _("You have requested to stop reminders to this number.  Send 'help' to this number for instructions on how to reactivate.")
+
     # stockout
-    STOCKOUT_HELP = "Please send in stockout reports in the form 'so <product>'"
-    STOCKOUT_INVALID_CODES = "Sorry, invalid product codes %(codes)s"
-    STOCKOUT_CONFIRM = 'Thank you %(contact_name)s for reporting stockouts of %(product_names)s for %(facility_name)s.'
-    
+    STOCKOUT_HELP = _("Please send in stockout reports in the form 'so <product>'")
+    STOCKOUT_INVALID_CODES = _("Sorry, invalid product codes %(codes)s")
+    STOCKOUT_CONFIRM = _('Thank you %(contact_name)s for reporting stockouts of %(product_names)s for %(facility_name)s.')
+
     # l & a
-    LOSS_ADJUST_HELP = "Please send in your adjustments in the format 'la <product> +-<amount> +-<product> +-<amount>...'"
-    LOSS_ADJUST_BAD_FORMAT = "Sorry, invalid format.  The message should be in the format 'la <product> +-<amount> +-<product> +-<amount>..."
-    LOSS_ADJUST_CONFIRM = "Thank you. Have you received supervision this month? Please reply 'supervision yes' or 'supervision no'"
-    
+    LOSS_ADJUST_HELP = _("Please send in your adjustments in the format 'la <product> +-<amount> +-<product> +-<amount>...'")
+    LOSS_ADJUST_BAD_FORMAT = _("Sorry, invalid format.  The message should be in the format 'la <product> +-<amount> +-<product> +-<amount>...")
+    LOSS_ADJUST_CONFIRM = _("Thank you. Have you received supervision this month? Please reply 'supervision yes' or 'supervision no'")
+
     # supervision
-    SUPERVISION_HELP = "Supervision reminders will come monthly, and you can respond 'supervision yes' if you have received supervision or 'supervision no' if you have not"
-    SUPERVISION_CONFIRM_NO = 'You have reported that you have not yet received supervision this month.'
-    SUPERVISION_CONFIRM_YES = 'Thank you for reporting that you have received supervision this month.'
-    SUPERVISION_REMINDER = "Have you received supervision this month? Please reply 'supervision yes' or 'supervision no'"
-    
+    SUPERVISION_HELP = _("Supervision reminders will come monthly, and you can respond 'supervision yes' if you have received supervision or 'supervision no' if you have not")
+    SUPERVISION_CONFIRM_NO = _('You have reported that you have not yet received supervision this month.')
+    SUPERVISION_CONFIRM_YES = _('Thank you for reporting that you have received supervision this month.')
+    SUPERVISION_REMINDER = _("Have you received supervision this month? Please reply 'supervision yes' or 'supervision no'")
+
     # yes
-    YES_HELP = 'If you have submitted your R&R, respond \"submitted\".  If you have received your delivery, respond \"delivered\"'
-    
-    # not 
-    NOT_HELP = "If you haven't submitted your R&R, respond \"not submitted\". If you haven't received your delivery, respond \"not delivered\""
-    
+    YES_HELP = _('If you have submitted your R&R, respond \"submitted\".  If you have received your delivery, respond \"delivered\"')
+
+    # not
+    NOT_HELP = _("If you haven't submitted your R&R, respond \"not submitted\". If you haven't received your delivery, respond \"not delivered\"")
+
     # not delivered
-    NOT_DELIVERED_CONFIRM = "You have reported that you haven't yet received your delivery."
+    NOT_DELIVERED_CONFIRM = _("You have reported that you haven't yet received your delivery.")
 
     # delivery
-    DELIVERED_CONFIRM = "Thank you, you reported a delivery of %(reply_list)s. If incorrect, please resend."
-    DELIVERY_BAD_FORMAT = "Sorry, invalid format.  The message should be in the format 'delivered product amount product amount'"
-    DELIVERY_CONFIRM = "Thank you, you reported a delivery of %(reply_list)s. If incorrect, please resend."
-    DELIVERY_PARTIAL_CONFIRM = "To record a delivery, respond with \"delivered product amount product amount...\""
-    DELIVERY_REMINDER_FACILITY = "Did you receive your delivery yet? Please reply 'delivered <product> <amount> <product> <amount>...'"
-    DELIVERY_REMINDER_DISTRICT = "Did you receive your delivery yet? Please reply 'delivered' or 'not delivered'"
-    DELIVERY_CONFIRM_DISTRICT = "Thank you %(contact_name)s for reporting your delivery for %(facility_name)s"
-    DELIVERY_LATE_DISTRICT = "Facility deliveries for group %(group_name)s (out of %(group_total)d): %(not_responded_count)d haven't responded and %(not_received_count)d have reported not receiving. See ilsgateway.com"
-    DELIVERY_CONFIRM_CHILDREN = "District %(district_name)s has reported that they sent their R&R forms to MSD"
+    DELIVERED_CONFIRM = _("Thank you, you reported a delivery of %(reply_list)s. If incorrect, please resend.")
+    DELIVERY_BAD_FORMAT = _("Sorry, invalid format.  The message should be in the format 'delivered product amount product amount'")
+    DELIVERY_CONFIRM = _("Thank you, you reported a delivery of %(reply_list)s. If incorrect, please resend.")
+    DELIVERY_PARTIAL_CONFIRM = _("To record a delivery, respond with \"delivered product amount product amount...\"")
+    DELIVERY_REMINDER_FACILITY = _("Did you receive your delivery yet? Please reply 'delivered <product> <amount> <product> <amount>...'")
+    DELIVERY_REMINDER_DISTRICT = _("Did you receive your delivery yet? Please reply 'delivered' or 'not delivered'")
+    DELIVERY_CONFIRM_DISTRICT = _("Thank you %(contact_name)s for reporting your delivery for %(facility_name)s")
+    DELIVERY_LATE_DISTRICT = _("Facility deliveries for group %(group_name)s (out of %(group_total)d): %(not_responded_count)d haven't responded and %(not_received_count)d have reported not receiving. See ilsgateway.com")
+    DELIVERY_CONFIRM_CHILDREN = _("District %(district_name)s has reported that they sent their R&R forms to MSD")
 
 
     # not submitted
-    NOT_SUBMITTED_CONFIRM = "You have reported that you haven't yet sent in your R&R."
+    NOT_SUBMITTED_CONFIRM = _("You have reported that you haven't yet sent in your R&R.")
 
-    # submitted         
-    SUBMITTED_CONFIRM = "Thank you %(contact_name)s for submitting your R and R form for %(sdp_name)s"
-    SUBMITTED_REMINDER_FACILITY = "Have you sent in your R&R form yet for this quarter? Please reply \"submitted\" or \"not submitted\""
-    SUBMITTED_REMINDER_DISTRICT= "How many R&R forms have you submitted to MSD? Reply with 'submitted A <number of R&Rs submitted for group A> B <number of R&Rs submitted for group B>'"
-    SUBMITTED_NOTIFICATION_MSD = "%(district_name)s has submitted their R&R forms to MSD: %(group_a)s for Group A, %(group_b)s for Group B, %(group_c)s for Group C"
+    # submitted
+    SUBMITTED_CONFIRM = _("Thank you %(contact_name)s for submitting your R and R form for %(sdp_name)s")
+    SUBMITTED_REMINDER_FACILITY = _("Have you sent in your R&R form yet for this quarter? Please reply \"submitted\" or \"not submitted\"")
+    SUBMITTED_REMINDER_DISTRICT= _("How many R&R forms have you submitted to MSD? Reply with 'submitted A <number of R&Rs submitted for group A> B <number of R&Rs submitted for group B>'")
+    SUBMITTED_NOTIFICATION_MSD = _("%(district_name)s has submitted their R&R forms to MSD: %(group_a)s for Group A, %(group_b)s for Group B, %(group_c)s for Group C")
 
     # reminders
-    REMINDER_STOCKONHAND = "Please send in your stock on hand information in the format 'soh <product> <amount> <product> <amount>...'"
-    REMINDER_R_AND_R_FACILITY = "Have you sent in your R&R form yet for this quarter? Please reply \"submitted\" or \"not submitted\""
-    REMINDER_R_AND_R_DISTRICT = "How many R&R forms have you submitted to MSD? Reply with 'submitted A <number of R&Rs submitted for group A> B <number of R&Rs submitted for group B>'"
-    REMINDER_DELIVERY_FACILITY = "Did you receive your delivery yet? Please reply 'delivered <product> <amount> <product> <amount>...'"
-    REMINDER_DELIVERY_DISTRICT = "Did you receive your delivery yet? Please reply 'delivered' or 'not delivered'"
-    REMINDER_SUPERVISION = "Have you received supervision this month? Please reply 'supervision yes' or 'supervision no'"
-    
-    # reminder reports 
-    REMINDER_MONTHLY_RANDR_SUMMARY = "R&R - %(submitted)s/%(total)s submitted, %(not_submitted)s/%(total)s did not submit, %(not_responding)s/%(total)s did not reply"
-    REMINDER_MONTHLY_SOH_SUMMARY = "SOH - %(submitted)s/%(total)s reported, %(not_responding)s/%(total)s did not reply" 
-    REMINDER_MONTHLY_DELIVERY_SUMMARY = "Deliveries - %(received)s/%(total)s received, %(not_received)s/%(total)s did not receive, %(not_responding)s/%(total)s did not reply"
+    REMINDER_STOCKONHAND = _("Please send in your stock on hand information in the format 'soh <product> <amount> <product> <amount>...'")
+    REMINDER_R_AND_R_FACILITY = _("Have you sent in your R&R form yet for this quarter? Please reply \"submitted\" or \"not submitted\"")
+    REMINDER_R_AND_R_DISTRICT = _("How many R&R forms have you submitted to MSD? Reply with 'submitted A <number of R&Rs submitted for group A> B <number of R&Rs submitted for group B>'")
+    REMINDER_DELIVERY_FACILITY = _("Did you receive your delivery yet? Please reply 'delivered <product> <amount> <product> <amount>...'")
+    REMINDER_DELIVERY_DISTRICT = _("Did you receive your delivery yet? Please reply 'delivered' or 'not delivered'")
+    REMINDER_SUPERVISION = _("Have you received supervision this month? Please reply 'supervision yes' or 'supervision no'")
+
+    # reminder reports
+    REMINDER_MONTHLY_RANDR_SUMMARY = _("R&R - %(submitted)s/%(total)s submitted, %(not_submitted)s/%(total)s did not submit, %(not_responding)s/%(total)s did not reply")
+    REMINDER_MONTHLY_SOH_SUMMARY = _("SOH - %(submitted)s/%(total)s reported, %(not_responding)s/%(total)s did not reply")
+    REMINDER_MONTHLY_DELIVERY_SUMMARY = _("Deliveries - %(received)s/%(total)s received, %(not_received)s/%(total)s did not receive, %(not_responding)s/%(total)s did not reply")
 
     # "soh" keyword (report stock on hand)
-    SOH_HELP_MESSAGE = "Please send in your stock on hand information in the format 'soh <product> <amount> <product> <amount>...'"
-    SOH_BAD_FORMAT = "Sorry, invalid format. The message should be in the format 'soh <product> <amount> <product> <amount>...'"
-    SOH_PARTIAL_CONFIRM = 'Thank you %(contact_name)s for reporting your stock on hand for %(facility_name)s.  Still missing %(product_list)s.'
-    SOH_CONFIRM = "Thank you. Reported products: %(reply_list)s. Please send in your adjustments in the format 'la <product> +-<amount> +-<product> +-<amount>...'"
-    SOH_ADJUSTMENTS_REMINDER = "Please send in your adjustments in the format 'la <product> +-<amount> +-<product> +-<amount>...'"
-    SOH_THANK_YOU = "Thank you for reporting your stock on hand this month"
-    SOH_OVERSTOCKED = "You are overstocked for %(overstocked_list)s that you can redistribute to other facilities. Keep %(products_list)s."
+    SOH_HELP_MESSAGE = _("Please send in your stock on hand information in the format 'soh <product> <amount> <product> <amount>...'")
+    SOH_BAD_FORMAT = _("Sorry, invalid format. The message should be in the format 'soh <product> <amount> <product> <amount>...'")
+    SOH_PARTIAL_CONFIRM = _('Thank you %(contact_name)s for reporting your stock on hand for %(facility_name)s.  Still missing %(product_list)s.')
+    SOH_CONFIRM = _("Thank you. Reported products: %(reply_list)s. Please send in your adjustments in the format 'la <product> +-<amount> +-<product> +-<amount>...'")
+    SOH_ADJUSTMENTS_REMINDER = _("Please send in your adjustments in the format 'la <product> +-<amount> +-<product> +-<amount>...'")
+    SOH_THANK_YOU = _("Thank you for reporting your stock on hand this month")
+    SOH_OVERSTOCKED = _("You are overstocked for %(overstocked_list)s that you can redistribute to other facilities. Keep %(products_list)s.")
 
     # stock inquiry
-    STOCK_INQUIRY_HELP_MESSAGE = "Please send in your stock on hand information in the format 'si <MSD product code> <amount>'"
-    STOCK_INQUIRY_CONFIRM = 'Thank you, you reported you have %(quantity)s %(product_name)s. If incorrect, please resend.'
-    STOCK_INQUIRY_MESSAGE = "How much %(product_name)s (msd_code %(msd_code)s) do you have in stock?  Please respond 'si %(msd_code)s <amount>'"
-    STOCK_INQUIRY_NOT_A_FACILITY_ERROR = "Can only initiate product inquiry for a single facility via SMS - %(location_name)s is a %(location_type)s"
+    STOCK_INQUIRY_HELP_MESSAGE = _("Please send in your stock on hand information in the format 'si <MSD product code> <amount>'")
+    STOCK_INQUIRY_CONFIRM = _('Thank you, you reported you have %(quantity)s %(product_name)s. If incorrect, please resend.')
+    STOCK_INQUIRY_MESSAGE = _("How much %(product_name)s (msd_code %(msd_code)s) do you have in stock?  Please respond 'si %(msd_code)s <amount>'")
+    STOCK_INQUIRY_NOT_A_FACILITY_ERROR = _("Can only initiate product inquiry for a single facility via SMS - %(location_name)s is a %(location_type)s")
 
     # "arrived" keyword
-    ARRIVED_HELP = "To report an arrival, please send 'arrived <MSD code>'."
-    ARRIVED_DEFAULT = "Thank you for confirming your arrival at the health facility."
-    ARRIVED_KNOWN = "Thank you for confirming your arrival at %(facility)s."
+    ARRIVED_HELP = _("To report an arrival, please send 'arrived <MSD code>'.")
+    ARRIVED_DEFAULT = _("Thank you for confirming your arrival at the health facility.")
+    ARRIVED_KNOWN = _("Thank you for confirming your arrival at %(facility)s.")
 
-    INVALID_PRODUCT_CODE = "Sorry, invalid product code %(product_code)s"
+    INVALID_PRODUCT_CODE = _("Sorry, invalid product code %(product_code)s")
 
     #test handler
-    TEST_HANDLER_HELP = "To test a reminder, send \"test [remindername] [msd code]\"; valid tests are soh, delivery, randr. Remember to setup your contact details!"
-    TEST_HANDLER_BAD_CODE = "Invalid msd code %(code)s"
-    TEST_HANDLER_CONFIRM = "Sent"
+    TEST_HANDLER_HELP = _("To test a reminder, send \"test [remindername] [msd code]\"; valid tests are soh, delivery, randr. Remember to setup your contact details!")
+    TEST_HANDLER_BAD_CODE = _("Invalid msd code %(code)s")
+    TEST_HANDLER_CONFIRM = _("Sent")
 
-    REMINDER_TRANS = "Did you receive or transfer stock to another facility last month? Please reply either 'trans no' or 'trans yes'"
-    TRANS_HELP = "You can respond 'trans yes' if you have received or transfered stock last month or 'trans no' if you have not"
+    REMINDER_TRANS = _("Did you receive or transfer stock to another facility last month? Please reply either 'trans no' or 'trans yes'")
+    TRANS_HELP = _("You can respond 'trans yes' if you have received or transfered stock last month or 'trans no' if you have not")
 
-    REMINDER_STOCKOUT = "You are stocked out of %(products_list)s. The following facilities are overstocked: %(overstocked_list)s"
+    REMINDER_STOCKOUT = _("You are stocked out of %(products_list)s. The following facilities are overstocked: %(overstocked_list)s")
 
     REGISTRATION_REQUIRED_MESSAGE = HELP_UNREGISTERED
 
 class Alerts(object):
-    
-    HSA_NO_PRODUCTS = "%(hsa)s is registered but is not associated with any products"
-    FACILITY_NO_SUPERVISOR = "No HSA supervisor or in-charge is registered for %(facility)s but there are HSAs registered there."
+
+    HSA_NO_PRODUCTS = _("%(hsa)s is registered but is not associated with any products")
+    FACILITY_NO_SUPERVISOR = _("No HSA supervisor or in-charge is registered for %(facility)s but there are HSAs registered there.")
