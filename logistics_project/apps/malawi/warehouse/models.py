b875646e7ee9de24f56c5f25c3f4e1121f9dd075
Cory Zue <czue@dimagi.com>
2015-03-11 14:26:36 -0400

    unicode function

--- logistics_project/apps/malawi/warehouse/models.py
+++ logistics_project/apps/malawi/warehouse/models.py
@@ -46,7 +46,14 @@ class ProductAvailabilityData(MalawiWarehouseModel):
     managed_and_without_stock = models.PositiveIntegerField(default=0)
     managed_and_without_data = models.PositiveIntegerField(default=0)
     managed_and_emergency_stock = models.PositiveIntegerField(default=0)
-    
+
+    def __unicode__(self):
+        return '{0}:{1} ({2}) total: {3} managed: {4} with_stock: {5}, stockout: {6}'.format(
+            self.supply_point, self.product, self.date,
+            self.total, self.managed, self.with_stock, self.without_stock
+        )
+
+
     def set_managed_attributes(self):
         if self.managed:
             for f in ProductAvailabilityData.STOCK_CATEGORIES:
