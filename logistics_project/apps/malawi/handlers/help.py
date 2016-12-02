889a249897f1bda694063f5646deb9f65ed3d1c2
Giovanni Capalbo <gcapalbo@dimagi.com>
2016-12-01 21:38:57 -0500

    Fix help handler for malawi

--- logistics_project/apps/malawi/handlers/help.py
+++ logistics_project/apps/malawi/handlers/help.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
-
-from django.utils.translation import ugettext as _
+from django.utils.datastructures import SortedDict
 from rapidsms.contrib.handlers.handlers.keyword import KeywordHandler
+from logistics.decorators import logistics_contact_required
 from logistics.models import Product
 from logistics.util import config
 
@@ -10,30 +10,49 @@ from logistics.util import config
 class Help(KeywordHandler):
     keyword = "help"
 
-
+    @logistics_contact_required()
     def help(self):
         self.respond(config.Messages.HELP_TEXT)
 
+    @logistics_contact_required()
     def handle(self, text):
+        is_hsa = (
+            self.msg.logistics_contact.supply_point and
+            self.msg.logistics_contact.supply_point.type_id == config.SupplyPointCodes.HSA
+        )
         topic = text.strip().lower()
+
         if topic == 'stock':
-            self.respond(_("Please send your receipts in the format ' <Commodity code> <stock on hand > . <quantity received>'"))
-        elif topic == 'stop':
-            self.respond(_("Text 'stop' to stop receiving text message reminders."))
-        elif topic == 'start':
-            self.respond(_("Text 'start' to get text message reminders every week to submit your stock reports."))
+            self.respond(config.Messages.SOH_HELP_MESSAGE)
         elif 'code' in topic:
-            codes = [c.sms_code for c in Product.objects.all().order_by('sms_code')]
-            self.respond("Available commodity codes: %(codes)s", codes=", ".join(codes))
+            products = Product.objects.filter(is_active=True).select_related('type').order_by("type__id", "sms_code")
+
+            # Only show HSA-level products to HSAs; show all products to everyone else
+            if is_hsa:
+                products = products.filter(type__base_level=config.BaseLevel.HSA)
+
+            grouped_codes = SortedDict()
+            for p in products:
+                if p.type.name not in grouped_codes:
+                    grouped_codes[p.type.name] = []
+                grouped_codes[p.type.name].append(p.sms_code.lower())
+
+            messages = []
+            for type_name, codes in grouped_codes.iteritems():
+                messages.append("%s codes: %s" % (type_name, " ".join(codes)))
+
+            self.respond("; ".join(messages))
         else:
             try:
-                p = Product.objects.get(sms_code=topic)
-                msg = "%s is the commodity code for %s" % (topic, p.name)
-                if p.units:
-                    msg = msg + " (%s)" % p.units
-                if p.description:
-                    if p.description not in p.name:
-                        msg = msg + " %s" % p.description
-                self.respond(msg)
+                if is_hsa:
+                    p = Product.objects.get(sms_code=topic, is_active=True, type__base_level=config.BaseLevel.HSA)
+                else:
+                    p = Product.objects.get(sms_code=topic, is_active=True)
             except Product.DoesNotExist:
                 self.respond(config.Messages.HELP_TEXT)
+
+            msg = "%s is the code for %s product %s" % (topic, p.type.name, p.name)
+            if p.units:
+                msg = msg + " (%s)" % p.units
+
+            self.respond(msg)
