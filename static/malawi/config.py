c2355353bf1217fad28f2921bd413f6ff23039f0
Giovanni Capalbo <gcapalbo@dimagi.com>
2017-03-12 10:18:24 -0400

    Use constants for backend names

--- static/malawi/config.py
+++ static/malawi/config.py
@@ -3,6 +3,13 @@ from datetime import datetime
 
 HSA = "hsa"
 
+# The backend names match what is defined in the database, which gets
+# populated from the backend configuration in localsettings. Don't change
+# these unless everything gets updated together.
+TNM_BACKEND_NAME = 'tnm-smpp'
+AIRTEL_BACKEND_NAME = 'airtel-smpp'
+
+
 class Roles(object):
     """
     Roles go here
