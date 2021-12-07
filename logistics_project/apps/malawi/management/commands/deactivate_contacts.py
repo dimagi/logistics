25bf43eca8f72525157d03aa8f943174ab1ec032
Cory Zue <cory@coryzue.com>
2021-12-07 16:55:48 +0200

    add --limit and --test arguments to command

--- logistics_project/apps/malawi/management/commands/deactivate_contacts.py
+++ logistics_project/apps/malawi/management/commands/deactivate_contacts.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+from optparse import make_option
+
 from django.db import transaction
 from django.core.management.base import BaseCommand
 from rapidsms.models import Contact
@@ -8,6 +10,16 @@ class DeactivateContactCommand(BaseCommand):
     help = "Deactivates some contacts"
     log_file_name = None
     log_file = None
+    option_list = BaseCommand.option_list + (
+        make_option('--limit', action='store', dest='limit', default=None,
+                    help='Number of contacts to operate on.'),
+        make_option('--test', action='store_true', dest='test', default=False,
+                    help='Work in test mode (does not write to the database).'),
+    )
+    args = '[--limit <limit> --test]'
+    limit = None
+    test = False
+
 
     def log(self, text):
         self.log_file.write("%s: %s\n" % (datetime.utcnow(), text))
@@ -25,16 +37,25 @@ class DeactivateContactCommand(BaseCommand):
         if contact.is_active:
             self.log("Contact is active, deactivating...")
             contact.is_active = False
-            contact.save()
+            if not self.test:
+                contact.save()
         else:
             self.log("Contact is already deactivated, ignoring...")
 
     def fix_contacts(self):
         self.log('Deactivating %s contacts' % self.get_queryset().count())
-        for contact in self.get_queryset():
+        queryset = self.get_queryset()
+        if self.limit:
+            queryset = queryset[:self.limit]
+        for contact in queryset:
             self.deactivate_contact(contact)
 
     def handle(self, *args, **options):
+        limit = options.get('limit', None)
+        if limit is not None:
+            limit = int(limit)
+        self.limit = limit
+        self.test = options.get('test')
         with open(self.log_file_name, 'a') as log_file:
             self.log_file = log_file
             with transaction.commit_on_success():
