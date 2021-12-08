2c09d4835f85d975f4182ba4faf111dec9f9f9e6
Cory Zue <cory@coryzue.com>
2021-12-08 14:50:41 +0200

    change from assertion to print statment

--- logistics_project/apps/malawi/signals.py
+++ logistics_project/apps/malawi/signals.py
@@ -3,6 +3,7 @@ from django.db.models.signals import post_save
 from rapidsms.models import Contact
 from static.malawi.config import SupplyPointCodes
 
+
 @transaction.commit_on_success
 def deactivate_hsa_location(sender, instance, created, **kwargs):
     if instance.is_hsa and not instance.is_active:
@@ -10,7 +11,9 @@ def deactivate_hsa_location(sender, instance, created, **kwargs):
         # point if necessary
         if instance.supply_point:
             sp = instance.supply_point
-            assert sp.type.code == SupplyPointCodes.HSA
+            if sp.type.code != SupplyPointCodes.HSA:
+                print('Refusing to deactivate location because HSA was registered to a non-HSA supply point %s (%s).' % (sp, sp.type))
+                return
 
             # Don't deactivate the supply point if other active contacts belong to it
             if sp.active_contact_set.count() > 0:
