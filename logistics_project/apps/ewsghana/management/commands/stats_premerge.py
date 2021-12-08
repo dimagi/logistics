a29d0394d24163825e3066a2f81152b0b173ae5e
Cory Zue <cory@coryzue.com>
2021-12-08 13:54:19 +0200

    remove unused imports in various commands

--- logistics_project/apps/ewsghana/management/commands/stats_premerge.py
+++ logistics_project/apps/ewsghana/management/commands/stats_premerge.py
@@ -1,7 +1,5 @@
-import sys
 from django.core.management.base import BaseCommand
-from django.conf import settings
-from dimagi.utils.couch.database import get_db
+
 
 class Command(BaseCommand):
     help = "Print vital statistics"
