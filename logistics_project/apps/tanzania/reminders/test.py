29e4ef54754ce40652413edc016e075620941aa3
Cory Zue <cory@coryzue.com>
2022-02-03 21:18:40 +0200

    delete ilsgateway and tanzania content

--- logistics_project/apps/tanzania/reminders/test.py
+++
@@ -1,7 +0,0 @@
-from django.core.mail import mail_admins
-
-def test_email_admins():
-    """
-    Just a proof of concept to test the scheduler. 
-    """
-    mail_admins("Scheduler test", "This is your reminder that all is well")
