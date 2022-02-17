df878478bee20273d9843328d52e7eab8f279a04
Cory Zue <cory@coryzue.com>
2022-02-17 17:13:39 +0200

    delete no-longer-used base handlers

--- ex-submodules/rapidsms-logistics/logistics/handlers/__init__.py
+++
@@ -1,6 +0,0 @@
-from rapidsms.conf import settings
-
-def logistics_keyword(kw):
-    if settings.LOGISTICS_USE_DEFAULT_HANDLERS:
-        return kw
-    return "thiscrazyhackylongkeywordshouldhopefullynevermatchanyintentionalsms"
