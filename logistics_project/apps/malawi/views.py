494f992038c04722d00d3393e33baad9d25b8d96
Cory Zue <cory@coryzue.com>
2022-02-21 15:44:24 +0200

    use normal permission_required decorator

--- logistics_project/apps/malawi/views.py
+++ logistics_project/apps/malawi/views.py
@@ -20,7 +20,6 @@ from django.db.models.aggregates import Count
 
 from dimagi.utils.csv import UnicodeWriter
 from logistics_project.utils.dates import DateSpan, months_between, add_months
-from dimagi.utils.django.permission_required import permission_required_with_403
 from dimagi.utils.decorators.datespan import datespan_in_request
 
 from rapidsms.models import Contact
@@ -457,13 +456,13 @@ def facility(request, code, context={}):
     return render_to_response("malawi/single_facility.html", context, context_instance=RequestContext(request))
 
     
-@permission_required_with_403("auth.admin_read")
+@permission_required("auth.admin_read")
 def monitoring(request):
     reports = (ReportDefinition(slug) for slug in REPORT_SLUGS) 
     return render_to_response("malawi/monitoring_home.html", {"reports": reports},
                               context_instance=RequestContext(request))
 @cache_page(60 * 15)
-@permission_required_with_403("auth.admin_read")
+@permission_required("auth.admin_read")
 @datespan_default
 def monitoring_report(request, report_slug):
     report_def = ReportDefinition(report_slug)
