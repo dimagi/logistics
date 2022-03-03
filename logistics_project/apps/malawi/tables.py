cea6c6242a0b7f655cdde88363de95461f9263a9
Cory Zue <cory@coryzue.com>
2022-03-03 13:07:26 +0200

    delete table

--- logistics_project/apps/malawi/tables.py
+++ logistics_project/apps/malawi/tables.py
@@ -13,29 +13,10 @@ from logistics.models import StockRequestStatus
 from rapidsms.models import Contact
 
 
-class MalawiContactTable(Table):
-    name     = Column(link=contact_edit_link)
-    role = Column()
-    hsa_id = Column(value=lambda cell: cell.object.hsa_id,
-                    name="HSA Id",
-                    sortable=False)
-    supply_point = Column(value=lambda cell: cell.object.associated_supply_point_name,
-                          name="Supply Point",
-                          sortable=False)
-    phone = Column(value=lambda cell: cell.object.phone, 
-                   name="Phone Number",
-                   sortable=False)
-    commodities = Column(name="Responsible For These Commodities", 
-                         value=list_commodities,
-                         sortable=False)
-    organization = Column()
-    
-    class Meta:
-        order_by = 'supply_point__code'
-
 def _edit_org_link(cell):
     return reverse("malawi_edit_organization", args=[cell.row.pk])
     
+
 class OrganizationTable(Table):
     name     = Column(link=_edit_org_link)
     members  = Column(value=lambda cell: Contact.objects.filter(organization=cell.object).count(),
