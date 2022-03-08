07d6732dcdd18ea4d130a5013c36158c37c5504e
Cory Zue <cory@coryzue.com>
2022-03-08 10:01:16 +0200

    move malawi contact extension into rapidsms models

--- ex-submodules/rapidsms/lib/rapidsms/models.py
+++ ex-submodules/rapidsms/lib/rapidsms/models.py
@@ -1,7 +1,9 @@
 from __future__ import absolute_import
 from django.core.exceptions import ImproperlyConfigured
-
 from django.db import models
+from django.utils.translation.trans_real import translation
+
+
 from .utils.modules import try_import, get_classes
 from .errors import NoConnectionError, MessageSendingError
 from .conf import settings
@@ -147,6 +149,7 @@ class Contact(ContactBase):
                                          blank=True, null=True)
     is_active = models.BooleanField(default=True)
     is_approved = models.BooleanField(default=False)
+    organization = models.ForeignKey('malawi.Organization', null=True, blank=True)
 
     class Meta:
         verbose_name = "Logistics Contact"
@@ -191,6 +194,54 @@ class Contact(ContactBase):
             # all active Products in the system
             return Product.objects.filter(is_active=True)
         raise ImproperlyConfigured("LOGISTICS_STOCKED_BY setting is not configured correctly")
+
+    @property
+    def is_hsa(self):
+        from logistics.models import ContactRole
+        from logistics.util import config
+        return self.role == ContactRole.objects.get(code=config.Roles.HSA)
+
+    @property
+    def associated_supply_point(self):
+        """
+        For HSA's return the parent facility. For everyone else return the
+        exact supply point.
+        """
+        from logistics.models import SupplyPoint
+
+        if not self.supply_point: return None
+
+        if self.is_hsa:
+            return SupplyPoint.objects.get(location=self.supply_point.location.parent)
+        return self.supply_point
+
+    @property
+    def associated_supply_point_name(self):
+        if self.associated_supply_point:
+            return self.associated_supply_point.name
+        else:
+            return ""
+
+    @property
+    def hsa_id(self):
+        if self.is_hsa:
+            if self.supply_point:
+                return self.supply_point.code
+            else:
+                return ""
+        else:
+            return ""
+
+    def translate(self, message):
+        """
+        In newer versions of Django, this is accomplished using the
+        django.utils.translation.override context manager.
+
+        We could write our own context manager, but this process mirrors
+        the one used in rapidsms.messages.outgoing.
+        """
+        language = self.language or settings.LANGUAGE_CODE
+        return translation(language).gettext(message)
 
 
 class ConnectionBase(models.Model):
