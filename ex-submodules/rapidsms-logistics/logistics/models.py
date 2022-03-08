ea59df12be8576cfad900b39bc745f8df1d82a0b
Cory Zue <cory@coryzue.com>
2022-03-08 10:09:45 +0200

    move malawi profile extension into logistics module

--- ex-submodules/rapidsms-logistics/logistics/models.py
+++ ex-submodules/rapidsms-logistics/logistics/models.py
@@ -29,6 +29,7 @@ from logistics.const import Reports
 from logistics.util import config, parse_report
 from logistics.mixin import StockCacheMixin
 from logistics.consumption import daily_consumption
+from static.malawi.config import BaseLevel
 
 if hasattr(settings, "MESSAGELOG_APP"):
     message_class = "%s.Message" % settings.MESSAGELOG_APP
@@ -655,8 +656,20 @@ class LogisticsProfileBase(models.Model):
     def __unicode__(self):
         return u"%s (%s, %s)" % (self.user.username, self.location, self.supply_point)
 
+
 class LogisticsProfile(LogisticsProfileBase):
     __metaclass__ = ExtensibleModelBase
+    organization = models.ForeignKey('malawi.Organization', null=True, blank=True)
+
+    # True if this user can view the HSA-level dashboard and reports
+    can_view_hsa_level_data = models.BooleanField(default=True)
+
+    # True if this user can view the facility-level dashboard and reports
+    can_view_facility_level_data = models.BooleanField(default=False)
+
+    # One of the base level constants representing the current dashboard the user sees upon login
+    current_dashboard_base_level = models.CharField(max_length=1, default=BaseLevel.HSA)
+
 
 class ProductStock(models.Model):
     """
