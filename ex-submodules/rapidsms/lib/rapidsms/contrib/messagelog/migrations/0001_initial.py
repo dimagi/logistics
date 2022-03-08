fd8ce2727f08e3f0cbe9ecf51e542507b799b88c
Cory Zue <cory@coryzue.com>
2022-03-08 13:06:19 +0200

    add message log migrations

---
+++ ex-submodules/rapidsms/lib/rapidsms/contrib/messagelog/migrations/0001_initial.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import taggit.managers
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('taggit', '0001_initial'),
+        ('rapidsms', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Message',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('direction', models.CharField(max_length=1, choices=[(b'I', b'Incoming'), (b'O', b'Outgoing')])),
+                ('date', models.DateTimeField()),
+                ('text', models.TextField()),
+                ('connection', models.ForeignKey(to='rapidsms.Connection', null=True)),
+                ('contact', models.ForeignKey(to='rapidsms.Contact', null=True)),
+                ('tags', taggit.managers.TaggableManager(to='taggit.Tag', through='taggit.TaggedItem', help_text='A comma-separated list of tags.', verbose_name='Tags')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
