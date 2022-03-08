a8faac0e4e5d60904c443ce05baa51ebb32a674a
Cory Zue <cory@coryzue.com>
2022-03-08 10:03:43 +0200

    move registration extensions to rapidsms

--- ex-submodules/rapidsms/lib/rapidsms/models.py
+++ ex-submodules/rapidsms/lib/rapidsms/models.py
@@ -243,6 +243,48 @@ class Contact(ContactBase):
         language = self.language or settings.LANGUAGE_CODE
         return translation(language).gettext(message)
 
+    @property
+    def default_backend(self):
+        # TODO: this is defined totally arbitrarily for a future
+        # sane implementation
+        if settings.DEFAULT_BACKEND:
+            return Backend.objects.get(name=settings.DEFAULT_BACKEND)
+        else:
+            return Backend.objects.all()[0]
+
+    @property
+    def default_connection(self):
+        """
+        Return the default connection for this person.
+        """
+        # TODO: this is defined totally arbitrarily for a future
+        # sane implementation
+        if self.connection_set.count() > 0:
+            return self.connection_set.all()[0]
+        return None
+
+    @default_connection.setter
+    def default_connection(self, identity):
+        backend = self.default_backend
+        default = self.default_connection
+        if default is not None:
+            if default.identity == identity and default.backend == backend:
+                # our job is done
+                return
+            # when you re-assign default connection,
+            # should you delete the unused connection?
+            # probably not because who knows what else will get deleted in the
+            # cascade
+            # default.delete()
+        try:
+            conn = Connection.objects.get(backend=backend, identity=identity)
+        except Connection.DoesNotExist:
+            # good, it doesn't exist already
+            conn = Connection(backend=backend,
+                              identity=identity)
+        conn.contact = self
+        conn.save()
+
 
 class ConnectionBase(models.Model):
     backend  = models.ForeignKey(Backend)
