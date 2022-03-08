1979a06349d123625ff0c34bf9b2b4154b1cfc2d
Cory Zue <cory@coryzue.com>
2022-03-08 09:58:27 +0200

    move contact logistics extension to rapidsms models

--- ex-submodules/rapidsms/lib/rapidsms/models.py
+++ ex-submodules/rapidsms/lib/rapidsms/models.py
@@ -1,8 +1,6 @@
-#!/usr/bin/env python
-# vim: ai ts=4 sts=4 et sw=4
+from __future__ import absolute_import
+from django.core.exceptions import ImproperlyConfigured
 
-
-from datetime import datetime
 from django.db import models
 from .utils.modules import try_import, get_classes
 from .errors import NoConnectionError, MessageSendingError
@@ -82,7 +80,7 @@ class App(models.Model):
 
 
 class ContactBase(models.Model):
-    name  = models.CharField(max_length=100, blank=True)
+    name = models.CharField(max_length=100, blank=True)
 
     # the spec: http://www.w3.org/International/articles/language-tags/Overview
     # reference:http://www.iana.org/assignments/language-subtag-registry
@@ -137,9 +135,62 @@ class ContactBase(models.Model):
         return True
 
 
-
 class Contact(ContactBase):
-    __metaclass__ = ExtensibleModelBase
+    # if one person wants to submit stocks for multiple facilities, then
+    # they'll have to create multiple contacts for themselves
+    role = models.ForeignKey("logistics.ContactRole", null=True, blank=True)
+    supply_point = models.ForeignKey("logistics.SupplyPoint", null=True, blank=True)
+    needs_reminders = models.BooleanField(default=True)
+    commodities = models.ManyToManyField("logistics.Product",
+                                         help_text="User manages these commodities.",
+                                         related_name="reported_by",
+                                         blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    is_approved = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = "Logistics Contact"
+
+    def __unicode__(self):
+        if self.name:
+            return self.name
+        return unicode(self.pk)
+
+    @property
+    def phone(self):
+        if self.default_connection:
+            return self.default_connection.identity
+        else:
+            return " "
+
+    @property
+    def last_message(self):
+        if self.message_set.count() > 0:
+            return self.message_set.order_by("-date")[0]
+
+    def has_responsibility(self, code):
+        if not self.role:
+            return False
+        responsibilities = self.role.responsibilities.values_list('code', flat=True)
+        if code in responsibilities:
+            return True
+        return False
+
+    def commodities_reported(self):
+        from logistics.models import Product
+        """ this user is responsible for reporting these commodities """
+        if settings.LOGISTICS_STOCKED_BY == settings.STOCKED_BY_USER:
+            # do a join on all commodities associated with this user
+            return Product.objects.filter(is_active=True).filter(reported_by=self)
+        elif settings.LOGISTICS_STOCKED_BY == settings.STOCKED_BY_FACILITY:
+            # look for products with active ProductStocks linked to user's facility
+            return Product.objects.filter(productstock__supply_point=self.supply_point,
+                                          productstock__is_active=True,
+                                          is_active=True)
+        elif settings.LOGISTICS_STOCKED_BY == settings.STOCKED_BY_PRODUCT:
+            # all active Products in the system
+            return Product.objects.filter(is_active=True)
+        raise ImproperlyConfigured("LOGISTICS_STOCKED_BY setting is not configured correctly")
 
 
 class ConnectionBase(models.Model):
