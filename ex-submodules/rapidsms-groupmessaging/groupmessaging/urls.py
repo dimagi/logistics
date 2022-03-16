c7e359c88fcd756153fa7dce15cbd5f4d4e444b4
Cory Zue <cory@coryzue.com>
2022-03-16 11:03:15 +0200

    upgrade groupmessaging urls

--- ex-submodules/rapidsms-groupmessaging/groupmessaging/urls.py
+++ ex-submodules/rapidsms-groupmessaging/groupmessaging/urls.py
@@ -1,11 +1,7 @@
 from django.conf.urls import *
+from . import views
 
-urlpatterns = patterns('',
-    url(r'^blast/?$',
-        "groupmessaging.views.group_message",
-        name="group_message"),
-    url(r'^ajax_contact_count',
-        "groupmessaging.views.ajax_contact_count",
-        name="ajax_contact_count"
-    )
-)
+urlpatterns = [
+    url(r'^blast/?$', views.group_message, name="group_message"),
+    url(r'^ajax_contact_count', views.ajax_contact_count, name="ajax_contact_count")
+]
