68023aeabb63ba556eb8183d60b8420696dde517
Cory Zue <cory@coryzue.com>
2022-03-16 10:56:47 +0200

    update registration urls

--- logistics_project/apps/registration/urls.py
+++ logistics_project/apps/registration/urls.py
@@ -1,21 +1,9 @@
-#!/usr/bin/env python
-# vim: ai ts=4 sts=4 et sw=4
-
 from django.conf.urls import *
 from . import views
 
 
-urlpatterns = patterns('',
-
-    url(r'^$',
-        views.registration,
-        name="registration"),
-
-    url(r'^search$',
-        views.search,
-        name="search"),
-
-    url(r'^(?P<pk>\d+)/edit$',
-        views.registration,
-        name="registration_edit")
-)
+urlpatterns = [
+    url(r'^$', views.registration, name="registration"),
+    url(r'^search$', views.search, name="search"),
+    url(r'^(?P<pk>\d+)/edit$', views.registration, name="registration_edit"),
+]
