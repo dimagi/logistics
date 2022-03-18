2c70738352daf085c52a2c6bb5dee2bb2fb63bc0
Cory Zue <cory@coryzue.com>
2022-03-18 15:40:32 +0200

    remove print outs

--- logistics_project/testsettings.py
+++ logistics_project/testsettings.py
@@ -1,8 +1,6 @@
 from __future__ import print_function
 from logistics_project.deployments.malawi.settings_base import *
 
-print('database user is', os.environ.get('DB_USER'))
-print('database password is', os.environ.get('DB_PASSWORD'))
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
