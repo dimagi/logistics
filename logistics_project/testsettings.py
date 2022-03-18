b77c878aaec769957acdfc75479ff8c389504fed
Cory Zue <cory@coryzue.com>
2022-03-18 15:04:16 +0200

    future comes first

--- logistics_project/testsettings.py
+++ logistics_project/testsettings.py
@@ -1,5 +1,5 @@
-from logistics_project.deployments.malawi.settings_base import *
 from __future__ import print_function
+from logistics_project.deployments.malawi.settings_base import *
 
 print('database is', os.environ.get('DB_NAME'))
 print('database user is', os.environ.get('DB_USER'))
