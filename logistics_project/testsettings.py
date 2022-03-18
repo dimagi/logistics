ac686a3560deb60b871e41382fb6f9117ecb9566
Cory Zue <cory@coryzue.com>
2022-03-18 15:13:05 +0200

    more tweaks

--- logistics_project/testsettings.py
+++ logistics_project/testsettings.py
@@ -1,13 +1,12 @@
 from __future__ import print_function
 from logistics_project.deployments.malawi.settings_base import *
 
-print('database is', os.environ.get('DB_NAME'))
 print('database user is', os.environ.get('DB_USER'))
 print('database password is', os.environ.get('DB_PASSWORD'))
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": os.environ.get('DB_NAME'),
+        "NAME": 'cstock',
         "USER": os.environ.get('DB_USER'),
         "PASSWORD": os.environ.get('DB_PASSWORD'),
         "HOST": "127.0.0.1",
