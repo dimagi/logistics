8f810622d8e735d02084bffe9ef7313641fbde41
Cory Zue <cory@coryzue.com>
2022-03-18 15:02:12 +0200

    try passing the name via env

--- logistics_project/testsettings.py
+++ logistics_project/testsettings.py
@@ -1,10 +1,13 @@
 from logistics_project.deployments.malawi.settings_base import *
+from __future__ import print_function
 
-
+print('database is', os.environ.get('DB_NAME'))
+print('database user is', os.environ.get('DB_USER'))
+print('database password is', os.environ.get('DB_PASSWORD'))
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": 'cstock',
+        "NAME": os.environ.get('DB_NAME'),
         "USER": os.environ.get('DB_USER'),
         "PASSWORD": os.environ.get('DB_PASSWORD'),
         "HOST": "127.0.0.1",
