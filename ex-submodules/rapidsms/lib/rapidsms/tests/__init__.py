6a338364bcd065d87a21bd7b53836dd6bcb2fb07
Cory Zue <cory@coryzue.com>
2022-03-22 10:25:19 +0200

    import all tests in test __init__.py modules (they don't run)

---
+++ ex-submodules/rapidsms/lib/rapidsms/tests/__init__.py
@@ -0,0 +1,5 @@
+from .test_app_base import *
+from .test_logger import *
+from .test_router import *
+from .test_views import *
+from .backend import *
---
+++ ex-submodules/rapidsms/lib/rapidsms/tests/backend/__init__.py
@@ -0,0 +1,3 @@
+from .test_base import *
+from .test_bucket import *
+from .test_http import *
