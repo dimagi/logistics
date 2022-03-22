675c9876bdec5790cfb8b20679e9d1d091576d79
Cory Zue <cory@coryzue.com>
2022-03-22 16:31:10 +0200

    add bucket test

--- ex-submodules/rapidsms/lib/rapidsms/tests/backend/test_bucket.py
+++ ex-submodules/rapidsms/lib/rapidsms/tests/backend/test_bucket.py
@@ -1,38 +1,32 @@
-#!/usr/bin/env python
-# vim: ai ts=4 sts=4 et sw=4
-
-
-import sys
+from django.test import TestCase
 import time
 import threading
-from nose.tools import assert_equals
-from ...backends.bucket import BucketBackend
-from ...messages import OutgoingMessage
 from ...router import Router
 
+class BucketTest(TestCase):
+    
+    def test_bucket_swallows_messages(self):
 
-def test_bucket_swallows_messages():
+        router = Router()
+        router.add_backend("mock", "rapidsms.backends.bucket")
+        worker = threading.Thread(target=router.start)
+        worker.daemon = True
+        worker.start()
 
-    router = Router()
-    router.add_backend("mock", "rapidsms.backends.bucket")
-    worker = threading.Thread(target=router.start)
-    worker.daemon = True
-    worker.start()
+        # wait until the router has started.
+        while not router.running:
+            time.sleep(0.1)
 
-    # wait until the router has started.
-    while not router.running:
-        time.sleep(0.1)
+        backend = router.backends["mock"]
+        backend.receive("1234", "Mock Incoming Message")
 
-    backend = router.backends["mock"]
-    backend.receive("1234", "Mock Incoming Message")
+        msg = object()
+        backend.send(msg)
 
-    msg = object()
-    backend.send(msg)
+        self.assertEqual(backend.bucket[0].text, "Mock Incoming Message")
+        self.assertEqual(backend.bucket[1], msg)
+        self.assertEqual(len(backend.bucket), 2)
 
-    assert_equals(backend.bucket[0].text, "Mock Incoming Message")
-    assert_equals(backend.bucket[1], msg)
-    assert_equals(len(backend.bucket), 2)
-
-    # wait until the router has stopped.
-    router.stop()
-    worker.join()
+        # wait until the router has stopped.
+        router.stop()
+        worker.join()
