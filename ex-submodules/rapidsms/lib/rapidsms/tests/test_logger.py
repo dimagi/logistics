ee4f4093ee4e285e11032d707be7bcd78cdcd25b
Cory Zue <cory@coryzue.com>
2022-03-22 16:25:04 +0200

    run logger tests again

--- ex-submodules/rapidsms/lib/rapidsms/tests/test_logger.py
+++ ex-submodules/rapidsms/lib/rapidsms/tests/test_logger.py
@@ -1,8 +1,4 @@
-#!/usr/bin/env python
-# vim: ai ts=4 sts=4 et sw=4
-
-
-from nose.tools import assert_equals, assert_raises
+from django.test import SimpleTestCase
 from ..log.mixin import LoggerMixin
 
 
@@ -10,40 +6,39 @@ class LoggableStub(LoggerMixin):
     pass
 
 
-def test_logger_mixin():
-    obj = LoggableStub()
+class LoggerTest(SimpleTestCase):
+    
+    def test_logger_mixin(self):
+        obj = LoggableStub()
+    
+        from logging.handlers import MemoryHandler
+        import logging
+    
+        log = logging.getLogger()
+        handler = MemoryHandler(999)
+        log.setLevel(logging.DEBUG)
+        log.addHandler(handler)
+    
+        obj.debug("This is a DEBUG message")
+        obj.info("This is an INFORMATIVE message")
+        obj.warning("This is a WARNING")
+        obj.error("This is an ERROR")
+        obj.critical("This is a CRITICAL error")
+        obj.exception("This is an exception")
+        obj.exception()
+    
+        self.assertEqual(len(handler.buffer), 7)
+        self.assertEqual(handler.buffer[2].name, "loggablestub")
+        self.assertEqual(handler.buffer[2].msg, "This is a WARNING")
+    
+        log.removeHandler(handler)
 
-    from logging.handlers import MemoryHandler
-    import logging
-
-    log = logging.getLogger()
-    handler = MemoryHandler(999)
-    log.setLevel(logging.DEBUG)
-    log.addHandler(handler)
-
-    obj.debug("This is a DEBUG message")
-    obj.info("This is an INFORMATIVE message")
-    obj.warning("This is a WARNING")
-    obj.error("This is an ERROR")
-    obj.critical("This is a CRITICAL error")
-    obj.exception("This is an exception")
-    obj.exception()
-
-    assert_equals(len(handler.buffer), 7)
-    assert_equals(handler.buffer[2].name, "loggablestub")
-    assert_equals(handler.buffer[2].msg, "This is a WARNING")
-
-    log.removeHandler(handler)
-
-
-def test_logger_raises_on_invalid_name_type():
-    class BrokenLoggableStub(LoggerMixin):
-        def _logger_name(self):
-            return 123
-
-    broken_logger = BrokenLoggableStub()
-
-    assert_raises(
-        TypeError,
-        broken_logger.debug,
-        "This shouldn't work")
+    def test_logger_raises_on_invalid_name_type(self):
+        class BrokenLoggableStub(LoggerMixin):
+            def _logger_name(self):
+                return 123
+    
+        broken_logger = BrokenLoggableStub()
+    
+        with self.assertRaises(TypeError):
+            broken_logger.debug("This shouldn't work")
