4b88d4a8682a696fbfa6d9077953f27b55cbac10
Cory Zue <cory@coryzue.com>
2022-03-30 14:50:45 +0200

    delete alert urls and views

--- ex-submodules/rapidsms-alerts/alerts/urls.py
+++
@@ -1,7 +0,0 @@
-from __future__ import unicode_literals
-from django.conf.urls import *
-
-urlpatterns = patterns('alerts.ajax',
-    url(r'^ajax/addcomment/$', 'add_comment'),
-    url(r'^ajax/alertaction/$', 'alert_action'),
-)
