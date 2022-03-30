732b35eb1f9d29e041658f138ecb9808b509ad19
Cory Zue <cory@coryzue.com>
2022-03-30 15:29:53 +0200

    mark template strings as safe

--- ex-submodules/rapidsms-logistics/logistics/templatetags/logistics_extras.py
+++ ex-submodules/rapidsms-logistics/logistics/templatetags/logistics_extras.py
@@ -1,5 +1,7 @@
 from __future__ import unicode_literals
 from django import template
+from django.utils.safestring import mark_safe
+
 from logistics.templatetags.math_tags import percent
 register = template.Library()
 
@@ -14,7 +16,7 @@ def highlight_months(stockonhand, static_url):
 @register.simple_tag
 def percent_cell(a, b):
     val = percent(a, b)
-    return '<td title="%(a)s of %(b)s">%(val)s</td><td>%(b)s</td>' % {"a": a, "b": b, "val": val}
+    return mark_safe('<td title="%(a)s of %(b)s">%(val)s</td><td>%(b)s</td>' % {"a": a, "b": b, "val": val})
 
 @register.filter('klass')
 def klass(ob):
