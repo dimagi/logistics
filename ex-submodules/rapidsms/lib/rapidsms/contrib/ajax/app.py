69882e2dc0d5941ef8b5d7ebf8c90c4d90b089b7
Cory Zue <cory@coryzue.com>
2022-04-06 13:40:15 +0200

    remove builtin import

--- ex-submodules/rapidsms/lib/rapidsms/contrib/ajax/app.py
+++ ex-submodules/rapidsms/lib/rapidsms/contrib/ajax/app.py
@@ -1,7 +1,6 @@
 from __future__ import unicode_literals
 from future import standard_library
 standard_library.install_aliases()
-from builtins import str
 import cgi
 import urllib.parse
 import traceback
