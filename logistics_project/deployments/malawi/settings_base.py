1d2f74ce8249301557536abafda056a0d01220b3
Cory Zue <cory@coryzue.com>
2022-04-06 14:46:34 +0200

    remove sessionauthenticationmiddleware

--- logistics_project/deployments/malawi/settings_base.py
+++ logistics_project/deployments/malawi/settings_base.py
@@ -17,7 +17,6 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'logistics_project.apps.malawi.middleware.RequireLoginMiddleware',
 ]
