f49366e3992688221f05522d7237b92dd135ea32
Cory Zue <cory@coryzue.com>
2022-04-07 13:48:24 +0200

    update urls

--- ex-submodules/rapidsms/lib/rapidsms/contrib/messagelog/urls.py
+++ ex-submodules/rapidsms/lib/rapidsms/contrib/messagelog/urls.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
-from django.conf.urls import url
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.message_log, name="message_log"),
+    path('', views.message_log, name="message_log"),
 ]
