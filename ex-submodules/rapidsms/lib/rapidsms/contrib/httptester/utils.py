a00bba5f3fa463c547c8ccfa54ffc3fcd1be2578
Cory Zue <cory@coryzue.com>
2022-04-08 10:24:40 +0200

    fix partial

--- ex-submodules/rapidsms/lib/rapidsms/contrib/httptester/utils.py
+++ ex-submodules/rapidsms/lib/rapidsms/contrib/httptester/utils.py
@@ -1,7 +1,7 @@
-from functools import partialmethod
+from functools import partial
 from rapidsms.contrib.ajax.utils import call_router
 
 
 # these helper methods are just proxies to app.py
-send_test_message = partialmethod(call_router, "httptester", "send")
-get_message_log = partialmethod(call_router, "httptester", "log")
+send_test_message = partial(call_router, "httptester", "send")
+get_message_log = partial(call_router, "httptester", "log")
