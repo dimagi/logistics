c87a767e18b9a09216b64917926a148c2050dc95
Cory Zue <cory@coryzue.com>
2022-05-05 15:47:08 +0200

    simplify command

--- ex-submodules/rapidsms/lib/rapidsms/management/commands/populate_last_messages.py
+++ ex-submodules/rapidsms/lib/rapidsms/management/commands/populate_last_messages.py
@@ -7,11 +7,11 @@ class Command(BaseCommand):
     help = "Populate last message objects on all contacts."
 
     def handle(self, **options):
-        contacts = Contact.objects.filter(is_active=True)
+        contacts = Contact.objects.filter(is_active=True, last_message=None)
         contact_count = contacts.count()
         for i, contact in enumerate(contacts):
             print(f'processing contact {i}/{contact_count}...')
-            if not contact.last_message and contact.message_set.filter(direction='I').exists():
+            if contact.message_set.filter(direction='I').exists():
                 last_message = contact.message_set.filter(direction='I').order_by('-date')[0]
                 contact.last_message = last_message
                 contact.save()
