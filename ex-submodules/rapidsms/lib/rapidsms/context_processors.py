8ca1f9b4801164105dd252e951e4238edd9ea682
Cory Zue <cory@coryzue.com>
2022-05-06 16:52:00 +0200

    remove logos from settings

--- ex-submodules/rapidsms/lib/rapidsms/context_processors.py
+++ ex-submodules/rapidsms/lib/rapidsms/context_processors.py
@@ -3,8 +3,6 @@ from django.conf import settings
 
 def logo(request):
     return {
-        'logo_right_url' : settings.LOGO_RIGHT_URL ,
-        'logo_left_url' : settings.LOGO_LEFT_URL,
         'site_title' : settings.SITE_TITLE,
         'base_template': settings.BASE_TEMPLATE,
         'base_template_split_2': settings.BASE_TEMPLATE_SPLIT_2
